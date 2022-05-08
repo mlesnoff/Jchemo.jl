cacd71ea1b77e87d394c49f3c495535c4bba85c8
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2022-05-08 11:22:02 +0200

    0.0-17

--- docs/make.jl
+++ docs/make.jl
@@ -4,24 +4,24 @@ using Jchemo
 #push!(LOAD_PATH, joinpath(@__DIR__, ".."))
 #push!(LOAD_PATH,"../src/")
 
-DocMeta.setdocmeta!(Jchemo, :DocTestSetup, :(using Jchemo); recursive = true)
+#DocMeta.setdocmeta!(Jchemo, :DocTestSetup, :(using Jchemo); recursive = true)
 
 makedocs(
     modules = [Jchemo],
     sitename = "Jchemo.jl",
     authors = "Matthieu Lesnoff (Cirad, Montpellier, France)",
-    repo = "https://mlesnoff.github.io/Jchemo.jl/blob/{commit}{path}#{line}",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://mlesnoff.github.io/Jchemo.jl",
-        assets = String[]),
-    #format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
+    #repo = "https://mlesnoff.github.io/Jchemo.jl/blob/{commit}{path}#{line}",
+    #format = Documenter.HTML(;
+    #    prettyurls = get(ENV, "CI", "false") == "true",
+    #    canonical = "https://mlesnoff.github.io/Jchemo.jl",
+    #    assets = String[]),
+    format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
     pages = ["Domains" => "functions.md",
         "Index" => "api.md",
         "News" => "news.md"]
     )
 
 deploydocs( 
-    repo = "github.com/mlesnoff/Jchemo.jl.git",
-    devbranch = "main"
+    repo = "github.com/mlesnoff/Jchemo.jl.git"
+    #devbranch = "main"
     )
