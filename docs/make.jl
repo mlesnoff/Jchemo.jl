30aca52e437ac72b543946f4f368235d36547476
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2022-05-08 15:47:36 +0200

    0.0-17

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,8 @@
+#push!(LOAD_PATH, joinpath(@__DIR__, ".."))
+push!(LOAD_PATH,"../src/")
+
 using Documenter
 using Jchemo
-
-#push!(LOAD_PATH, joinpath(@__DIR__, ".."))
-#push!(LOAD_PATH,"../src/")
 
 DocMeta.setdocmeta!(Jchemo, :DocTestSetup, :(using Jchemo); recursive = true)
 
@@ -10,10 +10,10 @@ makedocs(
     modules = [Jchemo],
     sitename = "Jchemo.jl",
     authors = "Matthieu Lesnoff (Cirad, Montpellier, France)",
-    repo = "https://mlesnoff.github.io/Jchemo.jl/blob/{commit}{path}#{line}",
+    #repo = "https://mlesnoff.github.io/Jchemo.jl/blob/{commit}{path}#{line}",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://mlesnoff.github.io/Jchemo.jl",
+        #canonical = "https://mlesnoff.github.io/Jchemo.jl",
         assets = String[]),
     #format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
     pages = ["Domains" => "functions.md",
@@ -21,7 +21,6 @@ makedocs(
         "News" => "news.md"]
     )
 
-deploydocs( 
-    repo = "github.com/mlesnoff/Jchemo.jl.git",
-    devbranch = "master"
+deploydocs(;
+    repo = "github.com/mlesnoff/Jchemo.jl.git"
     )
