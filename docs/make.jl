69d52499997c88bbe7fa4b49cd331fcc1300bef6
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2022-05-09 21:55:24 +0200

    0.0-17

--- docs/make.jl
+++ docs/make.jl
@@ -1,29 +1,32 @@
-#push!(LOAD_PATH, joinpath(@__DIR__, ".."))
+push!(LOAD_PATH,joinpath(@__DIR__, ".."))
 #push!(LOAD_PATH,"../src/")
+using Documenter, Jchemo
 
-using Documenter
-using Jchemo
+### PlantGeom.jl 
+#DocMeta.setdocmeta!(Jchemo, :DocTestSetup, :(using Jchemo); recursive = true)
+### End
 
-DocMeta.setdocmeta!(Jchemo, :DocTestSetup, :(using Jchemo); recursive = true)
-
-makedocs(;
+makedocs(
     modules = [Jchemo],
+    format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
+    ### PlantGeom.jul
+    #repo = "https://github.com/mlesnoff/Jchemo.jl/blob/{commit}{path}#{line}",
+    #format = Documenter.HTML(;
+    #    prettyurls = get(ENV, "CI", "false") == "true",
+    #    canonical = "https://mlesnoff.github.io/Jchemo.jl",
+    #    assets = String[]
+    #    ),
+    ### End
+    authors = "Matthieu Lesnoff",
     sitename = "Jchemo.jl",
-    authors = "Matthieu Lesnoff (Cirad, Montpellier, France)",
-    repo = "https://github.com/mlesnoff/Jchemo.jl/blob/{commit}{path}#{line}",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://mlesnoff.github.io/Jchemo.jl",
-        assets = String[]
-        ),
     pages = ["Domains" => "functions.md",
         "Index" => "api.md",
         "News" => "news.md"]
+    # strict = true,
+    # clean = true,
+    # checkdocs = :exports,
     )
 
-deploydocs(;
-    repo = "github.com/mlesnoff/Jchemo.jl.git"
-
-
+deploydocs(
+    repo = "github.com/mlesnoff/Jchemo.jl.git",
     )
-
