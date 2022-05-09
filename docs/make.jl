57222cab9f5ab8c86807160e3c3497704c5ec43c
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2022-05-09 08:48:49 +0200

    0.0-17

--- docs/make.jl
+++ docs/make.jl
@@ -6,33 +6,24 @@ using Jchemo
 
 DocMeta.setdocmeta!(Jchemo, :DocTestSetup, :(using Jchemo); recursive = true)
 
-makedocs(
-
-# Build documentation
 makedocs(;
     modules = [Jchemo],
     doctest = true,
     clean = false,
     sitename = "Jchemo.jl",
     authors = "Matthieu Lesnoff (Cirad, Montpellier, France)",
-    format = Documenter.HTML(
-        #prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://mlesnoff.github.io/Jchemo.jl/stable/",
-        assets = String[],
-        edit_link = "main"
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://mlesnoff.github.io/Jchemo.jl",
+        assets = String[]
         ),
-        pages = ["Domains" => "functions.md",
+    pages = ["Domains" => "functions.md",
         "Index" => "api.md",
-        "News" => "news.md"],
-    strict = true
+        "News" => "news.md"]
     )
 
-# Deploy built documentation from Travis
 deploydocs(;
     repo = "github.com/mlesnoff/Jchemo.jl.git",
-    target = "build",
-    deps = nothing,
-    make = nothing,
     devbranch = "main"
     )
 
