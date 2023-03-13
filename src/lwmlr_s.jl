97029ad31ae6e6a040476c992f04d3b709551a90
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-03-13 09:43:58 +0100

    0.1.15

--- src/lwmlr_s.jl
+++ src/lwmlr_s.jl
@@ -1,23 +1,27 @@
-struct LwmlrS
+struct LwmlrS1
     X::Array{Float64}
     Y::Array{Float64}
     fm
     nlv::Int
+    gamma::Real
     metric::String
     h::Real
     k::Int
+    typ::String
     tol::Real
     scal::Bool
     verbose::Bool
 end
 
 """
-    lwmlr_s(X, Y; nlv, metric, 
-        h, k, pls = true, tol = 1e-4, scal = false, 
+    lwmlr_s(X, Y; nlv, gamma = 1, metric, 
+        h, k, typ = "pls", tol = 1e-4, scal = false, 
         verbose = false)
 kNN-LWMLR after preliminary dimension reduction (kNN-LWMLR-S).
 * `X` : X-data (n, p).
 * `Y` : Y-data (n, q).
+* `gamma` :  Scale parameter for the Gaussian kernel when a KPLS is used 
+    for dimension reduction. See function `krbf`.
 * `nlv` : Nb. latent variables (LVs) for preliminary dimension reduction. 
 * `metric` : Type of dissimilarity used to select the neighbors and compute
     the weights. Possible values are "eucl" (default; Euclidean distance) 
@@ -25,14 +29,14 @@ kNN-LWMLR after preliminary dimension reduction (kNN-LWMLR-S).
 * `h` : A scalar defining the shape of the weight function. Lower is h, 
     sharper is the function. See function `wdist`.
 * `k` : The number of nearest neighbors to select for each observation to predict.
+* `typ` : Type of dimension reduction (by default a PLS). Possible values are:
+    "pca" (PCA), "pls" (PLS), "dkpls" (Gaussian direct KPLS). 
 * `tol` : For stabilization when very close neighbors.
 * `verbose` : If true, fitting information are printed.
 
 A kNN-LWMLR is done after preliminary dimension reduction
-(parameter `nlv`) of the X-data, by PLS or PCA (parameter `pls`). 
-
-When the dimension reduction is done by PCA, this corresponds to the 
-"LWR" algorithm described in Naes et al. (1990).
+(parameter `nlv`) of the X-data. When the reduction is done by PCA, 
+this corresponds to the "LWR" algorithm described in Naes et al. (1990).
 
 ## References 
 Naes, T., Isaksson, T., Kowalski, B., 1990. Locally weighted regression
@@ -65,28 +69,42 @@ plotxy(vec(pred), ytest; color = (:red, .5),
     bisect = true, xlabel = "Prediction", 
     ylabel = "Observed (Test)").f  
 
+
+fm = lwmlr_s(Xtrain, ytrain; nlv = nlv,
+           metric = res.metric[u], h = res.h[u],
+           k = res.k[u], typ = "dkpls", gamma = .01) ;
+pred = Jchemo.predict(fm, Xtest).pred
+rmsep(pred, ytest)
 ```
 """ 
-function lwmlr_s(X, Y; nlv, metric, 
-        h, k, pls = true, tol = 1e-4, scal = false, 
+function lwmlr_s(X, Y; nlv, gamma = 1, metric, 
+        h, k, typ = "pls", tol = 1e-4, scal = false, 
         verbose = false)
     X = ensure_mat(X)
     Y = ensure_mat(Y)
-    if pls
-        fm = plskern(X, Y; nlv = nlv, scal = scal)
-    else
+    if typ == "pca"
         fm = pcasvd(X; nlv = nlv, scal = scal)
+    elseif typ == "pls"
+        fm = plskern(X, Y; nlv = nlv, scal = scal)
+    elseif typ == "dkpls"
+        fm = dkplsr(X, Y; gamma = gamma, nlv = nlv, 
+            scal = scal)
     end
-    LwmlrS(X, Y, fm, nlv, metric, h, k, 
-        tol, scal, verbose)
+    LwmlrS1(X, Y, fm, nlv, gamma, metric, h, k, 
+        typ, tol, scal, verbose)
 end
 
-function predict(object::LwmlrS, X)
+function predict(object::LwmlrS1, X)
     X = ensure_mat(X)
     m = nro(X)
+    if object.typ == "dkpls" 
+        Ttrain = object.fm.fm.T
+    else
+        Ttrain = object.fm.T
+    end
     T = transform(object.fm, X)
     # Getknn
-    res = getknn(object.fm.T, T; 
+    res = getknn(Ttrain, T; 
         k = object.k, metric = object.metric)
     listw = copy(res.d)
     Threads.@threads for i = 1:m
@@ -95,7 +113,7 @@ function predict(object::LwmlrS, X)
         listw[i] = w
     end
     # End
-    pred = locw(object.fm.T, object.Y, T; 
+    pred = locw(Ttrain, object.Y, T; 
         listnn = res.ind, listw = listw, fun = mlr,
         verbose = object.verbose).pred
     (pred = pred, listnn = res.ind, listd = res.d, listw = listw)
