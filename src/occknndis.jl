45f0d47acb103b217354b9ba71e585eadd49cf17
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-01 10:14:01 +0200

    Update occknndis.jl

--- src/occknndis.jl
+++ src/occknndis.jl
@@ -164,8 +164,8 @@ function predict(object::Occknndis, X)
     @inbounds for i = 1:m
         d[i] = median(res.d[i])
     end
-    pval = 1 .- object.e_cdf(d)
-    d = DataFrame(d = d, dstand = d / object.cutoff, pval = pval)
+    p_val = pval(object.e_cdf, d)
+    d = DataFrame(d = d, dstand = d / object.cutoff, pval = p_val)
     pred = reshape(Int64.(d.dstand .> 1), m, 1)
     (pred = pred, d)
 end
