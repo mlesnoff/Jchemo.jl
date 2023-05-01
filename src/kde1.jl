272bc8d1de323fe5ae1b98048bec08d72e7130bd
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-01 16:27:37 +0200

    Update kde1.jl

--- src/kde1.jl
+++ src/kde1.jl
@@ -15,27 +15,57 @@ The function is a wrapper (with `predict` function) of the univariate KDE
 function `kde` of package `KernelDebsity.jl`
 
 ## References 
-
 https://github.com/JuliaStats/KernelDensity.jl
 
 ## Examples
 ```julia
+using CairoMakie
+
+n = 10^4
+x = randn(n)
+lims = (minimum(x), maximum(x))
+#lims = (-10, 10)
+m = 2^3
+bw = .1
+fm = kde1(x; npoints = m, 
+    bandwidth = bw,    # default: Silverman's rule
+    boundary = lims    # default: See KernelDensity
+    ) ;
+fm.x
+diff(fm.x)
+d = fm.density    # same scale as ':pdf' in Makie.hist
+sum(d * diff(fm.x)[1])  # = 1
+## Normalization to sum to 1
+dtot = sum(d)
+dn = d / dtot 
+sum(dn)
+## Standardization to a uniform distribution
+## ("dense" areas > 1) 
+mu_unif = mean(d)
+d / mu_unif 
+
+## Prediction
+xnew = [-200; -100; -1; 0; 1; 200]
+dnew = Jchemo.predict(fm, xnew)
+dnew / dtot 
+dnew / mu_unif 
+
 n = 10^3 
 x = randn(n)
-
-fm = dens(x)
-pnames(fm)
-fm.d     # Density (normalized to sum to 1)
-fm.lims  # Range of x
-fm.mu    # Mean expected density value for a uniform distribution
-f, ax = lines(fm.d.x, fm.d.d;
-    axis = (xlabel = "x", ylabel = "Density"))
-hlines!(ax, fm.mu; color = :grey, linestyle = "-")
-f
-
-xnew = [-4; -1; -3.8; 1; 4]
-d = Jchemo.predict(fm, xnew).d
-scatter!(ax, xnew, d.d; color = :red)
+lims = (minimum(x), maximum(x))
+#lims = (-6, 6)
+m = 2^8
+bw = .1
+fm = kde1(x; npoints = m, 
+    bandwidth = bw,    # Default = Silverman's rule
+    boundary = lims
+    ) ;
+f = Figure(resolution = (500, 350))
+ax = Axis(f[1, 1];
+    xlabel = "x", ylabel = "Density")
+hist!(ax, x; bins = 30, normalization = :pdf)  # area = 1
+lines!(ax, fm.x, fm.density;
+    color = :red)
 f
 ```
 """ 
