0773ddd9fcd80984a5d1edffb76a4a0bef3b0165
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-05 15:16:51 +0200

    Update dmkern.jl

--- src/dmkern.jl
+++ src/dmkern.jl
@@ -5,6 +5,70 @@ struct Dmkern
     detH::Float64
 end
 
+"""
+    dmkern(X; H = nothing, a = .5)
+Gaussian kernel density estimation (KDE).
+* `x` : Univariate data.
+* `kwargs` : Optional arguments to pass in function `kde` of `KernelDensity.jl`.
+
+
+
+
+## References 
+
+
+## Examples
+```julia
+using CairoMakie
+
+n = 10^4
+x = randn(n)
+lims = (minimum(x), maximum(x))
+#lims = (-10, 10)
+k = 2^3
+bw = .1
+fm = kde1(x; npoints = k, 
+    bandwidth = bw,     # default: Silverman's rule
+    #boundary = lims    # default: See KernelDensity
+    ) ;
+fm.x
+diff(fm.x)
+ds = fm.density    # densities with same scale as ':pdf' in Makie.hist
+sum(ds * diff(fm.x)[1])  # = 1
+## Normalization to sum to 1
+dstot = sum(ds)
+dsn = ds / dstot 
+sum(dsn)
+## Standardization to a uniform distribution
+## (if > 1 ==> "dense" areas) 
+mu_unif = mean(ds)
+ds / mu_unif 
+
+## Prediction
+xnew = [-200; -100; -1; 0; 1; 200]
+pred = Jchemo.predict(fm, xnew).pred    # densities
+pred / dstot 
+pred / mu_unif 
+
+n = 10^3 
+x = randn(n)
+lims = (minimum(x), maximum(x))
+#lims = (-6, 6)
+k = 2^8
+bw = .1
+fm = kde1(x; npoints = k, 
+    bandwidth = bw,         # Default = Silverman's rule
+    boundary = lims
+    ) ;
+f = Figure(resolution = (500, 350))
+ax = Axis(f[1, 1];
+    xlabel = "x", ylabel = "Density")
+hist!(ax, x; bins = 30, normalization = :pdf)  # area = 1
+lines!(ax, fm.x, fm.density;
+    color = :red)
+f
+```
+""" 
 function dmkern(X; H = nothing, a = .5)
     X = ensure_mat(X)
     n, p = size(X)
@@ -26,6 +90,12 @@ function dmkern(X; H = nothing, a = .5)
     Dmkern(X, H, Hinv, detH)
 end
 
+"""
+    predict(object::Dmkern, x)
+Compute predictions from a fitted model.
+* `object` : The fitted model.
+* `x` : Data (vector) for which predictions are computed.
+""" 
 function predict(object::Dmkern, X)
     X = ensure_mat(X)
     n, p = size(object.X)
