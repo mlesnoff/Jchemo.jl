33e662e11f8d1ca8657a540782afe058db825ab5
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-07 12:30:27 +0200

    Update plskdeda.jl

--- src/plskdeda.jl
+++ src/plskdeda.jl
@@ -1,3 +1,67 @@
+"""
+    plskdeda(X, y, weights = ones(nro(X)); nlv, 
+        prior = "unif", h = nothing, a = 1, scal = false)
+KDE-LDA on PLS latent variables (PLS-KDE-LDA).
+* `X` : X-data.
+* `y` : y-data (class membership).
+* `weights` : Weights of the observations. 
+    Internally normalized to sum to 1. 
+* `nlv` : Nb. latent variables (LVs) to compute.
+* `prior` : Type of prior probabilities for class membership.
+    Posible values are: "unif" (uniform), "prop" (proportional).
+* `h` : See `?dmkern`.
+* `h` : See `?dmkern`.
+* `scal` : Boolean. If `true`, each column of `X` 
+    is scaled by its uncorrected standard deviation.
+
+The principle is the same as function `plsqda` except that densities
+are estimated from  `dmkern` instead of  `dmnorm`. 
+
+## Examples
+```julia
+using JLD2, 
+mypath = dirname(dirname(pathof(JchemoData)))
+db = joinpath(mypath, "data", "iris.jld2") 
+@load db dat
+pnames(dat)
+summ(dat.X)
+  
+X = dat.X[:, 1:4] 
+y = dat.X[:, 5]
+n = nro(X)
+  
+ntrain = 120
+s = sample(1:n, ntrain; replace = false) 
+Xtrain = X[s, :]
+ytrain = y[s]
+Xtest = rmrow(X, s)
+ytest = rmrow(y, s)
+
+tab(ytrain)
+tab(ytest)
+aggstat(Matrix(Xtrain), ytrain).X
+
+nlv = 2
+fm = plskdeda(Xtrain, ytrain ; nlv = nlv) ;
+pred = Jchemo.predict(fm, Xtest).pred
+tab(pred)
+err(pred, ytest)
+
+nlv = 2
+fm = plsqda(Xtrain, ytrain ; nlv = nlv) ;
+pred = Jchemo.predict(fm, Xtest).pred
+tab(pred)
+err(pred, ytest)
+
+nlv = 2
+fm = plskdeda(Xtrain, ytrain; nlv = nlv) ;
+Jchemo.predict(fm, Xtest).posterior
+Jchemo.predict(fm, Xtest; nlv = 1).posterior
+nlv = 1:2
+Jchemo.predict(fm, Xtest; nlv = nlv).posterior[1]
+Jchemo.predict(fm, Xtest; nlv = nlv).posterior[2]
+```
+""" 
 function plskdeda(X, y, weights = ones(nro(X)); nlv, 
         prior = "unif", h = nothing, a = 1, scal = false)
     res = dummy(y)
