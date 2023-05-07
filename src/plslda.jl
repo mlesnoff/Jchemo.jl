30697d6bda95389b380aed14d2308416bf7d4546
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-07 12:32:07 +0200

    Update plslda.jl

--- src/plslda.jl
+++ src/plslda.jl
@@ -27,6 +27,7 @@ returning `nlv` latent variables (LVs). Finally, a LDA is run on these LVs and `
 ## Examples
 ```julia
 using JLD2
+using JchemoData
 path_jdat = dirname(dirname(pathof(JchemoData)))
 db = joinpath(path_jdat, "data/forages.jld2") 
 @load db dat
