e7bac3a8f99d441357a4845ac1a750d603cc4126
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-23 21:06:04 +0200

    Update difmean.jl

--- src/difmean.jl
+++ src/difmean.jl
@@ -1,27 +1,23 @@
-# """
-#    bop(X1, X2; nlv)
-# Build by Monte Carlo a detrimental matrix for calibration transfer without standard samples.
-# * `X1` : Target spectra, (n1, p).
-# * `X2` : Spectra to transfer to the target, (n, p).
-# * `nlv` : Function used for fitting the transfer model.  
-#
-# The function returns a matrix D containing the detrimental information
-# that has to be removed from spectra `X1` and `X2` by orthogonalization, 
-# for instance by method EPO (see `?eposvd`). Matrix D is computed 
-# by Monte Carlo sampling.
-#
-# ## References
-#
-# ## Examples
-# ```julia
-# ```
+"""
+    difmean(X1, X2)
+Compute a matrix of column means difference between two matrices.
+* `X1` : Matrix of pectra (n1, p).
+* `X2` : Matrix of pectra (n2, p).
 
-## Difference beween mean spectra 
+The function returns a matrix D (1, p) containing the detrimental information
+that has to be removed from spectra `X1` and `X2` by orthogonalization
+(calibration transfer; see function `eposvd`). Matrix D is computed by the 
+difference between the two mean spectra (column means of `X1` and `X2`).
+
+## Examples
+```julia
+```
+"""
 function difmean(X1, X2)
     xmeans1 = colmean(X1)
     xmeans2 = colmean(X2)
-    #xmeans1 ./= norm(xmeans1)
-    #xmeans2 ./= norm(xmeans2)
+    xmeans1 ./= norm(xmeans1)
+    xmeans2 ./= norm(xmeans2)
     D = (xmeans1 - xmeans2)'
     (D = D, xmeans1, xmeans2)
 end
