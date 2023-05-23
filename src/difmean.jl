9bded8b63e9e18ede9b46487333bdb7d9233837d
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-23 21:26:48 +0200

    Update difmean.jl

--- src/difmean.jl
+++ src/difmean.jl
@@ -3,6 +3,8 @@
 Compute a detrimental matrix (for calibration transfer) by column means difference.
 * `X1` : Matrix of spectra (n1, p).
 * `X2` : Matrix of spectra (n2, p).
+* `normx` : Boolean, if * `true`, the vectors of the column means 
+    of `X1` and `X2` are normed before computing the difference.
 
 The function returns a matrix D (1, p) containing the detrimental information
 that has to be removed from spectra `X1` and `X2` for calibration transfer 
@@ -13,11 +15,13 @@ by the difference between the two mean spectra (column means of `X1` and `X2`).
 ```julia
 ```
 """
-function difmean(X1, X2)
+function difmean(X1, X2; normx::Bool = false)
     xmeans1 = colmean(X1)
     xmeans2 = colmean(X2)
-    xmeans1 ./= norm(xmeans1)
-    xmeans2 ./= norm(xmeans2)
+    if normx
+        xmeans1 ./= norm(xmeans1)
+        xmeans2 ./= norm(xmeans2)
+    end
     D = (xmeans1 - xmeans2)'
     (D = D, xmeans1, xmeans2)
 end
