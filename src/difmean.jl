5b7f2de61b104b68f27ff1d67d166e08396f0543
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-23 21:41:43 +0200

    Update difmean.jl

--- src/difmean.jl
+++ src/difmean.jl
@@ -13,6 +13,29 @@ by the difference between the two mean spectra (column means of `X1` and `X2`).
 
 ## Examples
 ```julia
+using JchemoData, JLD2, CairoMakie
+path_jdat = dirname(dirname(pathof(JchemoData)))
+db = joinpath(path_jdat, "data/caltransfer.jld2") 
+@load db dat
+pnames(dat)
+X1cal = dat.X1cal
+X2cal = dat.X2cal
+X1val = dat.X1val
+X2val = dat.X2val
+
+D = difmean(X1cal, X2cal).D 
+res = eposvd(D; nlv = 1)
+## Corrected matrices
+X1 = X1val * res.M    
+X2 = X2val * res.M    
+
+i = 1
+f = Figure(resolution = (500, 300))
+ax = Axis(f[1, 1])
+lines!(X1[i, :]; label = "x1_correct")
+lines!(ax, X2[i, :]; label = "x2_correct")
+axislegend(position = :rb, framevisible = false)
+f
 ```
 """
 function difmean(X1, X2; normx::Bool = false)
