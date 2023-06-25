9cde9d64d011d37819b010d9fad7bc863325207a
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-06-25 21:30:43 +0200

    Update rda.jl

--- src/rda.jl
+++ src/rda.jl
@@ -1,3 +1,55 @@
+"""
+    rda(X, y; gamma, lb, prior = "unif", scal = false)
+Quadratic discriminant analysis  (QDA).
+* `X` : X-data.
+* `y` : y-data (class membership).
+* `prior` : Type of prior probabilities for class membership.
+    Posible values are: "unif" (uniform), "prop" (proportional).
+
+## References
+Friedman JH. Regularized Discriminant Analysis. Journal of the American 
+Statistical Association. 1989; 84(405):165-175. 
+doi:10.1080/01621459.1989.10478752.
+
+
+## Examples
+```julia
+using JchemoData, JLD2, StatsBase
+path_jdat = dirname(dirname(pathof(JchemoData)))
+db = joinpath(path_jdat, "data/iris.jld2") 
+@load db dat
+pnames(dat)
+summ(dat.X)
+
+X = dat.X[:, 1:4] 
+y = dat.X[:, 5]
+n = nro(X)
+
+ntrain = 120
+s = sample(1:n, ntrain; replace = false) 
+Xtrain = X[s, :]
+ytrain = y[s]
+Xtest = rmrow(X, s)
+ytest = rmrow(y, s)
+
+tab(ytrain)
+tab(ytest)
+
+prior = "unif"
+#prior = "prop"
+fm = qda(Xtrain, ytrain; prior = prior) ;
+pnames(fm)
+
+res = Jchemo.predict(fm, Xtest) ;
+pnames(res)
+res.dens
+res.posterior
+res.pred
+err(res.pred, ytest)
+confusion(res.pred, ytest).cnt
+```
+""" 
+
 
 function rda(X, y; gamma, lb, prior = "unif")
     @assert gamma >= 0 && gamma <= 1 "gamma must be in [0, 1]"
