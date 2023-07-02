07fcfe05004a184d3f89177fb62876f9704cb487
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-07-02 12:52:01 +0200

    Update plsqda.jl

--- src/plsqda.jl
+++ src/plsqda.jl
@@ -25,13 +25,14 @@ returning `nlv` latent variables (LVs). Finally, a QDA is run on these LVs and `
 See `?plslda` for examples.
 """ 
 function plsqda(X, y, weights = ones(nro(X)); nlv, 
-        prior = "unif", scal = false)
+        alpha = 0, prior = "unif", scal = false)
     res = dummy(y)
     ni = tab(y).vals
     fm_pls = plskern(X, res.Y, weights; nlv = nlv, scal = scal)
     fm_da = list(nlv)
     @inbounds for i = 1:nlv
-        fm_da[i] = qda(vcol(fm_pls.T, 1:i), y, weights; prior = prior)
+        fm_da[i] = qda(vcol(fm_pls.T, 1:i), y, weights; 
+            alpha = alpha, prior = prior)
     end
     fm = (fm_pls = fm_pls, fm_da = fm_da)
     Plslda(fm, res.lev, ni)
