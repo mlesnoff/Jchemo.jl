5b725b09e570f9ef9e8c7b53fd50d9d29a38c149
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-07-02 12:46:59 +0200

    Update plslda.jl

--- src/plslda.jl
+++ src/plslda.jl
@@ -84,7 +84,6 @@ function plslda(X, y, weights = ones(nro(X)); nlv,
     fm_pls = plskern(X, res.Y, weights; nlv = nlv, scal = scal)
     fm_da = list(nlv)
     @inbounds for i = 1:nlv
-        #fm_da[i] = lda(fm_pls.T[:, 1:i], y; prior = prior)
         fm_da[i] = lda(fm_pls.T[:, 1:i], y, weights; prior = prior)
     end
     fm = (fm_pls = fm_pls, fm_da = fm_da)
