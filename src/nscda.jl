41f592a0ac4cebfd8855af001f99589371aa61df
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-07-17 11:35:44 +0200

    Update nscda.jl

--- src/nscda.jl
+++ src/nscda.jl
@@ -4,6 +4,7 @@ struct Nscda3
     wprior::Vector{Float64}
     ni::Vector{Int64}
     lev::AbstractVector
+    xscales::Vector{Float64}
     weights::Vector{Float64}
 end
 
@@ -13,23 +14,21 @@ function nscda(X, y, weights = ones(nro(X)); delta,
     fms = nsc(X, y, weights;
         delta = delta, scal = scal)
     poolstd_s0 = fms.poolstd .+ fms.s0
-    ni = fms.ni
-    lev = fms.lev
-    nlev = length(lev)
+    nlev = length(fms.lev)
     if isequal(prior, "unif")
         wprior = ones(nlev) / nlev
     elseif isequal(prior, "prop")
-        wprior = mweight(ni)
+        wprior = mweight(fms.ni)
     end
-    Nscda3(fms, poolstd_s0, wprior, ni, 
-        lev, weights)
+    Nscda3(fms, poolstd_s0, wprior, fms.ni, 
+        fms.lev, fms.xscales, weights)
 end
 
 function predict(object::Nscda3, X; meth = 2)
     zX = scale(X, object.xscales)
     m = nro(zX)
-    scale!(zX, poolstd_s0)
-    cts = scale(object.fms.cts, poolstd_s0)
+    scale!(zX, object.poolstd_s0)
+    cts = scale(object.fms.cts, object.poolstd_s0)
     d2 = euclsq(zX, cts) .- 2 * log.(object.wprior)'
     posterior = softmax(-.5 * d2)
     z =  mapslices(argmin, d2; dims = 2)  # if equal, argmax takes the first
