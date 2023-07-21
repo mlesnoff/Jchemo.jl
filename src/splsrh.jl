9b24808fde34f55001aefbe2511c25c60a10abde
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-07-21 09:12:18 +0200

    Update splsrh.jl

--- src/splsrh.jl
+++ src/splsrh.jl
@@ -1,39 +1,27 @@
 """
     splsrh(X, Y, weights = ones(nro(X)); nlv,
-        scal::Bool = false)
+        nvar = 1; scal::Bool = false)
     splsrh!(X::Matrix, Y::Matrix, weights = ones(nro(X)); nlv,
-        scal::Bool = false)
+        nvar = 1, scal::Bool = false)
 Sparse partial least squares regression (SPLSR) by hard thresholding.
 * `X` : X-data (n, p).
 * `Y` : Y-data (n, q).
 * `weights` : Weights (n) of the observations. 
     Internally normalized to sum to 1.
 * `nlv` : Nb. latent variables (LVs) to compute.
+* `nvar` : Nb. variables (`X`-columns) selected for each 
+    latent variable (LV).
 * `scal` : Boolean. If `true`, each column of `X` and `Y` 
     is scaled by its uncorrected standard deviation.
     
-About the row-weighting in PLS algorithms (`weights`), see in particular Schaal et al. 2002, 
-Siccard & Sabatier 2006, Kim et al. 2011, and Lesnoff et al. 2020. 
+The PLSR agorithm is modified as follows. For each LV:
+* The usual PLS weights w1, w2, ..., wp are computed.
+* The weights wj (j = 1, ..., p) are set to 0 except the 
+    `nvar` largest weights in absolute value.  
 
-## References
-Dayal, B.S., MacGregor, J.F., 1997. Improved PLS algorithms. 
-Journal of Chemometrics 11, 73-85.
-
-Kim, S., Kano, M., Nakagawa, H., Hasebe, S., 2011. Estimation of active 
-pharmaceutical ingredients content using locally weighted partial 
-least squares and statistical wavelength selection. Int. J. Pharm., 421, 269-274.
-
-Lesnoff, M., Metz, M., Roger, J.M., 2020. Comparison of locally weighted 
-PLS strategies for regression and discrimination on agronomic NIR Data. 
-Journal of Chemometrics. e3209. 
-https://onlinelibrary.wiley.com/doi/abs/10.1002/cem.3209
-
-Schaal, S., Atkeson, C., Vijayamakumar, S. 2002. Scalable techniques 
-from nonparametric statistics for the real time robot learning. 
-Applied Intell., 17, 49-60.
-
-Sicard, E. Sabatier, R., 2006. Theoretical framework for local PLS1 regression 
-and application to a rainfall data set. Comput. Stat. Data Anal., 51, 1393-1410.
+When `nvar` = 1 and when `Y` is univariate (q = 1), function `splsrh`
+returns the same results as Covsel regression algorithm (see functions 
+`covsel` and `covselr`), but is faster.
 
 ## Examples
 ```julia
@@ -54,20 +42,13 @@ Xtest = rmrow(X, s)
 ytest = rmrow(y, s)
 
 nlv = 15
-fm = splsrh(Xtrain, ytrain; nlv = nlv) ;
-#fm = plsnipals(Xtrain, ytrain; nlv = nlv) ;
-#fm = plsrosa(Xtrain, ytrain; nlv = nlv) ;
-#fm = plssimp(Xtrain, ytrain; nlv = nlv) ;
+fm = splsrh(Xtrain, ytrain; nlv = nlv,
+    nvar = 1) ;  # Covsel regressin
 pnames(fm)
-fm.T
 
 zcoef = Jchemo.coef(fm)
 zcoef.int
 zcoef.B
-Jchemo.coef(fm; nlv = 7).B
-
-Jchemo.transform(fm, Xtest)
-Jchemo.transform(fm, Xtest; nlv = 7)
 
 res = Jchemo.predict(fm, Xtest)
 res.pred
@@ -75,10 +56,6 @@ rmsep(res.pred, ytest)
 plotxy(vec(res.pred), ytest; color = (:red, .5),
     bisect = true, xlabel = "Prediction", 
     ylabel = "Observed").f    
-
-res = Jchemo.predict(fm, Xtest; nlv = 1:2)
-res.pred[1]
-res.pred[2]
 
 res = summary(fm, Xtrain) ;
 pnames(res)
