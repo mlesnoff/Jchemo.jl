0935b956e14ad9b8545209b2a409402e035e3cbd
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-07-22 11:01:38 +0200

    Update pcanipals.jl

--- src/pcanipals.jl
+++ src/pcanipals.jl
@@ -1,11 +1,11 @@
 function pcanipals(X, weights = ones(nro(X)); nlv, 
-    gs = true, niter = 100, scal::Bool = false)
+        gs = true, niter = 100, scal::Bool = false)
     pcanipals!(copy(ensure_mat(X)), weights; nlv = nlv, 
         gs = gs, niter = niterscal = scal)
 end
 
 function pcanipals!(X::Matrix, weights = ones(nro(X)); nlv, 
-    gs = true, niter = 100, scal::Bool = false)
+        gs = true, niter = 100, scal::Bool = false)
     n, p = size(X)
     nlv = min(nlv, n, p)
     weights = mweight(weights)
@@ -19,21 +19,22 @@ function pcanipals!(X::Matrix, weights = ones(nro(X)); nlv,
     end
     sqrtw = sqrt.(weights)
     X .= Diagonal(sqrtw) * X
+    t = similar(X, n)
+    T = similar(X, n, nlv)
+    P = similar(X, p, nlv)
+    sv = similar(X, nlv)
+    niter = list(nlv, Int64)
     for a = 1:nlv
         res = nipals(X; tol = tol, maxit = maxit)
-        P[:, a] .= res.u           
-        T[:, a] .= t    
-
-
-
-
-    res = eigen!(Symmetric(X' * X); sortby = x -> -abs(x)) 
-    P = res.vectors[:, 1:nlv]
-    eig = res.values[1:min(n, p)]
-    eig[eig .< 0] .= 0
-    sv = sqrt.(eig)
-    T = Diagonal(1 ./ sqrtw) * X * P
-    
-    Pca(T, P, sv, xmeans, xscales, weights, nothing, nothing) 
+        t .= res.u * res.sv
+        sv[a] = res.sv
+        P[:, a] .= res.v           
+        T[:, a] .= t
+        X .-= t * res.v'
+        t ./= sqrtw
+        niter[a] = res.niter
+    end    
+    #T = Diagonal(1 ./ sqrtw) * X * P
+    Pca(T, P, sv, xmeans, xscales, weights, niter, nothing) 
 end
 
