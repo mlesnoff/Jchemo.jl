7ac796d11108a31e0b7b1a03203502f997958a98
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-07-22 09:40:15 +0200

    Create pcanipals.jl

---
+++ src/pcanipals.jl
@@ -0,0 +1,27 @@
+function pcanipals(X, weights = ones(nro(X)); nlv, scal::Bool = false)
+    pcanipals!(copy(ensure_mat(X)), weights; nlv = nlv, scal = scal)
+end
+
+function pcanipals!(X::Matrix, weights = ones(nro(X)); nlv, scal::Bool = false)
+    n, p = size(X)
+    nlv = min(nlv, n, p)
+    weights = mweight(weights)
+    xmeans = colmean(X, weights) 
+    xscales = ones(p)
+    if scal 
+        xscales .= colstd(X, weights)
+        cscale!(X, xmeans, xscales)
+    else
+        center!(X, xmeans)
+    end
+    sqrtw = sqrt.(weights)
+    X .= Diagonal(sqrtw) * X
+    res = eigen!(Symmetric(X' * X); sortby = x -> -abs(x)) 
+    P = res.vectors[:, 1:nlv]
+    eig = res.values[1:min(n, p)]
+    eig[eig .< 0] .= 0
+    sv = sqrt.(eig)
+    T = Diagonal(1 ./ sqrtw) * X * P
+    Pca(T, P, sv, xmeans, xscales, weights, nothing, nothing) 
+end
+
