f38acf0ed7f3d22bc1eb474afcf7aca888a94ef9
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-07-25 16:32:22 +0200

    Update pcanipals.jl

--- src/pcanipals.jl
+++ src/pcanipals.jl
@@ -47,9 +47,6 @@ function pcanipals!(X::Matrix, weights = ones(nro(X)); nlv,
     sqrtw = sqrt.(weights)
     X .= Diagonal(sqrtw) * X
     t = similar(X, n)
-    u = copy(t)
-    u0 = copy(t)
-    v = similar(X, p)
     T = similar(X, n, nlv)
     P = similar(X, p, nlv)
     sv = similar(X, nlv)
@@ -59,37 +56,21 @@ function pcanipals!(X::Matrix, weights = ones(nro(X)); nlv,
         VVt = zeros(p, p)
     end
     for a = 1:nlv
-        u .= X[:, argmax(colnorm(X))]
-        cont = true
-        iter = 1
-        while cont
-            u0 .= copy(u)      
-            mul!(v, X', u)
-            if gs & (a > 1)
-                v .= v .- VVt * v
-            end
-            v ./= norm(v)
-            mul!(u, X, v)
-            if gs & (a > 1)
-                u .= u .- UUt * u 
-            end
-            dif = sum((u .- u0).^2)
-            iter = iter + 1
-            if (dif < tol) || (iter > maxit)
-                cont = false
-            end
+        if gs == false
+            res = nipals(X; tol = tol, maxit = maxit)
+        else
+            res = nipals(X, UUt, VVt; 
+                tol = tol, maxit = maxit)
         end
-        s = norm(u)
-        u ./= s
-        t .= u * s
+        t .= res.u * res.sv
         T[:, a] .= t ./ sqrtw
-        P[:, a] .= v           
-        sv[a] = s
-        niter[a] = iter - 1
-        X .-= t * v'
+        P[:, a] .= res.v           
+        sv[a] = res.sv
+        niter[a] = res.niter
+        X .-= t * res.v'
         if gs
-            UUt .+= u * u' 
-            VVt .+= v * v'
+            UUt .+= res.u * res.u' 
+            VVt .+= res.v * res.v'
         end
     end    
     Pca(T, P, sv, xmeans, xscales, weights, niter) 
