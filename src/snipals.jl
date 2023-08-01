947a358700302a5b65c2cbf382995789eab79024
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-08-01 22:53:56 +0200

    snipals

--- src/snipals.jl
+++ src/snipals.jl
@@ -2,18 +2,18 @@ function snipals(X; delta = 0,
         tol = sqrt(eps(1.)), maxit = 200)
     X = ensure_mat(X)
     n, p = size(X)
-    u = nipals(X; tol = tol, maxit = maxit).u
-    u0 = similar(X, n)
+    res = nipals(X; tol = tol, maxit = maxit)
+    t = res.u * res.sv
+    t0 = similar(X, n)
     v = similar(X, p)
     absv = copy(v)
     absv_stand = copy(v)
     theta = copy(v)
     cont = true
     iter = 1
-    s = 0
     while cont
-        u0 .= copy(u)
-        mul!(v, X', u)
+        t0 .= copy(t)
+        mul!(v, X', t)
         ## Sparsity
         absv .= abs.(v)
         absv_max = maximum(absv)
@@ -21,18 +21,15 @@ function snipals(X; delta = 0,
         theta .= max.(0, absv_stand .- delta) 
         v .= sign.(v) .* theta * absv_max 
         ## End
-        mul!(u, X, v)
-        s = norm(u)
-        u ./= s
         v ./= norm(v)
-        dif = sum((u .- u0).^2)
+        mul!(t, X, v)
+        dif = sum((t .- t0).^2)
         iter = iter + 1
         if (dif < tol) || (iter > maxit)
             cont = false
         end
     end
-    sv = sqrt(s)
     niter = iter - 1
-    (u = u, v, sv, niter)
+    (t = t, v, niter)
 end
 
--- src/spca.jl
+++ src/spca.jl
@@ -40,10 +40,7 @@ function spca!(X::Matrix, weights = ones(nro(X)); nlv,
             res = snipalsh(X; 
                 nvar = nvar[a], tol = tol, maxit = maxit)
         end
-        #t .= res.u * res.sv
-        #t .= res.u * res.s
-        #t .= X * res.v    # Shen & Huang 2008 (2.3 p.1020)
-        t .= res.t
+        t .= res.t   # = PC = X_defl * res.v    
         tt = dot(t, t)
         X .-= t * t' * X / tt        
         T[:, a] .= t ./ sqrtw
@@ -51,6 +48,6 @@ function spca!(X::Matrix, weights = ones(nro(X)); nlv,
         #sv[a] = res.sv
         niter[a] = res.niter
     end    
-    Pca(T, P, sv, xmeans, xscales, weights, niter) 
+    (T = T, P, sv, xmeans, xscales, weights, niter) 
 end
 
