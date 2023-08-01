2656617419f325031dd0f0d23a4bd91a37d95bfa
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-08-01 09:52:35 +0200

    Update spca.jl

--- src/spca.jl
+++ src/spca.jl
@@ -35,10 +35,10 @@ function spca!(X::Matrix, weights = ones(nro(X)); nlv,
                 delta = delta, tol = tol, maxit = maxit)
         elseif meth == "mix"
             res = snipalsmix(X; 
-                nvar = nvar, tol = tol, maxit = maxit)
+                nvar = nvar[a], tol = tol, maxit = maxit)
         elseif meth == "hard"
             res = snipalsh(X; 
-                nvar = nvar, tol = tol, maxit = maxit)
+                nvar = nvar[a], tol = tol, maxit = maxit)
         end
         t .= res.u * res.sv
         tt = dot(t, t)
