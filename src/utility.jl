fb47931247a407b5be04b5922aa1fad3d1f1ee13
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-08-05 17:03:25 +0200

    Update utility.jl

--- src/utility.jl
+++ src/utility.jl
@@ -1204,6 +1204,10 @@ Soft thresholding function.
 * `x` : Value to transform.
 * `delta` : Range for the thresholding.
 
+The returned value is:
+* sign(x) * max(0, abs(x) - delta)
+where delta >= 0.
+
 ## Examples
 ```julia
 delta = .2
