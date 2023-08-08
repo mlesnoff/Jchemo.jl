e471b75785b0f26c3e971a86f27e09d396c0ab54
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-08-08 15:20:57 +0200

    Create splskern.jl

---
+++ src/splskern.jl
@@ -0,0 +1,91 @@
+function splskern(X, Y, weights = ones(nro(X)); nlv, 
+        nvar = nco(X), scal::Bool = false)
+    splskern!(copy(ensure_mat(X)), copy(ensure_mat(Y)), weights; 
+        nvar = nvar, nlv = nlv, scal = scal)
+end
+
+function splskern!(X::Matrix, Y::Matrix, weights = ones(nro(X)); nlv,
+        nvar = nco(X), scal::Bool = false)
+    n, p = size(X)
+    q = nco(Y)
+    nlv = min(n, p, nlv)
+    length(nvar) == 1 ? nvar = repeat([nvar], nlv) : nothing
+    weights = mweight(weights)
+    xmeans = colmean(X, weights) 
+    ymeans = colmean(Y, weights)  
+    xscales = ones(p)
+    yscales = ones(q)
+    if scal 
+        xscales .= colstd(X, weights)
+        yscales .= colstd(Y, weights)
+        cscale!(X, xmeans, xscales)
+        cscale!(Y, ymeans, yscales)
+    else
+        center!(X, xmeans)
+        center!(Y, ymeans)
+    end
+    D = Diagonal(weights)
+    XtY = X' * (D * Y)                   # = Xd' * Y = X' * D * Y  (Xd = D * X   Very costly!!)
+    #XtY = X' * (weights .* Y)           # Can create OutOfMemory errors for very large matrices
+    # Pre-allocation
+    T = similar(X, n, nlv)
+    W = similar(X, p, nlv)
+    P = copy(W)
+    R = copy(P)
+    C = similar(X, q, nlv)
+    TT = similar(X, nlv)
+    t   = similar(X, n)
+    dt  = copy(t)   
+    zp  = similar(X, p)
+    w   = copy(zp)
+    absw = copy(zp)
+    r   = copy(zp)
+    c   = similar(X, q)
+    tmp = similar(XtY) # = XtY_approx
+    # End
+    @inbounds for a = 1:nlv
+        if q == 1
+            w .= vcol(XtY, 1)
+            ## Sparsity
+            nrm = p - nvar[a]
+            if nrm > 0
+                absw .= abs.(w)
+                sel = sortperm(absw; rev = true)[1:nvar[a]]
+                wmax = w[sel]
+                w .= zeros(p)
+                w[sel] .= wmax
+                delta = maximum(sort(absw)[1:nrm])
+                w .= soft.(w, delta)
+            end
+            ## End
+            w ./= norm(w)
+        else
+            #w .= svd(XtY).U[:, 1]
+            w .= snipalsmix(XtY'; nvar = nvar[a]).v
+        end                                  
+        r .= w
+        if a > 1
+            @inbounds for j = 1:(a - 1)
+                r .-= dot(w, vcol(P, j)) .* vcol(R, j)    
+            end
+        end                   
+        mul!(t, X, r)                 # t = X * r
+        dt .= weights .* t            # dt = D * t
+        tt = dot(t, dt)               # tt = t' * dt = t' * D * t 
+        mul!(c, XtY', r)
+        c ./= tt                      # c = XtY' * r / tt
+        mul!(zp, X', dt)              # zp = (D * X)' * t = X' * (D * t)
+        XtY .-= mul!(tmp, zp, c')     # XtY = XtY - zp * c' ; deflation of the kernel matrix 
+        P[:, a] .= zp ./ tt           # ==> the metric applied to covariance is applied outside the loop,
+        T[:, a] .= t                  # conversely to other algorithms such as nipals
+        W[:, a] .= w
+        R[:, a] .= r
+        C[:, a] .= c
+        TT[a] = tt
+     end
+     Plsr(T, P, R, W, C, TT, xmeans, xscales, ymeans, 
+         yscales, weights, nothing)
+end
+
+
+
