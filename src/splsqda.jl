6b6a7a436cd81ff0c5aec0132cacd5323c639ca6
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-08-12 10:17:54 +0200

    Update splsqda.jl

--- src/splsqda.jl
+++ src/splsqda.jl
@@ -1,8 +1,11 @@
 function splsqda(X, y, weights = ones(nro(X)); nlv, 
+        meth = "soft", delta = 0, nvar = nco(X),
         alpha = 0, prior = "unif", scal::Bool = false)
     res = dummy(y)
     ni = tab(y).vals
-    fm_pls = plskern(X, res.Y, weights; nlv = nlv, scal = scal)
+    fm_pls = splskern(X, res.Y, weights; nlv = nlv, 
+        meth = meth, delta = delta, nvar = nvar,
+        scal = scal)
     fm_da = list(nlv)
     @inbounds for i = 1:nlv
         fm_da[i] = qda(vcol(fm_pls.T, 1:i), y, weights; 
