c0a8e463ae106c06ac2323fa8917b34c6ec0902e
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-08-12 10:16:33 +0200

    splslda

--- src/splsqda.jl
+++ src/splsqda.jl
@@ -1,30 +1,4 @@
-"""
-    plsqda(X, y, weights = ones(nro(X)); nlv, 
-        alpha = 0, prior = "unif", scal::Bool = false)
-PLS-QDA (with continuum).
-* `X` : X-data.
-* `y` : y-data (class membership).
-* `weights` : Weights of the observations. 
-    Internally normalized to sum to 1. 
-* `nlv` : Nb. latent variables (LVs) to compute.
-* `alpha` : Scalar (∈ [0, 1]) defining the continuum
-    between QDA (`alpha = 0`; default) and LDA (`alpha = 1`).
-* `prior` : Type of prior probabilities for class membership.
-    Posible values are: "unif" (uniform), "prop" (proportional).
-* `scal` : Boolean. If `true`, each column of `X` 
-    is scaled by its uncorrected standard deviation.
-
-QDA on PLS latent variables. 
-
-The training variable `y` (univariate class membership) is transformed
-to a dummy table (Ydummy) containing nlev columns, where nlev is the number 
-of classes present in `y`. Each column of Ydummy is a dummy variable (0/1). 
-Then, a PLS2 is implemented on `X` and Ydummy, 
-returning `nlv` latent variables (LVs). Finally, a QDA is run on these LVs and `y`.
-
-See `?plslda` for examples.
-""" 
-function plsqda(X, y, weights = ones(nro(X)); nlv, 
+function splsqda(X, y, weights = ones(nro(X)); nlv, 
         alpha = 0, prior = "unif", scal::Bool = false)
     res = dummy(y)
     ni = tab(y).vals
