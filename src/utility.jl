0637c84d552a573cd7d29f9e41911fb2da5f3b7e
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-08-16 10:42:08 +0200

    Update utility.jl

--- src/utility.jl
+++ src/utility.jl
@@ -53,39 +53,6 @@ function aggstat(X::DataFrame; vars, groups, fun = mean)
 end
 
 """
-    center(X, v)
-    center!(X::AbstractMatrix, v)
-Center each column of `X`.
-* `X` : Data.
-* `v` : Centering factors.
-
-## examples
-```julia
-n, p = 5, 6
-X = rand(n, p)
-xmeans = colmean(X)
-center(X, xmeans)
-```
-""" 
-center(X, v) = X .- vec(v)'
-
-function center!(X::AbstractMatrix, v)
-    X .-= vec(v)'
-end
-
-#function center(X, v)
-#    zX = copy(ensure_mat(X))
-#    center!(zX, v)
-#    zX
-#end
-#function center!(X::AbstractMatrix, v)
-#    p = nco(X)
-#    @inbounds for i = 1:p
-#        X[:, i] .= vcol(X, i) .- v[i]
-#    end
-#end
-
-"""
     corm(X, w)
     corm(X, Y, w)
 Compute weighted correlation matrices.
@@ -214,42 +181,6 @@ function covm(X, Y, w)
     center!(zX, colmean(zX, w))
     center!(zY, colmean(zY, w))
     zX' * Diagonal(w) * zY
-end
-
-"""
-    cscale(X, u, v)
-    cscale!(X, u, v)
-Center and scale each column of `X`.
-* `X` : Data.
-* `u` : Centering factors.
-* `v` : Scaling factors.
-
-## examples
-```julia
-n, p = 5, 6
-X = rand(n, p)
-xmeans = colmean(X)
-xstds = colstd(X)
-cscale(X, xmeans, xstds)
-```
-""" 
-function cscale(X, u, v)
-    zX = copy(ensure_mat(X))
-    cscale!(zX, u, v)
-    zX
-end
-
-function cscale!(X::AbstractMatrix, u, v)
-    p = nco(X)
-    @inbounds for i = 1:p
-        X[:, i] .= (vcol(X, i) .- u[i]) ./ v[i]
-    end
-end
-
-# Slower:
-function cscale2!(X::AbstractMatrix, u, v)
-    center!(X, u)
-    scale!(X, v)
 end
 
 """
@@ -861,43 +792,6 @@ function rmrow(X::Vector, s::Union{Vector, BitVector, Number})
     isa(s, BitVector) ? s = findall(s .== 1) : nothing
     X[setdiff(1:end, Int64.(s))]
 end
-
-"""
-    scale(X, v)
-    scale!(X::AbstractMatrix, v)
-Scale each column of `X`.
-* `X` : Data.
-* `v` : Scaling factors.
-
-## Examples
-```julia
-X = rand(5, 2) 
-scale(X, colstd(X))
-```
-""" 
-
-scale3(X, v) = X ./ vec(v)'
-
-function scale3!(X::AbstractMatrix, v)
-    X ./= vec(v)'
-end
-
-
-function scale(X, v)
-    zX = copy(ensure_mat(X))
-    scale!(zX, v)
-    zX
-end
-
-function scale!(X::AbstractMatrix, v)
-    p = nco(X)
-    @inbounds for i = 1:p
-        X[:, i] .= vcol(X, i) ./ v[i]
-    end
-end
-
-# Below: Much slower and requires more memories
-scale2(X, v) = mapslices(function f(x) ; x ./ v ; end, ensure_mat(X), dims = 2)
 
 """
     soft(x::Real, delta)
