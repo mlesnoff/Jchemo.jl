e360004e258577eac53efe6a2121a49066498802
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-25 12:00:11 +0100

    Update isel.jl

--- src/isel.jl
+++ src/isel.jl
@@ -1,70 +1,83 @@
 """
-    isel(X, Y, wl = 1:nco(X); rep = 1, 
-        nint = 5, psamp = 1/3, fun, 
-        score = rmsep, kwargs...)
+    isel(mod, X, Y, wl = 1:nco(X); 
+        rep = 1, nint = 5, psamp = 1 / 3, 
+        score = rmsep)
 Interval variable selection.
 * `X` : X-data (n, p).
 * `Y` : Y-data (n, q).
-* `wl` : Optional numeric labels (p, 1) of the X-columns.  
-* `rep` : Number of replications. 
+* `wl` : Optional numeric labels (p, 1) of the X-columns.
+Keyword arguments:  
+* `mod` : The model to evaluate.
+* `rep` : Number of replications of the splitting
+    training/test. 
 * `nint` : Nb. intervals. 
-* `psamp` : Proportion of data used as test set to compute the `score`
-    (default: n/3 of the data).
-* `score` : Function computing the prediction score (= error rate; e.g. msep).
-* `fun` : Function defining the prediction model.
-* `kwarg` : Optional other arguments to pass to funtion defined in `fun`.
+* `psamp` : Proportion of data used as test set 
+    to compute the `score`.
+* `score` : Function computing the prediction score.
 
 The principle is as follows:
 * Data (X, Y) are splitted randomly to a training and a test set.
-* Range 1:p in `X` is segmented to `nint` intervals of equal (when possible) size. 
-* The model is fitted on the training set and the score (error rate) on the test set, 
-    firtsly accounting for all the p variables (reference) and secondly 
-    for each of the `nint` intervals. 
-* This process is replicated `rep` times. Average results are provided in the outputs,
-    as well the results per replication. 
+* Range 1:p in `X` is segmented to `nint` intervals, when possible 
+    of equal size. 
+* The model is fitted on the training set and the score (error rate) 
+    on the test set, firtsly accounting for all the p variables 
+    (reference) and secondly for each of the `nint` intervals. 
+* This process is replicated `rep` times. Average results are provided 
+    in the outputs, as well the results per replication. 
 
 ## References
 - Nørgaard, L., Saudland, A., Wagner, J., Nielsen, J.P., Munck, L., 
 Engelsen, S.B., 2000. Interval Partial Least-Squares Regression (iPLS): 
 A Comparative Chemometric Study with an Example from Near-Infrared 
-Spectroscopy. Appl Spectrosc 54, 413–419. https://doi.org/10.1366/0003702001949500
+Spectroscopy. Appl Spectrosc 54, 413–419. 
+https://doi.org/10.1366/0003702001949500
 
 ## Examples
 ```julia
-using JchemoData, DataFrames, JLD2
-using CairoMakie
-
-path_jdat = dirname(dirname(pathof(JchemoData)))
-db = joinpath(path_jdat, "data/tecator.jld2") 
+using DataFrames, JLD2, CairoMakie
+using JchemoData
+mypath = dirname(dirname(pathof(JchemoData)))
+db = joinpath(mypath, "data", "tecator.jld2") 
 @load db dat
 pnames(dat)
-
 X = dat.X
 Y = dat.Y 
-wlst = names(X)
-wl = parse.(Float64, wlst) 
+wl_str = names(X)
+wl = parse.(Float64, wl_str) 
+ntot, p = size(X)
 typ = Y.typ
-y = Y.fat
-
-f = 15 ; pol = 3 ; d = 2 
-Xp = savgol(snv(X); f = f, pol = pol, d = d) 
+namy = names(Y)[1:3]
+plotsp(X, wl; xlabel = "Wavelength (nm)", 
+    ylabel = "Absorbance").f
 
 s = typ .== "train"
-Xtrain = Xp[s, :]
-ytrain = y[s]
+Xtrain = X[s, :]
+Ytrain = Y[s, namy]
+Xtest = rmrow(X, s)
+Ytest = rmrow(Y[:, namy], s)
+ntrain = nro(Xtrain)
+ntest = nro(Xtest)
+ntot = ntrain + ntest
+(ntot = ntot, ntrain, ntest)
 
+## Work on the j-th 
+## y-variable 
+j = 2
+nam = namy[j]
+ytrain = Ytrain[:, nam]
+ytest = Ytest[:, nam]
+
+mod = plskern(nlv = 5)
 nint = 10
-nlv = 5
-res = isel(Xtrain, ytrain, wl; rep = 20, 
-    nint = nint, fun = plskern, nlv = nlv) ;
+res = isel(mod, Xtrain, ytrain, wl; 
+    rep = 30, nint) ;
 res.res_rep
 res.res0_rep
 zres = res.res
 zres0 = res.res0
-
-f = Figure(size = (900, 400))
+f = Figure(size = (650, 300))
 ax = Axis(f[1, 1],
-    xlabel = "Wawelength (nm)", ylabel = "RMSEP",
+    xlabel = "Wawelength (nm)", ylabel = "RMSEP_Val",
     xticks = zres.lo)
 scatter!(ax, zres.mid, zres.y1; color = (:red, .5))
 vlines!(ax, zres.lo; color = :grey,
@@ -73,9 +86,9 @@ hlines!(ax, zres0.y1, linestyle = :dash)
 f
 ```
 """
-function isel(X, Y, wl = 1:nco(X); rep = 1, 
-        nint = 5, psamp = 1/3, fun, 
-        score = rmsep, kwargs...)
+function isel(mod, X, Y, wl = 1:nco(X); 
+        rep = 1, nint = 5, psamp = 1 / 3, 
+        score = rmsep)
     X = ensure_mat(X)
     Y = ensure_mat(Y) 
     n, p = size(X)
@@ -102,15 +115,15 @@ function isel(X, Y, wl = 1:nco(X); rep = 1,
         Ycal .= rmrow(Y, s)
         Xval .= X[s, :]
         Yval .= Y[s, :]
-        ## All variables (Ref0)
-        fm = fun(Xcal, Ycal; kwargs...)
-        pred = predict(fm, Xval).pred
+        ## All variables ('res0')
+        fit!(mod, Xcal, Ycal)
+        pred = predict(mod, Xval).pred
         res0_rep[:, :, i] = score(pred, Yval)
         ## Intervals
         @inbounds for j = 1:nint
             u = int[j, 1]:int[j, 2]
-            fm = fun(vcol(Xcal, u), Ycal; kwargs...)
-            pred .= predict(fm, vcol(Xval, u)).pred
+            fit!(mod, vcol(Xcal, u), Ycal)
+            pred = predict(mod, vcol(Xval, u)).pred
             zres[j] = score(pred, Yval)
         end
         ## End
@@ -126,7 +139,8 @@ function isel(X, Y, wl = 1:nco(X); rep = 1,
     res = hcat(dat, res)
     res0 = mean(res0_rep, dims = 3)[:, :, 1]
     res0 = DataFrame(res0, Symbol.(namy))
-    (res = res, res0, res_rep, res0_rep, int)
+    (res = res, res0, 
+        res_rep, res0_rep, int)
 end
 
 
