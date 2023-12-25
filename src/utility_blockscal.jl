7add3e1d59c9d008fa2324501fc1bf214fe5b028
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-25 21:30:30 +0100

    Update utility_blockscal.jl

--- src/utility_blockscal.jl
+++ src/utility_blockscal.jl
@@ -1,36 +1,49 @@
 """
     fblockscal(Xbl; bscales)
-    fblockscal_frob(Xbl)
-    fblockscal_frob(Xbl, weights = ones(nro(X[1]))
-    fblockscal_mfa(Xbl, weights = ones(nro(X[1]))
+    fblockscal_frob(Xbl, weights::Weight)
+    fblockscal_mfa(Xbl, weights::Weight)
     fblockscal_ncol(Xbl)
-    fblockscal_sd(Xbl, weights = ones(nro(X[1]))
+    fblockscal_sd(Xbl, weights::Weight)
 Scale a list of blocks (matrices).
-* `Xbl` : List (vector) of blocks (matrices) of X-data. 
-* `weights` : Weights of the observations (rows of the blocks). 
-* `bscales` : A vector (of length equal to the nb. blocks) of the scalars diving the blocks.
+* `Xbl` : List (vector) of blocks (matrices) of 
+    X-data. 
+* `weights` : Weights (n) of the observations (rows 
+    of the blocks). Must be of type `Weight` (see e.g. 
+    function `mweight`).
+Keyword arguments: 
+* `bscales` : A vector (of length equal to the nb. 
+    of blocks) of the scalars diving the blocks.
 
 Specificities of each function:
-* `fblockscal`: Each block X is tranformed to X / `bscales'.
-* `fblockscal_frob`: Let D be the diagonal matrix of vector `weights`,
-    standardized to sum to 1. Each block X is divided by its Frobenius norm 
-    = sqrt(tr(X' * D * X)). After this scaling, tr(X' * D * X) = 1.
+* `fblockscal`: Each block X is tranformed 
+    to X / `bscales'.
+* `fblockscal_frob`: Let D be the diagonal matrix 
+    of vector `weights.w`. Each block X is divided by 
+    its Frobenius norm  = sqrt(tr(X' * D * X)). After 
+    this scaling, tr(X' * D * X) = 1.
 * `fblockscal_mfa`: Each block X is divided by sqrt(lamda),
-    where lambda is the dominant eigenvalue of X (this is the "MFA" approach).
-* `fblockscal_ncol`: Each block X is divided by the nb. columns of the block.
-* `fblockscal_sd`: Each block X is divided by sqrt(sum(weighted variances of the block-columns)).
-    After this scaling, sum(weighted variances of the block-columns) = 1.
+    where lambda is the dominant eigenvalue of X 
+    (this is the "MFA" approach).
+* `fblockscal_ncol`: Each block X is divided by the nb. 
+    of columns of the block.
+* `fblockscal_sd`: Each block X is divided by 
+    sqrt(sum(weighted variances of the block-columns)). After 
+    this scaling, sum(weighted variances of 
+    the block-columns) = 1.
 
-The functions return the scaled blocks and the scaling values.
+Each function returns the scaled blocks and the scaling values.
 
 ## Examples
 ```julia
-n = 5 ; p = 10 
+using LinearAlgebra
+
+n = 5 ; m = 3 ; p = 10 
 X = rand(n, p) 
-Xnew = X[1:3, :]
+Xnew = rand(m, p)
 
 listbl = [3:4, 1, [6; 8:10]]
 Xbl = mblock(X, listbl) 
+Xblnew = mblock(Xnew, listbl) 
 
 Xbl[1]
 Xbl[2]
@@ -39,37 +52,37 @@ Xbl[3]
 bscales = ones(3)
 res = fblockscal(Xbl, bscales) ;
 res.bscales
-res.X[3]
+res.Xbl[3]
 Xbl[3]
 
 w = ones(n)
-#w = mweight(rand(n))
-D = Diagonal(mweight(w))
-res = fblockscal_frob(Xbl, w) ;
-res.bscales
-k = 3 ; tr(Xbl[k]' * D * Xbl[3])^.5
-tr(res.X[k]' * D * res.X[k])^.5
+#w = rand(n)
+weights = mweight(w) 
 
-w = ones(n)
-#w = mweight(rand(n))
-res = fblockscal_mfa(Xbl, w) ;
+res = fblockscal_frob(Xbl, weights) ;
 res.bscales
-k = 3 ; pcasvd(Xbl[k], w; nlv = 1).sv[1]
+i = 3
+D = Diagonal(weights.w)
+tr(res.Xbl[i]' * D * res.Xbl[i])^.5
+
+res = fblockscal_mfa(Xbl, weights) ;
+i = 3
+res.bscales[i]
+pcasvd(Xbl[i], weights).sv[1]
 
 res = fblockscal_ncol(Xbl) ;
 res.bscales
-res.X[3]
-Xbl[3] / size(Xbl[3], 2)
+i = 3
+res.Xbl[i]
+Xbl[3] / nco(Xbl[3])
 
-w = ones(n)
-#w = mweight(rand(n))
-res = fblockscal_sd(Xbl, w) ;
+res = fblockscal_sd(Xbl, weights) ;
 res.bscales
-sum(colvar(res.X[3], w))
+i = 3
+sum(colvar(res.Xbl[i], weights))
 
-# To concatenate the returned blocks
-
-X_concat = reduce(hcat, res.X)
+## To concatenate the returned blocks
+X_concat = reduce(hcat, res.Xbl)
 ```
 """
 function fblockscal(Xbl, bscales)
