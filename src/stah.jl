fc39db2cbf7a6fe239413b5b4ccefbc8711a2365
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-25 20:57:36 +0100

    Update stah.jl

--- src/stah.jl
+++ src/stah.jl
@@ -1,37 +1,44 @@
 """
     stah(X, a; scal = true)
-Stahel-Donoho outlierness.
-
+Compute the Stahel-Donoho outlierness.
 * `X` : X-data.
-* `a` : Nb. dimensions simulated for the projection pursuit method.
-* `scal` : Boolean. If `true`, matrix `X` is centred (by median) 
-    and scaled (by MAD) before computing the outlierness.
+* `a` : Nb. dimensions simulated for the 
+    projection pursuit method.
+Keyword arguments:
+* `scal` : Boolean. If `true`, matrix `X` is centred 
+    (by median) and scaled (by MAD) before computing 
+    the outlierness.
 
-The outlierness measure is computed from a projection-pursuit approach:
-* directions in the column-`X` space (linear combinations of the columns 
-    of `X`) are randomly simulated, 
-* and the observations (rows of `X`) are projected on these directions.
+The outlierness measure is computed from a projection-pursuit 
+approach:
+* directions in the column-`X` space (linear combinations of 
+    the columns of `X`) are randomly simulated, 
+* and the observations (rows of `X`) are projected on 
+    these directions.
 
 See Maronna and Yohai (1995) for details. 
 
 ## References
-Maronna, R.A., Yohai, V.J., 1995. The Behavior of the Stahel-Donoho Robust Multivariate Estimator. 
-Journal of the American Statistical Association 90, 330–341. https://doi.org/10.1080/01621459.1995.10476517
+Maronna, R.A., Yohai, V.J., 1995. The Behavior of the Stahel-Donoho Robust 
+Multivariate Estimator. Journal of the American Statistical Association 90, 
+330–341. https://doi.org/10.1080/01621459.1995.10476517
 
 ## Examples
 ```julia
-using StatsBase
-
-n = 300 ; p = 700 ; m = 80 ; ntot = n + m
+n = 300 ; p = 700 ; m = 80
+ntot = n + m
 X1 = randn(n, p)
-X2 = randn(m, p) .+ sample(1:3, p)'
+X2 = randn(m, p) .+ rand(1:3, p)'
 X = vcat(X1, X2)
 
-a = 100
-res = stah(X, a; scal = true) ;
-res.d # outlierness
-
+a = 10
+scal = false
+#scal = true
+res = stah(X, a; scal) ;
+pnames(res)
+res.d
 plotxy(1:nro(X), res.d).f
+
 ```
 """ 
 function stah(X, a; scal = true) 
