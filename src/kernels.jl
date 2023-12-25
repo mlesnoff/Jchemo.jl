55a2d4c878b89b53a4d28034e132f48d785b25cf
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-25 15:04:11 +0100

    Update kernels.jl

--- src/kernels.jl
+++ src/kernels.jl
@@ -1,26 +1,29 @@
 """
-    krbf(X, Y; gamma = 1)
-Compute a Radial-Basis-Function (RBF) kernel Gram matrix. 
-* `X` : Data.
-* `Y` : Data.
-* `gamma` : Scale parameter.
+    krbf(X, Y; kwargs...)
+Compute a Radial-Basis-Function (RBF) kernel 
+    Gram matrix. 
+* `X` : X-data (n, p).
+* `Y` : Y-data (m, p).
+Keyword arguments:
+* `gamma` : Scale parameter (default = 1).
 
-Given matrices `X` and `Y`of sizes (n, p) and (m, p), respectively, the function returns
-the (n, m) Gram matrix K(X, Y) = Phi(X) * Phi(Y)'.
+Given matrices `X` and `Y`of sizes (n, p) and (m, p), 
+respectively, the function returns the (n, m) Gram matrix:
+* K(X, Y) = Phi(X) * Phi(Y)'.
 
 The RBF kernel between two vectors x and y is computed by 
-exp(-gamma * ||x - y||^2).
+exp(-`gamma` * ||x - y||^2).
 
 ## References 
-Scholkopf, B., Smola, A.J., 2002. Learning with kernels: support vector machines, 
-regularization, optimization, and beyond, Adaptive computation and machine learning. 
-MIT Press, Cambridge, Mass.
+Scholkopf, B., Smola, A.J., 2002. Learning with kernels: support 
+vector machines, regularization, optimization, and beyond, Adaptive 
+computation and machine learning. MIT Press, Cambridge, Mass.
 
 ## Examples
 ```julia
 X = rand(5, 3)
 Y = rand(2, 3)
-krbf(X, Y)
+krbf(X, Y; gamma = .1)
 ```
 """ 
 function krbf(X, Y; kwargs...)
@@ -33,30 +36,33 @@ function krbf(X, Y; kwargs...)
 end
 
 """
-    kpol(X, Y; degree = 1, gamma = 1, coef0 = 0)
+    kpol(X, Y; kwargs...)
 Compute a polynomial kernel Gram matrix. 
-* `X` : Data.
-* `Y` : Data.
-* `degree` : Degree of the polynom.
-* `gamma` : Scale of the polynom.
-* `coef0` : Offset of the polynom.
+* `X` : X-data (n, p).
+* `Y` : Y-data (m, p).
+Keyword arguments:
+* `degree` : Degree of the polynom (default = 1).
+* `gamma` : Scale of the polynom (default = 1).
+* `coef0` : Offset of the polynom (default = 0).
 
-Given matrices `X` and `Y`of sizes (n, p) and (m, p), respectively, the function returns
-the (n, m) Gram matrix K(X, Y) = Phi(X) * Phi(Y)'.
+Given matrices `X` and `Y`of sizes (n, p) and (m, p), 
+respectively, the function returns the (n, m) Gram matrix:
+*  K(X, Y) = Phi(X) * Phi(Y)'.
 
-The polynomial kernel between two vectors x and y is computed by 
-(gamma * (x' * y) + coef0)^degree.
+The polynomial kernel between two vectors x and y is 
+computed by (`gamma` * (x' * y) + `coef0`)^`degree`.
 
 ## References 
-Scholkopf, B., Smola, A.J., 2002. Learning with kernels: support vector machines, 
-regularization, optimization, and beyond, Adaptive computation and machine learning. 
-MIT Press, Cambridge, Mass.
+Scholkopf, B., Smola, A.J., 2002. Learning with kernels: support 
+vector machines, regularization, optimization, and beyond, Adaptive 
+computation and machine learning. MIT Press, Cambridge, Mass.
 
 ## Examples
 ```julia
 X = rand(5, 3)
 Y = rand(2, 3)
-kpol(X, Y)
+kpol(X, Y; gamma = .1, 
+    degree = 3)
 ```
 """ 
 function kpol(X, Y; kwargs...)
