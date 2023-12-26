930b0a5147651693fd063d1971b236827e1b82a8
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-26 21:36:21 +0100

    Update gridscore.jl

--- src/gridscore.jl
+++ src/gridscore.jl
@@ -31,8 +31,226 @@ argument `pars. See the examples.
 ```julia
 ######## Regression 
 
-######## Discrimination
+using JLD2, CairoMakie, JchemoData
+mypath = dirname(dirname(pathof(JchemoData)))
+db = joinpath(mypath, "data", "cassav.jld2") 
+@load db dat
+pnames(dat)
+X = dat.X 
+y = dat.Y.tbc
+year = dat.Y.year
+tab(year)
+mod = savgol(npoint = 21, 
+    deriv = 2, degree = 2)
+fit!(mod, X)
+Xp = transf(mod, X)
+s = year .<= 2012
+Xtrain = Xp[s, :]
+ytrain = y[s]
+Xtest = rmrow(Xp, s)
+ytest = rmrow(y, s)
+ntrain = nro(Xtrain)
+ntest = nro(Xtest)
+ntot = ntrain + ntest
+(ntot = ntot, ntrain, ntest)
+## Building Cal and Val 
+## within Train
+nval = Int(round(.3 * ntrain))
+s = samprand(ntrain, nval)
+Xcal = Xtrain[s.train, :]
+ycal = ytrain[s.train]
+Xval = Xtrain[s.test, :]
+yval = ytrain[s.test]
 
+#### Plsr
+mod = plskern()
+nlv = 0:30
+res = gridscore(mod, Xcal, ycal, Xval, yval; 
+    score = rmsep, nlv)
+plotgrid(res.nlv, res.y1; step = 2,
+    xlabel = "Nb. LVs", ylabel = "RMSEP").f
+u = findall(res.y1 .== minimum(res.y1))[1] 
+res[u, :]
+mod = plskern(nlv = res.nlv[u])
+fit!(mod, Xtrain, ytrain)
+pred = predict(mod, Xtest).pred
+@show rmsep(pred, ytest)
+plotxy(vec(pred), ytest; color = (:red, .5),
+    bisect = true, xlabel = "Prediction", 
+    ylabel = "Observed").f    
+
+## Adding pars 
+pars = mpar(scal = [false; true])
+res = gridscore(mod, Xcal, ycal, Xval, yval; 
+    score = rmsep, pars = pars, nlv = nlv)
+typ = res.scal
+plotgrid(res.nlv, res.y1, typ; step = 2,
+    xlabel = "Nb. LVs", ylabel = "RMSEP").f
+u = findall(res.y1 .== minimum(res.y1))[1] 
+res[u, :]
+mod = plskern(nlv = res.nlv[u], 
+    scal = res.scal[u])
+fit!(mod, Xtrain, ytrain)
+pred = predict(mod, Xtest).pred
+@show rmsep(pred, ytest)
+plotxy(vec(pred), ytest; color = (:red, .5),
+    bisect = true, xlabel = "Prediction", 
+    ylabel = "Observed").f    
+
+#### Rr 
+lb = (10).^(-8:.1:3)
+mod = rr() 
+res = gridscore(mod, Xcal, ycal, Xval, yval; 
+    score = rmsep, lb)
+loglb = log.(10, res.lb)
+plotgrid(loglb, res.y1; step = 2,
+    xlabel = "log(lambda)", ylabel = "RMSEP").f
+u = findall(res.y1 .== minimum(res.y1))[1] 
+res[u, :]
+mod = rr(lb = res.lb[u])
+fit!(mod, Xtrain, ytrain)
+pred = predict(mod, Xtest).pred
+@show rmsep(pred, ytest)
+plotxy(vec(pred), ytest; color = (:red, .5),
+    bisect = true, xlabel = "Prediction", 
+    ylabel = "Observed").f    
+    
+## Adding pars 
+pars = mpar(scal = [false; true])
+res = gridscore(mod, Xcal, ycal, Xval, yval; 
+    score = rmsep, pars, lb)
+loglb = log.(10, res.lb)
+typ = string.(res.scal)
+plotgrid(loglb, res.y1, typ; step = 2,
+    xlabel = "log(lambda)", ylabel = "RMSEP").f
+u = findall(res.y1 .== minimum(res.y1))[1] 
+res[u, :]
+mod = rr(lb = res.lb[u],
+    scal = res.scal[u])
+fit!(mod, Xtrain, ytrain)
+pred = predict(mod, Xtest).pred
+@show rmsep(pred, ytest)
+plotxy(vec(pred), ytest; color = (:red, .5),
+    bisect = true, xlabel = "Prediction", 
+    ylabel = "Observed").f    
+
+#### Kplsr 
+mod = kplsr()
+nlv = 0:30
+gamma = (10).^collect(-5:1.:5)
+pars = mpar(gamma = gamma)
+res = gridscore(mod, Xcal, ycal, Xval, yval; 
+    score = rmsep, pars = pars, nlv = nlv)
+loggamma = round.(log.(10, res.gamma), digits = 1)
+plotgrid(res.nlv, res.y1, loggamma; step = 2,
+    xlabel = "Nb. LVs", ylabel = "RMSEP",
+    leg_title = "Log(gamma)").f
+u = findall(res.y1 .== minimum(res.y1))[1] 
+res[u, :]
+mod = kplsr(nlv = res.nlv[u], 
+    gamma = res.gamma[u])
+fit!(mod, Xtrain, ytrain)
+pred = predict(mod, Xtest).pred
+@show rmsep(pred, ytest)
+plotxy(vec(pred), ytest; color = (:red, .5),
+    bisect = true, xlabel = "Prediction", 
+    ylabel = "Observed").f    
+
+#### Lwplsr 
+nlvdis = 15 ; metric = [:mah]
+h = [1 ; 2.5 ; 5] ; k = [50 ; 100] 
+pars = mpar(nlvdis = nlvdis, 
+    metric = metric, h = h, k = k)
+length(pars[1]) 
+nlv = 0:20
+mod = lwplsr()
+res = gridscore(mod, Xcal, ycal, Xval, yval;
+    score = rmsep, pars, nlv, verbose = true)
+group = string.("h=", res.h, " k=", res.k)
+plotgrid(res.nlv, res.y1, group;
+    xlabel = "Nb. LVs", ylabel = "RMSEP").f
+u = findall(res.y1 .== minimum(res.y1))[1] 
+res[u, :]
+mod = lwplsr(nlvdis = res.nlvdis[u],
+    metric = res.metric[u], h = res.h[u], 
+    k = res.k[u], nlv = res.nlv[u])
+fit!(mod, Xtrain, ytrain)
+pred = predict(mod, Xtest).pred
+@show rmsep(pred, ytest)
+plotxy(vec(pred), ytest; color = (:red, .5),
+    bisect = true, xlabel = "Prediction", 
+    ylabel = "Observed").f    
+
+#### Knnr 
+nlvdis = [15; 25] ; metric = [:mah]
+h = [1 ; 2.5 ; 5]
+k = [1; 5; 10; 20; 50 ; 100] 
+pars = mpar(nlvdis = nlvdis, 
+    metric = metric, h = h, k = k)
+length(pars[1]) 
+mod = knnr()
+res = gridscore(mod, Xcal, ycal, Xval, yval;
+    score = rmsep, pars, verbose = true)
+u = findall(res.y1 .== minimum(res.y1))[1] 
+res[u, :]
+mod = knnr(nlvdis = res.nlvdis[u],
+    metric = res.metric[u], h = res.h[u], 
+    k = res.k[u])
+fit!(mod, Xtrain, ytrain)
+pred = predict(mod, Xtest).pred
+@show rmsep(pred, ytest)
+plotxy(vec(pred), ytest; color = (:red, .5),
+    bisect = true, xlabel = "Prediction", 
+    ylabel = "Observed").f    
+
+######## Discrimination
+## The principle is the same as 
+## for regression
+
+using JLD2, CairoMakie, JchemoData
+path_jdat = dirname(dirname(pathof(JchemoData)))
+db = joinpath(path_jdat, "data/forages2.jld2")
+@load db dat
+pnames(dat)
+X = dat.X
+Y = dat.Y
+tab(Y.typ)
+s = Bool.(Y.test)
+Xtrain = rmrow(X, s)
+ytrain = rmrow(Y.typ, s)
+Xtest = X[s, :]
+ytest = Y.typ[s]
+ntrain = nro(Xtrain)
+ntest = nro(Xtest)
+ntot = ntrain + ntest
+(ntot = ntot, ntrain, ntest)
+## Building Cal and Val 
+## within Train
+nval = Int(round(.3 * ntrain))
+s = samprand(ntrain, nval)
+Xcal = Xtrain[s.train, :]
+ycal = ytrain[s.train]
+Xval = Xtrain[s.test, :]
+yval = ytrain[s.test]
+
+#### Plslda
+mod = plslda()
+nlv = 1:30
+prior = [:unif; :prop]
+pars = mpar(prior = prior)
+res = gridscore(mod, Xcal, ycal, Xval, yval; 
+    score = err, pars = pars, nlv = nlv)
+typ = res.prior
+plotgrid(res.nlv, res.y1, typ; step = 2,
+    xlabel = "Nb. LVs", ylabel = "RMSEP").f
+u = findall(res.y1 .== minimum(res.y1))[1] 
+res[u, :]
+mod = plslda(nlv = res.nlv[u], 
+    prior = res.prior[u])
+fit!(mod, Xtrain, ytrain)
+pred = predict(mod, Xtest).pred
+@show err(pred, ytest)
+confusion(pred, ytest).pct
 ```
 """
 function gridscore(mod, Xtrain, Ytrain, X, Y; 
