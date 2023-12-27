e90857fad6af4ca3aee757027b0fcbdbdd55e5c9
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-27 15:23:17 +0100

    Update nipalsmiss.jl

--- src/nipalsmiss.jl
+++ src/nipalsmiss.jl
@@ -1,8 +1,8 @@
 """
     nipalsmiss(X; kwargs...)
     nipalsmiss(X, UUt, VVt; kwargs...)
-Nipals to compute the first score and loading vectors of a matrix
-    with missing data.
+Nipals to compute the first score and loading vectors 
+    of a matrix with missing data.
 * `X` : X-data (n, p).
 * `UUt` : Matrix (n, n) for Gram-Schmidt orthogonalization.
 * `VVt` : Matrix (p, p) for Gram-Schmidt orthogonalization.
@@ -14,12 +14,12 @@ Keyword arguments:
 See function `nipals`. 
 
 ## References
-K.R. Gabriel, S. Zamir, Lower rank approximation of matrices
-by least squares with any choice of weights, 
+K.R. Gabriel, S. Zamir, Lower rank approximation of 
+matrices by least squares with any choice of weights, 
 Technometrics 21 (1979) 489–498.
 
-Wright, K., 2018. Package nipals: Principal Components Analysis 
-using NIPALS with Gram-Schmidt Orthogonalization. 
+Wright, K., 2018. Package nipals: Principal Components 
+Analysis using NIPALS with Gram-Schmidt Orthogonalization. 
 https://cran.r-project.org/
 
 ## Examples
