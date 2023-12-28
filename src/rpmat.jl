75d7088547e0945d9efafc05b74adc77e34d31cd
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-28 15:22:17 +0100

    Update rpmat.jl

--- src/rpmat.jl
+++ src/rpmat.jl
@@ -2,19 +2,21 @@
     rpmatgauss(p, nlv)
 Build a gaussian random projection matrix.
 * `p` : Nb. variables (attributes) to project.
-* `nlv` : Nb. of simulated projection dimensions.
+* `nlv` : Nb. of simulated projection 
+    dimensions.
 
-The function returns a random projection matrix P of dimension 
-`p` x `nlv`. The projection of a given matrix X of size n x `p` is given
-by X * P.
+The function returns a random projection matrix P of 
+dimension `p` x `nlv`. The projection of a given matrix X 
+of size n x `p` is given by X * P.
 
-P is simulated from i.i.d. N(0, 1)/sqrt(`nlv`).
+P is simulated from i.i.d. N(0, 1) / sqrt(`nlv`).
 
 ## References 
-Li, P., Hastie, T.J., Church, K.W., 2006. Very sparse random projections, 
-in: Proceedings of the 12th ACM SIGKDD International Conference on Knowledge 
-Discovery and Data Mining, KDD ’06. Association for Computing Machinery,
-New York, NY, USA, pp. 287–296. https://doi.org/10.1145/1150402.1150436
+Li, P., Hastie, T.J., Church, K.W., 2006. Very sparse random 
+projections, in: Proceedings of the 12th ACM SIGKDD International 
+Conference on Knowledge Discovery and Data Mining, KDD ’06. 
+Association for Computing Machinery, New York, NY, USA, pp. 287–296. 
+https://doi.org/10.1145/1150402.1150436
 
 ## Examples
 ```julia
@@ -28,18 +30,21 @@ end
 
 """
     rpmatli(p, nlv; s = sqrt(p))
-Build a sparse random projection matrix (Achlioptas 2001, Li et al. 2006).
+Build a sparse random projection matrix 
+    (Achlioptas 2001, Li et al. 2006).
 * `p` : Nb. variables (attributes) to project.
-* `nlv` : Nb. final dimensions, i.e. after projection.
-* `s` : Coefficient defining the sparsity of the returned matrix 
-    (higher is `s`, higher is the sparsity).
+* `nlv` : Nb. of simulated projection 
+    dimensions.
+Keyword arguments:
+* `s` : Coefficient defining the sparsity of the 
+    returned matrix (higher is `s`, higher is the sparsity).
 
+The function returns a random projection matrix P of 
+dimension `p` x `nlv`. The projection of a given matrix X 
+of size n x `p` is given by X * P.
 
-The function returns a random projection matrix P of dimension 
-`p` x `nlv`. The projection of a given matrix X of size n x `p` is given
-by X * P.
-
-P is simulated from i.i.d. "p_ij" = 
+Matrix P is simulated from i.i.d. discrete 
+sampling within values: 
 * 1 with prob. 1/(2 * `s`)
 * 0 with prob. 1 - 1 / `s`
 * -1 with prob. 1/(2 * `s`)
@@ -52,14 +57,16 @@ Usual values for `s` are:
 
 ## References 
 Achlioptas, D., 2001. Database-friendly random projections, 
-in: Proceedings of the Twentieth ACM SIGMOD-SIGACT-SIGART Symposium on 
-Principles of Database Systems, PODS ’01. Association for Computing Machinery, 
-New York, NY, USA, pp. 274–281. https://doi.org/10.1145/375551.375608
+in: Proceedings of the Twentieth ACM SIGMOD-SIGACT-SIGART 
+Symposium on Principles of Database Systems, PODS ’01. 
+Association for Computing Machinery, New York, NY, USA, pp. 274–281. 
+https://doi.org/10.1145/375551.375608
 
-Li, P., Hastie, T.J., Church, K.W., 2006. Very sparse random projections, 
-in: Proceedings of the 12th ACM SIGKDD International Conference on Knowledge 
-Discovery and Data Mining, KDD ’06. Association for Computing Machinery,
-New York, NY, USA, pp. 287–296. https://doi.org/10.1145/1150402.1150436
+Li, P., Hastie, T.J., Church, K.W., 2006. Very sparse random 
+projections, in: Proceedings of the 12th ACM SIGKDD International 
+Conference on Knowledge Discovery and Data Mining, KDD ’06. Association 
+for Computing Machinery, New York, NY, USA, pp. 287–296. 
+https://doi.org/10.1145/1150402.1150436
 
 ## Examples
 ```julia
