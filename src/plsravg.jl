4b62408bd40ea7aaee0624a3d258725d5dd31399
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-29 16:26:49 +0100

    Update plsravg.jl

--- src/plsravg.jl
+++ src/plsravg.jl
@@ -1,70 +1,37 @@
 """ 
-    plsravg(X, Y, weights = ones(nro(X)); nlv, 
-        typavg = :unif, typw = :bisquare,
-        alpha = 0, K = 5, rep = 10, scal::Bool = false)
-    plsravg!(X::Matrix, Y::Matrix, weights = ones(nro(X)); nlv, 
-        typavg = :unif, typw = :bisquare, 
-        alpha = 0, K = 5, rep = 10, scal::Bool = false)
-Averaging and stacking PLSR models with different numbers of 
+    plsravg(; kwargs...)
+    plsravg(X, Y; kwargs...)
+    plsravg(X, Y, weights::Weight; 
+        kwargs...)
+    plsravg!(X::Matrix, Y::Matrix, weights::Weight; 
+        kwargs...)
+Averaging PLSR models with different numbers of 
     latent variables (LVs).
 * `X` : X-data (n, p).
-* `Y` : Y-data (n, q). Must be univariate (q = 1) if `typw` != :unif.
+* `Y` : Y-data (n, q).
 * `weights` : Weights (n) of the observations. 
     Must be of type `Weight` (see e.g. function `mweight`).
-* `nlv` : A character string such as "5:20" defining the range of the numbers of LVs 
-    to consider ("5:20": the predictions of models with nb LVS = 5, 6, ..., 20 
-    are averaged). Syntax such as "10" is also allowed ("10": correponds to
-    the single model with 10 LVs).
-* `typavg` : Type of averaging. 
+Keyword arguments:
+* `nlv` : Nb. latent variables (LVs) to compute.
 * `scal` : Boolean. If `true`, each column of `X` and `Y` 
     is scaled by its uncorrected standard deviation.
 
-For `typavg` in {:aic, :bic, :cv}
-* `typw` : Type of weight function. 
-* `alpha` : Pareter of the weight function.
+Ensemblist method where the predictions are computed 
+by averaging the predictions of a set of models built 
+with different numbers of LVs.
 
-For `typavg` = :stack
-* `K` : Nb. of folds segmenting the data in the (K-fold) CV.
-* `rep` : Nb. of repetitions of the K-fold CV. 
-
-Ensemblist method where the predictions are computed by averaging 
-or stacking the predictions of a set of models built with different numbers of 
-LVs.
-
-For instance, if argument `nlv` is set to `nlv = "5:10"`, the prediction for 
-a new observation is the average (eventually weighted) or stacking of the predictions 
-returned by the models with 5 LVS, 6 LVs, ... 10 LVs, respectively.
-
-Possible values of `typavg` are: 
-* :unif : Simple average.
-* :aic : Weighted average based on AIC computed for each model.
-* :bic : Weighted average based on BIC computed for each model.
-* :cv : Weighted average based on RMSEP_CV computed for each model.
-* :shenk : Weighted average using "Shenk et al." weights computed for each model.
-* :stack : Linear stacking. A K-fold CV (eventually repeated) is done and 
-the CV predictions are regressed (multiple linear model without intercept)
-on the observed response data.
-
-For arguments `typw` and `alpha` (weight function): see `?fweight`.
+For instance, if argument `nlv` is set to `nlv = "5:10"`, 
+the prediction for a new observation is the simple average
+of the predictions returned by the models with 5 LVs, 6 LVs, 
+... 10 LVs, respectively.
 
 ## References
-Lesnoff, M., Roger, J.-M., Rutledge, D.N., 2021. Monte Carlo methods for estimating 
-Mallows’s Cp and AIC criteria for PLSR models. Illustration on agronomic spectroscopic NIR data. 
-Journal of Chemometrics n/a, e3369. https://doi.org/10.1002/cem.3369
-
-Lesnoff, M., Andueza, D., Barotin, C., Barre, P., Bonnal, L., Fernández Pierna, J.A., Picard, 
-F., Vermeulen, P., Roger, J.-M., 2022. Averaging and Stacking Partial Least Squares Regression Models 
-to Predict the Chemical Compositions and the Nutritive Values of Forages from Spectral Near 
-Infrared Data. Applied Sciences 12, 7850. https://doi.org/10.3390/app12157850
-
-Shenk, J., Westerhaus, M., Berzaghi, P., 1997. Investigation of a LOCAL calibration 
-procedure for near infrared instruments. 
-Journal of Near Infrared Spectroscopy 5, 223. https://doi.org/10.1255/jnirs.115
-
-Shenk et al. 1998 United States Patent (19). Patent Number: 5,798.526.
-
-Zhang, M.H., Xu, Q.S., Massart, D.L., 2004. Averaged and weighted average partial 
-least squares. Analytica Chimica Acta 504, 279–289. https://doi.org/10.1016/j.aca.2003.10.056
+Lesnoff, M., Andueza, D., Barotin, C., Barre, P., Bonnal, L., 
+Fernández Pierna, J.A., Picard, F., Vermeulen, P., Roger, J.-M., 2022. 
+Averaging and Stacking Partial Least Squares Regression Models 
+to Predict the Chemical Compositions and the Nutritive Values of Forages
+from Spectral Near Infrared Data. Applied Sciences 12, 7850. 
+https://doi.org/10.3390/app12157850
 
 ## Examples
 ```julia
@@ -73,13 +40,12 @@ path_jdat = dirname(dirname(pathof(JchemoData)))
 db = joinpath(path_jdat, "data/forages2.jld2") 
 @load db dat
 pnames(dat)
-  
 X = dat.X 
 Y = dat.Y
-summ(Y)
+@head Y
 y = Y.ndf
 #y = Y.dm
-
+n = nro(X)
 s = Bool.(Y.test)
 Xtrain = rmrow(X, s)
 ytrain = rmrow(y, s)
@@ -87,25 +53,21 @@ Xtest = X[s, :]
 ytest = y[s]
 ntrain = nro(Xtrain)
 ntest = nro(Xtest)
-(ntot = ntot, ntrain, ntest)
+(ntot = n, ntrain, ntest)
 
-nlv = "0:50"
-fm = plsravg(Xtrain, ytrain; nlv = nlv) ;
-res = Jchemo.predict(fm, Xtest)
-res.pred
-rmsep(res.pred, ytest)
+nlv = 0:30
+#nlv = 5:20
+#nlv = 25
+mod = plsravg(; nlv) ;
+fit!(mod, Xtrain, ytrain)
+
+res = predict(mod, Xtest)
+@head res.pred
+res.predlv   # predictions for each nb. of LVs 
+@show rmsep(res.pred, ytest)
 plotxy(res.pred, ytest; color = (:red, .5),
-    bisect = true, xlabel = "Prediction", ylabel = "Observed").f   
-
-fm = plsravg(Xtrain, ytrain; nlv = nlv,
-    typavg = :cv) ;
-res = Jchemo.predict(fm, Xtest)
-res.pred
-rmsep(res.pred, ytest)
-
-predlv = reduce(hcat, res.predlv)
-plotsp(predlv, 0:(nco(predlv) - 1); nsamp = 30).f
-
+    bisect = true, xlabel = "Prediction", 
+    ylabel = "Observed").f    
 ```
 """ 
 function plsravg(X, Y; kwargs...)
