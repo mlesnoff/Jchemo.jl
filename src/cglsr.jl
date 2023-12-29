47588e3a028ce232e41f3a0f3c56987f0f8a3135
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-29 14:57:14 +0100

    Update cglsr.jl

--- src/cglsr.jl
+++ src/cglsr.jl
@@ -1,47 +1,54 @@
 """
     cglsr(; kwargs...)
     cglsr(X, y; kwargs...)
-    cglsr!(X::Matrix, y::Matrix; kwargs...)
-Conjugate gradient algorithm for the normal equations (CGLS; Björck 1996).
+    cglsr!(X::Matrix, y::Matrix; 
+        kwargs...)
+Conjugate gradient algorithm for the normal 
+    equations (CGLS; Björck 1996).
 * `X` : X-data  (n, p).
 * `y` : Univariate Y-data (n).
 Keyword arguments:
 * `nlv` : Nb. CG iterations.
 * `gs` : Boolean. If `true` (default), a Gram-Schmidt 
-    orthogonalization of the normal equation residual vectors 
-    is done.
+    orthogonalization of the normal equation 
+    residual vectors is done.
 * `filt` : Boolean. If `true`, CG filter factors 
     are computed (output `F`). Default = `false`.
-* `scal` : Boolean. If `true`, each column of `X` and `y` 
-    are scaled by its uncorrected standard deviation 
-    (default = `false`).
+* `scal` : Boolean. If `true`, each column of `X` 
+    and `y` are scaled by its uncorrected standard 
+    deviation (default = `false`).
 
 `X` and `y` are internally centered. 
 
-CGLS algorithm "7.4.1" Bjorck 1996, p.289. The part of the code computing the 
-re-orthogonalization (Hansen 1998) and filter factors (Vogel 1987, Hansen 1998) 
-is a transcription (with few adaptations) of the Matlab function `cgls` 
-(Saunders et al. https://web.stanford.edu/group/SOL/software/cgls/; Hansen 2008).
+CGLS algorithm "7.4.1" Bjorck 1996, p.289. The part of the 
+code computing the re-orthogonalization (Hansen 1998) and 
+filter factors (Vogel 1987, Hansen 1998) is a transcription 
+(with few adaptations) of the Matlab function `cgls` (Saunders et al. 
+https://web.stanford.edu/group/SOL/software/cgls/; Hansen 2008).
 
 ## References
-Björck, A., 1996. Numerical Methods for Least Squares Problems, Other Titles in Applied Mathematics. 
-Society for Industrial and Applied Mathematics. https://doi.org/10.1137/1.9781611971484
+Björck, A., 1996. Numerical Methods for Least Squares Problems, 
+Other Titles in Applied Mathematics. Society for Industrial and Applied 
+Mathematics. https://doi.org/10.1137/1.9781611971484
 
-Hansen, P.C., 1998. Rank-Deficient and Discrete Ill-Posed Problems, Mathematical Modeling and Computation. 
-Society for Industrial and Applied Mathematics. https://doi.org/10.1137/1.9780898719697
+Hansen, P.C., 1998. Rank-Deficient and Discrete Ill-Posed Problems, 
+Mathematical Modeling and Computation. Society for Industrial and Applied 
+Mathematics. https://doi.org/10.1137/1.9780898719697
 
 Hansen, P.C., 2008. Regularization Tools version 4.0 for Matlab 7.3. 
 Numer Algor 46, 189–194. https://doi.org/10.1007/s11075-007-9136-9
 
-Manne R. Analysis of two partial-least-squares algorithms for multivariate calibration. Chemometrics Intell.
-Lab. Syst. 1987; 2: 187–197.
+Manne R. Analysis of two partial-least-squares algorithms for 
+multivariate calibration. Chemometrics Intell. Lab. Syst. 1987,
+2: 187–197.
 
-Phatak A, De Hoog F. Exploiting the connection between
-PLS, Lanczos methods and conjugate gradients: alternative proofs of some properties of PLS. J. Chemometrics
-2002; 16: 361–367.
+Phatak A, De Hoog F. Exploiting the connection between PLS, 
+Lanczos methods and conjugate gradients: alternative proofs 
+of some properties of PLS. J. Chemometrics 2002; 16: 361–367.
 
-Vogel, C. R.,  "Solving ill-conditioned linear systems using the conjugate gradient method", 
-Report, Dept. of Mathematical Sciences, Montana State University, 1987.
+Vogel, C. R.,  "Solving ill-conditioned linear systems using 
+the conjugate gradient method", Report, Dept. of Mathematical 
+Sciences, Montana State University, 1987.
 
 ## Examples
 ```julia
@@ -78,7 +85,8 @@ plotxy(vec(pred), ytest; color = (:red, .5),
 cglsr(X, y; kwargs...) = cglsr!(copy(ensure_mat(X)), 
     copy(ensure_mat(y)); kwargs...)
 
-function cglsr!(X::Matrix, y::Matrix; kwargs...)
+function cglsr!(X::Matrix, y::Matrix; 
+        kwargs...)
     par = recovkwargs(Par, kwargs)
     Q = eltype(X)   
     n, p = size(X)
@@ -185,13 +193,14 @@ end
 Compute Y-predictions from a fitted model.
 * `object` : The fitted model.
 * `X` : X-data for which predictions are computed.
-* `nlv` : Nb. iterations, or collection of nb. iterations, to consider. 
-If nothing, it is the maximum nb. iterations.
+* `nlv` : Nb. iterations, or collection of 
+    nb. iterations, to consider. 
 """ 
 function predict(object::Cglsr, X; nlv = nothing)
     X = ensure_mat(X)
     a = nco(object.B)
-    isnothing(nlv) ? nlv = a : nlv = (max(minimum(nlv), 0):min(maximum(nlv), a))
+    isnothing(nlv) ? nlv = a : 
+        nlv = (max(minimum(nlv), 0):min(maximum(nlv), a))
     le_nlv = length(nlv)
     pred = list(Matrix{eltype(X)}, le_nlv)
     @inbounds for i = 1:le_nlv
@@ -201,6 +210,4 @@ function predict(object::Cglsr, X; nlv = nothing)
     le_nlv == 1 ? pred = pred[1] : nothing
     (pred = pred,)
 end
-
-
 
