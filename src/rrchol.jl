6c49abab16aaff9db1ba56ac69abf4100511ef07
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-29 21:14:36 +0100

    Update rrchol.jl

--- src/rrchol.jl
+++ src/rrchol.jl
@@ -1,33 +1,39 @@
 """
-    rrchol(X, Y, weights = ones(nro(X)); lb = .01, 
-        scal::Bool = false)
-    rrchol!(X::Matrix, Y::Matrix, weights = ones(nro(X)); lb = .01,
-        scal::Bool = false)
-Ridge regression (RR) using the Normal equations and a Cholesky factorization.
-* `X` : X-data.
-* `Y` : Y-data.
+    rrchol(; kwargs...)
+    rrchol(X, Y; kwargs...)
+    rrchol(X, Y, weights::Weight; 
+        kwargs...)
+    rrchol!(X::Matrix, Y::Matrix, weights::Weight; 
+        kwargs...)
+Ridge regression (RR) using the Normal equations 
+    and a Cholesky factorization.
+* `X` : X-data (n, p).
+* `Y` : Y-data (n, q).
 * `weights` : Weights (n) of the observations. 
-    Must be of type `Weight` (see e.g. function `mweight`). 
+    Must be of type `Weight` (see e.g. function `mweight`).
+Keyword arguments:
 * `lb` : Ridge regularization parameter "lambda".
-* `scal` : Boolean. If `true`, each column of `X` 
+* `scal` : Boolean. If `true`, each column of `X` and `Y` 
     is scaled by its uncorrected standard deviation.
 
-`X` and `Y` are internally centered. The model is computed with an intercept. 
-
-See function `rr` for eaxamples.
+See function `rr` for examples.
 
 ## References 
-Cule, E., De Iorio, M., 2012. A semi-automatic method to guide the choice 
-of ridge parameter in ridge regression. arXiv:1205.0686.
+Cule, E., De Iorio, M., 2012. A semi-automatic method 
+to guide the choice of ridge parameter in ridge regression. 
+arXiv:1205.0686.
 
-Hastie, T., Tibshirani, R., 2004. Efficient quadratic regularization 
-for expression arrays. Biostatistics 5, 329-340. https://doi.org/10.1093/biostatistics/kxh010
+Hastie, T., Tibshirani, R., 2004. Efficient quadratic 
+regularization for expression arrays. Biostatistics 5, 329-340. 
+https://doi.org/10.1093/biostatistics/kxh010
 
-Hastie, T., Tibshirani, R., Friedman, J., 2009. The elements of statistical learning: data mining, 
+Hastie, T., Tibshirani, R., Friedman, J., 2009. The 
+elements of statistical learning: data mining, 
 inference, and prediction, 2nd ed. Springer, New York.
 
-Hoerl, A.E., Kennard, R.W., 1970. Ridge Regression: Biased Estimation for Nonorthogonal Problems. 
-Technometrics 12, 55-67. https://doi.org/10.1080/00401706.1970.10488634
+Hoerl, A.E., Kennard, R.W., 1970. Ridge Regression: Biased 
+Estimation for Nonorthogonal Problems. Technometrics 12, 55-67. 
+https://doi.org/10.1080/00401706.1970.10488634
 """ 
 function rrchol(X, Y; kwargs...)
     Q = eltype(X[1, 1])
