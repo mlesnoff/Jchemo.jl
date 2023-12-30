a3c2d1fb0334062615efbbb14af7d77fa2061815
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-30 10:05:45 +0100

    Update rfr_dt.jl

--- src/rfr_dt.jl
+++ src/rfr_dt.jl
@@ -1,45 +1,46 @@
 """ 
-    rfr_dt(X, y; n_trees = 10,
-        partial_sampling = .7,  
-        n_subfeatures = -1,
-        max_depth = -1, min_samples_leaf = 5, 
-        min_samples_split = 2, scal::Bool = false, 
-        mth = true, kwargs...)
+    rfr_dt(X, y; kwargs...)
 Random forest regression with DecisionTree.jl.
-* `X` : X-data (n obs., p variables).
-* `y` : Univariate y-data (n obs.).
+* `X` : X-data (n, p).
+* `y` : Univariate y-data (n).
+Keyword arguments:
 * `n_trees` : Nb. trees built for the forest. 
-* `partial_sampling` : Proportion of sampled observations for each tree.
-* `n_subfeatures` : Nb. variables to select at random at each split (default: -1 ==> sqrt(#variables)).
-* `max_depth` : Maximum depth of the decision trees (default: -1 ==> no maximum).
-* `min_sample_leaf` : Minimum number of samples each leaf needs to have.
-* `min_sample_split` : Minimum number of observations in needed for a split.
+* `partial_sampling` : Proportion of sampled 
+    observations for each tree.
+* `n_subfeatures` : Nb. variables to select at random 
+    at each split (default: -1 ==> sqrt(#variables)).
+* `max_depth` : Maximum depth of the decision trees 
+    (default: -1 ==> no maximum).
+* `min_sample_leaf` : Minimum number of samples 
+    each leaf needs to have.
+* `min_sample_split` : Minimum number of observations
+    in needed for a split.
 * `scal` : Boolean. If `true`, each column of `X` 
     is scaled by its uncorrected standard deviation.
-* `mth` : Boolean indicating if a multi-threading is done when new data are 
-    predicted with function `predict`.
-* `kwargs` : Optional named arguments to pass in function `build_forest` 
-    of `DecisionTree.jl`.
+* `mth` : Boolean indicating if a multi-threading is 
+    done when new data are predicted with function `predict`.
 
-The function fits a random forest regression model using package 
-`DecisionTree.jl'.
+The function fits a random forest regression model using 
+package `DecisionTree.jl'.
 
 ## References
-Breiman, L., 1996. Bagging predictors. Mach Learn 24, 123–140. 
-https://doi.org/10.1007/BF00058655
+Breiman, L., 1996. Bagging predictors. Mach Learn 24, 
+123–140. https://doi.org/10.1007/BF00058655
 
-Breiman, L., 2001. Random Forests. Machine Learning 45, 5–32. 
-https://doi.org/10.1023/A:1010933404324
+Breiman, L., 2001. Random Forests. Machine Learning 
+45, 5–32. https://doi.org/10.1023/A:1010933404324
 
 DecisionTree.jl
 https://github.com/JuliaAI/DecisionTree.jl
 
 Genuer, R., 2010. Forêts aléatoires : aspects théoriques, 
-sélection de variables et applications. PhD Thesis. Université Paris Sud - Paris XI.
+sélection de variables et applications. PhD Thesis. 
+Université Paris Sud - Paris XI.
 
-Gey, S., 2002. Bornes de risque, détection de ruptures, boosting : 
-trois thèmes statistiques autour de CART en régression (These de doctorat). 
-Paris 11. http://www.theses.fr/2002PA112245
+Gey, S., 2002. Bornes de risque, détection de ruptures, 
+boosting : trois thèmes statistiques autour de CART en 
+régression (These de doctorat). Paris 11. 
+http://www.theses.fr/2002PA112245
 
 ## Examples
 ```julia
