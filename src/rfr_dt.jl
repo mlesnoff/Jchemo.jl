933c50fb94c1756437a20179d87152d147b39ead
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-30 10:18:58 +0100

    Update rfr_dt.jl

--- src/rfr_dt.jl
+++ src/rfr_dt.jl
@@ -19,6 +19,8 @@ Keyword arguments:
     done when new data are predicted with function `predict`.
 * `scal` : Boolean. If `true`, each column of `X` 
     is scaled by its uncorrected standard deviation.
+* Do `dump(Par(), maxdepth = 1)` to print the default 
+    values of the keyword arguments. 
 
 The function fits a random forest regression model using 
 package `DecisionTree.jl'.
