4905915f04e2828a793e3e2b27dacf656e90c46f
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-12-31 20:16:02 +0100

    Update kdeda.jl

--- src/kdeda.jl
+++ src/kdeda.jl
@@ -1,5 +1,6 @@
 """
-    kdeda(X, y; prior = :unif, h = nothing, a = 1)
+    kdeda(; kwargs...)
+    kde(X, y; kwargs...)
 Discriminant analysis using non-parametric kernel Gaussian 
     density estimation (KDE-DA).
 * `X` : X-data (n, p).
@@ -7,8 +8,8 @@ Discriminant analysis using non-parametric kernel Gaussian
 Keyword arguments:
 * `prior` : Type of prior probabilities for class membership.
     Possible values are: :unif (uniform; default), :prop (proportional).
-* The keyword arguments of function `dmkern` can also be 
-    specified here.
+* Keyword arguments of function `dmkern` (bandwidth definition) 
+    can also be specified here.
 
 The principle is the same as functions `lda` and `qda` 
 except that densities are estimated from function `dmkern` 
@@ -16,53 +17,50 @@ instead of function `dmnorm`.
 
 ## Examples
 ```julia
-using JchemoData, JLD2, StatsBase
+using JchemoData, JLD2
 path_jdat = dirname(dirname(pathof(JchemoData)))
-db = joinpath(path_jdat, "data/iris.jld2") 
+db = joinpath(path_jdat, "data/iris.jld2")
 @load db dat
 pnames(dat)
-summ(dat.X)
-
-X = dat.X[:, 1:4] 
+@head dat.X
+X = dat.X[:, 1:4]
 y = dat.X[:, 5]
 n = nro(X)
-
-ntrain = 120
-s = sample(1:n, ntrain; replace = false) 
-Xtrain = X[s, :]
-ytrain = y[s]
-Xtest = rmrow(X, s)
-ytest = rmrow(y, s)
-
+ntest = 30
+s = samprand(n, ntest)
+Xtrain = X[s.train, :]
+ytrain = y[s.train]
+Xtest = X[s.test, :]
+ytest = y[s.test]
+ntrain = n - ntest
+(ntot = n, ntrain, ntest)
 tab(ytrain)
 tab(ytest)
 
 prior = :unif
 #prior = :prop
-fm = kdeda(Xtrain, ytrain; prior = prior) ;
-#fm = kdeda(Xtrain, ytrain; prior = prior, a = .5) ;
-#fm = kdeda(Xtrain, ytrain; prior = prior, h = .1) ;
-pnames(fm)
-fm.fm[1].H
+mod = kdeda(; prior)
+fit!(mod, Xtrain, ytrain)
+pnames(mod)
+pnames(mod.fm)
+fm = mod.fm ;
+fm.lev
+fm.ni
 
-res = Jchemo.predict(fm, Xtest) ;
+res = predict(mod, Xtest) ;
 pnames(res)
-res.dens
-res.posterior
-res.pred
+@head res.posterior
+@head res.pred
 errp(res.pred, ytest)
 confusion(res.pred, ytest).cnt
+
+mod = kdeda(; prior, a_kde = .5) ;
+#mod = kdeda(; prior, h_kde = .1) ;
+fit!(mod, Xtrain, ytrain)
+mod.fm.fm[1].H
 ```
 """ 
-function kdeda(X, y; kwargs...)
-    Q = eltype(X[1, 1])
-    weights = mweight(ones(Q, nro(X)))
-    kdeda(X, y, weights; 
-        kwargs...)
-end
-
-function kdeda(X, y, weights::Weight; 
-        kwargs...) 
+function kdeda(X, y; kwargs...) 
     par = recovkwargs(Par, kwargs)
     @assert in([:unif; :prop])(par.prior) "Wrong value for argument 'prior'."
     X = ensure_mat(X)
@@ -79,7 +77,7 @@ function kdeda(X, y, weights::Weight;
     for i = 1:nlev
         s = y .== lev[i]
         fm[i] = dmkern(vrow(X, s); 
-            h = par.h_kde, a = par.a_kde)
+            h_kde = par.h_kde, a_kde = par.a_kde)
     end
     Kdeda(fm, wprior, lev, ni)
 end
@@ -92,14 +90,16 @@ function predict(object::Kdeda, X)
     dens = similar(X, m, nlev)
     ni = object.ni
     for i = 1:nlev
-        dens[:, i] .= vec(Jchemo.predict(object.fm[i], X).pred)
+        dens[:, i] .= vec(
+            predict(object.fm[i], X).pred)
     end
     A = object.wprior' .* dens
     v = sum(A, dims = 2)
-    posterior = fscale(A', v)'                    # This could be replaced by code similar as in fscale! 
-    z =  mapslices(argmax, posterior; dims = 2)  # if equal, argmax takes the first
+    posterior = fscale(A', v)'  # Could be replaced by similar as in fscale! 
+    z =  mapslices(argmax, posterior; 
+        dims = 2)  # if equal, argmax takes the first
     pred = reshape(replacebylev2(z, 
         object.lev), m, 1)
-    (pred = pred, dens = dens, posterior = posterior)
+    (pred = pred, dens, posterior)
 end
     
