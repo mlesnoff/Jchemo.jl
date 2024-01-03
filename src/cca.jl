d44268f59b54824efc6f93a8d86bc74e31a0fc61
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-01-03 14:25:23 +0100

    Update cca.jl

--- src/cca.jl
+++ src/cca.jl
@@ -1,18 +1,21 @@
 """
-    cca(X, Y, weights = ones(nro(X)); nlv, 
-        bscal = :none, tau = 1e-8, scal::Bool = false)
-    cca!(X::Matrix, Y::Matrix, weights = ones(nro(X)); nlv,
-        bscal = :none, tau = 1e-8, scal::Bool = false)
+    cca(; kwargs...)
+    cca(X, Y; kwargs...)
+    cca(X, Y, weights::Weight; 
+        kwargs...)
+    cca!(X::Matrix, Y::Matrix, weights::Weight; 
+        kwargs...)
 Canonical correlation Analysis (CCA, RCCA).
-* `X` : First block (matrix) of data.
-* `Y` : Second block (matrix) of data.
+* `X` : First block of data.
+* `Y` : Second block of data.
 * `weights` : Weights of the observations (rows). 
     Internally normalized to sum to 1. 
+Keyword arguments:
 * `nlv` : Nb. latent variables (LVs = scores T) to compute.
-* `bscal` : Type of block scaling (`:none`, `:frob`). 
-    See functions `fblockscal`.
+* `bscal` : Type of block scaling. Possible values are:
+    `:none`, `:frob`, `:mfa`. See functions `fblockscal`.
 * `tau` : Regularization parameter (∊ [0, 1]).
-* `scal` : Boolean. If `true`, each column of `X` and `Y` 
+* `scal` : Boolean. If `true`, each column of blocks in `Xbl` 
     is scaled by its uncorrected standard deviation 
     (before the block scaling).
 
@@ -20,38 +23,44 @@ This function implements a CCA algorithm using SVD decompositions and
 presented in Weenink 2003 section 2. 
 
 A continuum regularization is available (parameter `tau`). 
-After block centering and scaling, the returned block scores (Tx and Ty) 
-are proportionnal to the eigenvectors of Projx * Projy 
-and Projy * Projx, respectively, defined as follows: 
+After block centering and scaling, the function returns 
+block scores (Tx and Ty) that are proportionnal to the 
+eigenvectors of Projx * Projy and Projy * Projx, respectively, 
+defined as follows: 
 * Cx = (1 - `tau`) * X'DX + `tau` * Ix
 * Cy = (1 - `tau`) * Y'DY + `tau` * Iy
 * Cxy = X'DY 
 * Projx = sqrt(D) * X * invCx * X' * sqrt(D)
 * Projy = sqrt(D) * Y * invCx * Y' * sqrt(D)
 where D is the observation (row) metric. 
-Value `tau` = 0 can generate unstability when inverting the covariance matrices. 
-A better alternative is generally to use an epsilon value (e.g. `tau` = 1e-8) 
-to get similar results as with pseudo-inverses.  
+Value `tau` = 0 can generate unstability when inverting 
+the covariance matrices. Often, a better alternative is 
+to use an epsilon value (e.g. `tau` = 1e-8) to get similar 
+results as with pseudo-inverses.  
 
-With uniform `weights`, the normed scores returned 
-by the function are expected to be the same as those returned 
-by functions `rcc` of the R packages `CCA` (González et al.) and `mixOmics` 
-(Lê Cao et al.) whith the parameters lambda1 and lambda2 set to:
+The normed scores returned by the function are expected 
+((using uniform `weights`) to be the same as those 
+returned by functions `rcc` of the R packages `CCA` (González et al.) 
+and `mixOmics` (Lê Cao et al.) whith their parameters lambda1 
+and lambda2 set to:
 * lambda1 = lambda2 = `tau` / (1 - `tau`) * n / (n - 1) 
 
 ## References
-González, I., Déjean, S., Martin, P.G.P., Baccini, A., 2008. CCA: 
-An R Package to Extend Canonical Correlation Analysis. Journal of Statistical 
-Software 23, 1-14. https://doi.org/10.18637/jss.v023.i12
+González, I., Déjean, S., Martin, P.G.P., Baccini, A., 2008. 
+CCA: An R Package to Extend Canonical Correlation Analysis. 
+Journal of Statistical Software 23, 1-14. 
+https://doi.org/10.18637/jss.v023.i12
 
-Hotelling, H. (1936): “Relations between two sets of variates”, Biometrika 28: pp. 321–377.
+Hotelling, H. (1936): “Relations between two sets of variates”, 
+Biometrika 28: pp. 321–377.
 
-Lê Cao, K.-A., Rohart, F., Gonzalez, I., Dejean, S., Abadi, A.J., Gautier, B., Bartolo, F., 
-Monget, P., Coquery, J., Yao, F., Liquet, B., 2022. mixOmics: Omics Data Integration Project. 
+Lê Cao, K.-A., Rohart, F., Gonzalez, I., Dejean, S., Abadi, A.J., 
+Gautier, B., Bartolo, F., Monget, P., Coquery, J., Yao, F., 
+Liquet, B., 2022. mixOmics: Omics Data Integration Project. 
 https://doi.org/10.18129/B9.bioc.mixOmics
 
-Weenink, D. 2003. Canonical Correlation Analysis, Institute of Phonetic Sciences, 
-Univ. of Amsterdam, Proceedings 25, 81-99.
+Weenink, D. 2003. Canonical Correlation Analysis, Institute of 
+Phonetic Sciences, Univ. of Amsterdam, Proceedings 25, 81-99.
 
 ## Examples
 ```julia
@@ -82,7 +91,8 @@ function cca(X, Y; kwargs...)
     cca(X, Y, weights; kwargs...)
 end
 
-function cca(X, Y, weights::Weight; kwargs...)
+function cca(X, Y, weights::Weight; 
+        kwargs...)
     cca!(copy(ensure_mat(X)), copy(ensure_mat(Y)), 
         weights; kwargs...)
 end
