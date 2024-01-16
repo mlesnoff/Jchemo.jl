2b21dd096784f0d60585dec4508427979a08a6c7
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-01-16 18:40:14 +0100

    Update utility_scaling_mb.jl

--- src/utility_scaling_mb.jl
+++ src/utility_scaling_mb.jl
@@ -1,88 +1,26 @@
 """
-    fblockscal(Xbl; bscales)
+    fblockscal(Xbl, bscales)
     fblockscal!(Xbl::Vector, bscales::Vector)
-    fblockscal_frob(Xbl, weights::Weight)
-    fblockscal_mfa(Xbl, weights::Weight)
-    fblockscal_ncol(Xbl)
-    fblockscal_sd(Xbl, weights::Weight)
 Scale multiblock X-data.
 * `Xbl` : List of blocks (vector of matrices) of X-data 
     Typically, output of function `mblock` from (n, p) data.  
 * `bscales` : A vector (of length equal to the nb. 
     of blocks) of the scalars diving the blocks.
-* `weights` : Weights (n) of the observations (rows 
-    of the blocks). Must be of type `Weight` (see e.g. 
-    function `mweight`).
-
-Specificities of each function:
-* `fblockscal`: Each block X is tranformed 
-    to X / `bscales'.
-* `fblockscal_frob`: Let D be the diagonal matrix 
-    of vector `weights.w`. Each block X is divided by 
-    its Frobenius norm  = sqrt(tr(X' * D * X)). After 
-    this scaling, tr(X' * D * X) = 1.
-* `fblockscal_mfa`: Each block X is divided by sqrt(lamda),
-    where lambda is the dominant eigenvalue of X 
-    (this is the "MFA" approach).
-* `fblockscal_ncol`: Each block X is divided by the nb. 
-    of columns of the block.
-* `fblockscal_sd`: Each block X is divided by 
-    sqrt(sum(weighted variances of the block-columns)). After 
-    this scaling, sum(weighted variances of 
-    the block-columns) = 1.
-
-Each function returns the scaled blocks and the scaling values.
 
 ## Examples
 ```julia
-using LinearAlgebra
-
 n = 5 ; m = 3 ; p = 10 
 X = rand(n, p) 
-Xnew = rand(m, p)
-
 listbl = [3:4, 1, [6; 8:10]]
 Xbl = mblock(X, listbl) 
-Xblnew = mblock(Xnew, listbl) 
 
-Xbl[1]
-Xbl[2]
-Xbl[3]
+bscales = 10 * ones(3)
+zXbl = fblockscal(Xbl, bscales) ;
+@head zXbl[3]
+@head Xbl[3]
 
-bscales = ones(3)
-res = fblockscal(Xbl, bscales) ;
-res.bscales
-res.Xbl[3]
-Xbl[3]
-
-w = ones(n)
-#w = rand(n)
-weights = mweight(w) 
-
-res = fblockscal_frob(Xbl, weights) ;
-res.bscales
-i = 3
-D = Diagonal(weights.w)
-tr(res.Xbl[i]' * D * res.Xbl[i])^.5
-
-res = fblockscal_mfa(Xbl, weights) ;
-i = 3
-res.bscales[i]
-pcasvd(Xbl[i], weights).sv[1]
-
-res = fblockscal_ncol(Xbl) ;
-res.bscales
-i = 3
-res.Xbl[i]
-Xbl[3] / nco(Xbl[3])
-
-res = fblockscal_sd(Xbl, weights) ;
-res.bscales
-i = 3
-sum(colvar(res.Xbl[i], weights))
-
-## To concatenate the returned blocks
-X_concat = reduce(hcat, res.Xbl)
+fblockscal!(Xbl, bscales) ;
+@head Xbl[3]
 ```
 """
 function fblockscal(Xbl, bscales)
@@ -101,7 +39,7 @@ function fblockscal!(Xbl::Vector, bscales::Vector)
     @inbounds for k = 1:nbl
         Xbl[k] ./= bscales[k]
     end
-    (Xbl = Xbl, bscales)
+    Xbl
 end 
 
 
