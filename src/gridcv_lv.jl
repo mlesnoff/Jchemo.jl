bef97190bcc5399a6fc86dbfe8520647ba05d8c8
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-01-19 10:41:52 +0100

    Update gridcv_lv.jl

--- src/gridcv_lv.jl
+++ src/gridcv_lv.jl
@@ -25,13 +25,13 @@ function gridcv_lv(X, Y; segm, fun, score, pars = nothing, nlv,
         @inbounds for j = 1:nsegm
             verbose ? print("segm=", j, " ") : nothing
             s = listsegm[j]
-            zres[j] = gridscore_lv(rmrow(X, s), rmrow(Y, s), 
-                X[s, :], Y[s, :]; fun, score, nlv, pars)
+            zres[j] = gridscore_lv(rmrow(X, s), rmrow(Y, s), X[s, :], Y[s, :]; 
+                fun, score, nlv, pars)
         end
         zres = reduce(vcat, zres)
         ## Case where pars is empty
         if isnothing(pars) 
-            dat = DataFrame(rep = fill(i, nsegm * le_nlv),
+            dat = DataFrame(rep = fill(i, nsegm * le_nlv), 
                 segm = repeat(1:nsegm, inner = le_nlv))
         else
             ncomb = length(pars[1]) # nb. combinations in pars
@@ -43,12 +43,10 @@ function gridcv_lv(X, Y; segm, fun, score, pars = nothing, nlv,
     end
     verbose ? println("/ End.") : nothing
     res_rep = reduce(vcat, res_rep)
-    isnothing(pars) ? namgroup = [:nlv] : 
-        namgroup =  [:nlv ; collect(keys(pars))]
+    isnothing(pars) ? namgroup = [:nlv] : namgroup =  [:nlv ; collect(keys(pars))]
     gdf = groupby(res_rep, namgroup) 
     namy = map(string, repeat(["y"], q), 1:q)
-    res = combine(gdf, namy .=> mean, 
-        renamecols = false)
+    res = combine(gdf, namy .=> mean, renamecols = false)
     (res = res, res_rep)
 end
 
