1927a54d469a24bef370b5fbf9de6d4d75a61667
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-03-30 11:02:04 +0100

    Update Jchemo.jl

--- src/Jchemo.jl
+++ src/Jchemo.jl
@@ -377,7 +377,7 @@ export
     ## Validation
     residreg, residcla, 
     ssr, msep, rmsep, rmsepstand, 
-    bias, sep, cor2, r2, rpd, rpdr, mse, errp,
+    bias, sep, cor2, r2, rpd, rpdr, mse, errp, merrp,
     mpar,
     gridscore, 
     gridscore_br, gridscore_lv, gridscore_lb,
