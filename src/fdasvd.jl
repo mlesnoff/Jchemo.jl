44e8ebdd1ab77cc53bd09b399299b84a9052cf04
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-04-02 16:16:25 +0200

    Update fdasvd.jl

--- src/fdasvd.jl
+++ src/fdasvd.jl
@@ -11,6 +11,11 @@ Keyword arguments:
 * `nlv` : Nb. of discriminant components.
 * `lb` : Ridge regularization parameter "lambda".
     Can be used when `X` has collinearities. 
+* `prior` : Type of prior probabilities for class 
+    membership. Possible values are: `:unif` (uniform), 
+    `:prop` (proportional), or a vector (of length equal to 
+    the number of classes) giving the prior weight for each class 
+    (the vector must be sorted in the same order as `mlev(x)`).
 * `scal` : Boolean. If `true`, each column of `X` 
     is scaled by its uncorrected standard deviation.
 
@@ -18,7 +23,7 @@ FDA by a weighted SVD factorization of the matrix of the
 class centers (after spherical transformaton). 
 The function gives the same results as function `fda`.
 
-See function `fda` for examples.
+See function `fda` for details and examples.
 """ 
 function fdasvd(X, y; kwargs...)
     par = recovkwargs(Par, kwargs)
