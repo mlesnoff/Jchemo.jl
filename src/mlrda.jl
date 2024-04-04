8c45d9b80a831af67c3e868115ad8a06125d1c0a
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-04-04 09:33:45 +0200

    Update mlrda.jl

--- src/mlrda.jl
+++ src/mlrda.jl
@@ -8,6 +8,12 @@ Discrimination based on multple linear regression
 * `y` : Univariate class membership (n).
 * `weights` : Weights (n) of the observations. 
     Must be of type `Weight` (see e.g. function `mweight`). 
+Keyword arguments:
+* `prior` : Type of prior probabilities for class 
+    membership. Possible values are: `:unif` (uniform), 
+    `:prop` (proportional), or a vector (of length equal to 
+    the number of classes) giving the prior weight for each class 
+    (the vector must be sorted in the same order as `mlev(x)`).
 
 The training variable `y` (univariate class membership) is 
 transformed to a dummy table (Ydummy) containing nlev columns, 
@@ -20,6 +26,10 @@ eventuall outside of [0, 1]) of the class membership probabilities.
 For a given observation, the final prediction is the class 
 corresponding to the dummy variable for which the probability 
 estimate is the highest.
+
+In the high-level version of the function, the observation weights used in 
+the MLR are defined with argument `prior`. For other choices, use the 
+low-level version.
 
 ## Examples
 ```julia
