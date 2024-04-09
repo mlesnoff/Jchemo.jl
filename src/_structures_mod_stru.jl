b3a73cfb8ff6d61cfd5cc611566d8e60d3a1316b
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-04-09 22:03:09 +0200

    Update _structures_mod_stru.jl

--- src/_structures_mod_stru.jl
+++ src/_structures_mod_stru.jl
@@ -1,25 +1,16 @@
-Base.@kwdef mutable struct Transformer{
-        FUN <: Function, 
-        FM, 
-        KWARGS <: Base.Pairs}
+Base.@kwdef mutable struct Transformer{FUN <: Function, FM, KWARGS <: Base.Pairs}
     fun::FUN   
     fm::Union{Nothing, FM}
     kwargs::KWARGS    
 end
 
-Base.@kwdef mutable struct Predictor{
-        FUN <: Function, 
-        FM, 
-        KWARGS <: Base.Pairs}
+Base.@kwdef mutable struct Predictor{FUN <: Function, FM, KWARGS <: Base.Pairs}
     fun::FUN   
     fm::Union{Nothing, FM}
     kwargs::KWARGS    
 end
 
-Base.@kwdef mutable struct PredictorNoY{
-        FUN <: Function, 
-        FM, 
-        KWARGS <: Base.Pairs}
+Base.@kwdef mutable struct PredictorNoY{FUN <: Function, FM, KWARGS <: Base.Pairs}
     fun::FUN   
     fm::Union{Nothing, FM}
     kwargs::KWARGS    
