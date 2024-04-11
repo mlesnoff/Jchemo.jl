f868a3bec6e719ba6d09049066739e2e1bca9a4a
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-04-11 10:00:44 +0200

    Update _structures_mod_fun.jl

--- src/_structures_mod_fun.jl
+++ src/_structures_mod_fun.jl
@@ -1,9 +1,3 @@
-Base.@kwdef mutable struct Model1{FUN <: Function, FM, KWARGS <: Base.Pairs}
-    fun::FUN   
-    fm::Union{Nothing, FM}
-    kwargs::KWARGS    
-end
-
 Base.@kwdef mutable struct Model
     fun::Function   
     fm
