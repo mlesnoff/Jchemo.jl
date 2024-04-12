6a25e91ce1bc36eaca8aba19574a84e5238b4ff0
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-04-12 14:12:06 +0200

    Update isel.jl

--- src/isel.jl
+++ src/isel.jl
@@ -1,7 +1,5 @@
 """
-    isel!(mod, X, Y, wl = 1:nco(X); 
-        rep = 1, nint = 5, psamp = .3, 
-        score = rmsep)
+    isel!(mod, X, Y, wl = 1:nco(X); rep = 1, nint = 5, psamp = .3, score = rmsep)
 Interval variable selection.
 * `mod` : Model to evaluate.
 * `X` : X-data (n, p).
@@ -47,8 +45,7 @@ wl = parse.(Float64, wl_str)
 ntot, p = size(X)
 typ = Y.typ
 namy = names(Y)[1:3]
-plotsp(X, wl; xlabel = "Wavelength (nm)", 
-    ylabel = "Absorbance").f
+plotsp(X, wl; xlabel = "Wavelength (nm)", ylabel = "Absorbance").f
 
 s = typ .== "train"
 Xtrain = X[s, :]
@@ -67,35 +64,29 @@ nam = namy[j]
 ytrain = Ytrain[:, nam]
 ytest = Ytest[:, nam]
 
-mod = plskern(nlv = 5)
+mod = model(plskern; nlv = 5)
 nint = 10
-res = isel!(mod, Xtrain, ytrain, wl; 
-    rep = 30, nint) ;
+res = isel!(mod, Xtrain, ytrain, wl; rep = 30, nint) ;
 res.res_rep
 res.res0_rep
 zres = res.res
 zres0 = res.res0
 f = Figure(size = (650, 300))
-ax = Axis(f[1, 1],
-    xlabel = "Wawelength (nm)", ylabel = "RMSEP_Val",
+ax = Axis(f[1, 1], xlabel = "Wawelength (nm)", ylabel = "RMSEP_Val",
     xticks = zres.lo)
 scatter!(ax, zres.mid, zres.y1; color = (:red, .5))
-vlines!(ax, zres.lo; color = :grey,
-    linestyle = :dash, linewidth = 1)
+vlines!(ax, zres.lo; color = :grey, linestyle = :dash, linewidth = 1)
 hlines!(ax, zres0.y1, linestyle = :dash)
 f
 ```
 """
-function isel!(mod, X, Y, wl = 1:nco(X); 
-        rep = 1, nint = 5, psamp = .3, 
-        score = rmsep)
+function isel!(mod, X, Y, wl = 1:nco(X); rep = 1, nint = 5, psamp = .3, score = rmsep)
     X = ensure_mat(X)
     Y = ensure_mat(Y) 
     n, p = size(X)
     q = nco(Y)
     nint = Int(nint)
-    z = collect(round.(range(1, p + 1; 
-        length = nint + 1)))
+    z = collect(round.(range(1, p + 1; length = nint + 1)))
     int = [z[1:nint] z[2:(nint + 1)] .- 1]
     int = hcat(int, round.(rowmean(int)))
     int = Int.(int)
@@ -139,8 +130,7 @@ function isel!(mod, X, Y, wl = 1:nco(X);
     res = hcat(dat, res)
     res0 = mean(res0_rep, dims = 3)[:, :, 1]
     res0 = DataFrame(res0, Symbol.(namy))
-    (res = res, res0, 
-        res_rep, res0_rep, int)
+    (res = res, res0, res_rep, res0_rep, int)
 end
 
 
