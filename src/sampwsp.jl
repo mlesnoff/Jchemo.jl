0d9200f130dd5800d1182cadeb6cf8051fe7935a
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-05-17 15:02:02 +0200

    Update sampwsp.jl

--- src/sampwsp.jl
+++ src/sampwsp.jl
@@ -1,3 +1,41 @@
+"""
+    sampwsp(X, dcri; maxit = nro(X))
+Build training vs. test sets by WSP sampling.  
+* `X` : X-data (n, p).
+* `dcri` :
+Keyword arguments: 
+* `maxit` : Maximum number of iterations.
+
+Two outputs (= row indexes of the data) are returned: 
+* `train` (`n` - k),
+* `test` (k). 
+
+Output `test` is built from the "Wootton, Sergent, Phan-Tan-Luu" (WSP) 
+algorithm, assumed to generate samples uniformely distributed in the domain 
+space (Santiago et al. 2012).
+
+## References
+
+Béal A. 2015. Description et sélection de données en grande dimensio. Thèse de doctorat.
+Laboratoire d’Instrumentation et de sciences analytiques,
+Ecole doctorale des siences chimiques, Université d'Aix-Marseille.
+
+Santiago, J., Claeys-Bruno, M., Sergent, M., 2012. Construction of space-filling 
+designs using WSP algorithm for high dimensional spaces. 
+Chemometrics and Intelligent Laboratory Systems, Selected Papers from 
+Chimiométrie 2010 113, 26–31. https://doi.org/10.1016/j.chemolab.2011.06.003
+
+## Examples
+```julia
+n = 600 ; p = 2
+X = rand(n, p)
+dcri = .5
+s = sampwsp(X, dcri)
+pnames(res)
+@show length(s.test)
+plotxy(X[s.test, 1], X[s.test, 2]).f
+```
+""" 
 function sampwsp(X, dcri; maxit = nro(X))
     X = ensure_mat(X)
     n, p = size(X)
