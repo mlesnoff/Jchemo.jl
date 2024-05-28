565a51caa9d65a3f398d0e4328303cc0f5e6887b
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-05-28 22:58:05 +0200

    Update sampwsp.jl

--- src/sampwsp.jl
+++ src/sampwsp.jl
@@ -36,32 +36,40 @@ pnames(res)
 plotxy(X[s.test, 1], X[s.test, 2]).f
 ```
 """ 
-function sampwsp(X, dmin; maxit = nro(X))
+function sampwsp(X, dmin; recod = true, maxit = nro(X))
     X = ensure_mat(X)
     n, p = size(X)
     indX = collect(1:n)
     indXtot = copy(indX)
     x = similar(X, 1, p)
     ## First reference point: set as the closest 
-    ## from the domain center 
-    xmeans = colmean(X)
-    s = getknn(X, xmeans'; k = 1).ind[1][1]
-    x .= vrow(X, s:s)
+    ## from the domain center
+    if recod
+        zX = recodwsp(X) 
+        zX = copy(X)
+        xmeans = repeat([.5], p)
+    else
+        zX = copy(X)
+        xmeans = colmean(zX)
+    end
+    s = getknn(zX, xmeans'; k = 1).ind[1][1]
+    x .= vrow(zX, s:s)
     ind = [indX[s]]
     ## Start
     iter = 1
     while (n > 1) && (iter < maxit) 
-        res = getknn(X, x; k = n)
+        res = getknn(zX, x; k = n)
+        println(res)
         s = res.d[1] .> dmin
         v = res.ind[1][s]  # new {reference point + candidates}
         if length(v) > 0
             s1 = v[1]
             s2 = v[2:end]
-            x .= vrow(X, s1:s1)  # reference point
-            X = vrow(X, s2)      # candidates
+            x .= vrow(zX, s1:s1)  # reference point
+            zX = vrow(zX, s2)     # candidates
             push!(ind, indX[s1])
             indX = indX[s2]
-            n = nro(X)
+            n = nro(zX)
         else
             train = rmrow(indXtot, ind)
             return (train, test = ind, niter = iter)
@@ -71,5 +79,23 @@ function sampwsp(X, dmin; maxit = nro(X))
     train = rmrow(indXtot, ind)
     (train, test = ind, niter = iter)
 end
+
+function recodwsp(X)
+    X = ensure_mat(X)
+    p = nco(X) 
+    zX = similar(X)
+    for j = 1:p
+        x = vcol(X, j)
+        vmin = minimum(x)
+        vmax = maximum(x)
+        vdiff = vmax - vmin
+        zX[:, j] .=  0.5 .+ (x .- (vdiff / 2 + vmin)) / vdiff
+    end
+    zX
+end
+
+    
+    
+
 
 
