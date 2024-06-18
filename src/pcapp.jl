cc8d79a14665863b36f8787ecfa8051f2cd9e201
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-06-18 08:49:43 +0200

    Update pcapp.jl

--- src/pcapp.jl
+++ src/pcapp.jl
@@ -1,8 +1,8 @@
-function pcapp(X, weights::Weight; rob = true, kwargs...)
-    pcapp!(copy(ensure_mat(X)), weights; rob, kwargs...)
+function pcapp(X; rob = true, nsim = 50, kwargs...)
+    pcapp!(copy(ensure_mat(X)); rob, nsim, kwargs...)
 end
 
-function pcapp!(X::Matrix, weights::Weight; rob = true, kwargs...)
+function pcapp!(X::Matrix; rob = true, nsim = 50, kwargs...)
     par = recovkwargs(Par, kwargs) 
     Q = eltype(X)
     n, p = size(X)
@@ -10,28 +10,29 @@ function pcapp!(X::Matrix, weights::Weight; rob = true, kwargs...)
     if rob 
         fcolmean = Jchemo.colmedspa
         fcolstd = colmad
+        fobj = colstd
     else 
         fcolmean = colmean
         fcolstd = colstd
+        fobj = colmad
     end
-    xmeans = fcolmean(X, weights) 
+    xmeans = fcolmean(X) 
     xscales = ones(Q, p)
     if par.scal 
-        xscales .= fcolstd(X, weights)
+        xscales .= fcolstd(X)
         fcscale!(X, xmeans, xscales)
     else
         fcenter!(X, xmeans)
     end
-
-
-    sqrtw = sqrt.(weights.w)
-    X .= Diagonal(sqrtw) * X
-    res = eigen!(Symmetric(X' * X); sortby = x -> -abs(x)) 
-    P = res.vectors[:, 1:nlv]
-    eig = res.values[1:min(n, p)]
-    eig[eig .< 0] .= 0
-    sv = sqrt.(eig)
-    T = Diagonal(1 ./ sqrtw) * X * P
-    Pca(T, P, sv, xmeans, xscales, weights, nothing, kwargs, par) 
+    fsimpp = simppbin 
+    for a = 1:nlv
+        P = fsimpp(X; nsim)
+        zT = X * P 
+        zobj = colstd(zT)
+        s = findall(zobj .== maximum(zobj))[1]
+        sv = zobj[s]
+        T[:, a] = vcol(T, a)
+    end
+    (T = T, P, sv, xmeans, xscales, kwargs, par) 
 end
 
