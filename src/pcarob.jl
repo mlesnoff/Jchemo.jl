30109c061314322d73c7dfbb28f2b78b259ff579
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-06-24 14:56:09 +0200

    Update pcarob.jl

--- src/pcarob.jl
+++ src/pcarob.jl
@@ -2,7 +2,7 @@
     pcarob(X; kwargs...)
     pcarob(X, weights::Weight; kwargs...)
     pcarob!(X::Matrix, weights::Weight; kwargs...)
-Robust PCA.
+Robust PCA using outlierness.
 * `X` : X-data (n, p). 
 * `weights` : Weights (n) of the observations. 
     Must be of type `Weight` (see e.g. function `mweight`).
@@ -11,20 +11,8 @@ Keyword arguments:
 * `scal` : Boolean. If `true`, each column of `X` 
     is scaled by its uncorrected standard deviation.
 
-Spherical PCA (Locantore et al. 1990, Maronna 2005, Daszykowski et al. 2007). 
-Matrix `X` is centered by the spatial median computed by function
-`Jchemo.colmedspa`.
 
 ## References
-Daszykowski, M., Kaczmarek, K., Vander Heyden, Y., Walczak, B., 2007. 
-Robust statistics in data analysis - A review. Chemometrics and Intelligent 
-Laboratory Systems 85, 203-219. https://doi.org/10.1016/j.chemolab.2006.06.016
-
-Locantore N., Marron J.S., Simpson D.G., Tripoli N., Zhang J.T., Cohen K.L.
-Robust principal component analysis for functional data, Test 8 (1999) 1–7
-
-Maronna, R., 2005. Principal components and orthogonal regression based on 
-robust scales, Technometrics, 47:3, 264-273, DOI: 10.1198/004017005000000166
 
 ## Examples
 ```julia
