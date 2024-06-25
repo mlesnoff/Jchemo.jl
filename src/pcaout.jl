928bd4328759b70601f021adf99b003c02af0d2c
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-06-25 21:15:45 +0200

    Update pcaout.jl

--- src/pcaout.jl
+++ src/pcaout.jl
@@ -8,11 +8,44 @@ Robust PCA using outlierness.
     Must be of type `Weight` (see e.g. function `mweight`).
 Keyword arguments:
 * `nlv` : Nb. of principal components (PCs).
+* `prm` : Proportion of the data removed (hard rejection of outliers) 
+    for each outlierness measure.
 * `scal` : Boolean. If `true`, each column of `X` 
     is scaled by its uncorrected standard deviation.
 
+Robust PCA combining outlyingness measures and weighted PCA (WPCA). 
+
+Observations (`X`-rows) receive weights depending on outlyingness 
+with the objective to remove the effect of multivariate `X`-outliers 
+that have potentially bad leverages.
+* First, the Stahel-Donoho outlyingness (Maronna and Yohai, 1995) 
+    is computed (function `outstah`). The proportion `prm` of the 
+    observations with the highest outlyingness values receive a 
+    weight w1 = 0 (the other receive a weight w1 = 1).
+* Second, an outlyingness computed from the Euclidean distance to center 
+    (function `outstah`) is computed. The proportion `prm` of the observations 
+    with the highest outlyingness values receive a weight w2 = 0 
+    (the other receive a weight w2 = 1).
+The final weights of the observations are computed by weights.w * w1 * w2,
+which is used in a weighted PCA.
+
+By default, the function uses `prm = .3` (such as in the ROBPCA algorithm 
+of Hubert et al. 2005, 2009). 
 
 ## References
+
+Hubert, M., Rousseeuw, P.J., Vanden Branden, K., 2005. ROBPCA: A New Approach 
+to Robust Principal Component Analysis. Technometrics 47, 64-79. 
+https://doi.org/10.1198/004017004000000563
+
+Hubert, M., Rousseeuw, P., Verdonck, T., 2009. Robust PCA for skewed data and its 
+outlier map. Computational Statistics & Data Analysis 53, 2264-2274. 
+https://doi.org/10.1016/j.csda.2008.05.027
+
+Maronna, R.A., Yohai, V.J., 1995. The Behavior of the 
+Stahel-Donoho Robust Multivariate Estimator. Journal of the 
+American Statistical Association 90, 330–341. 
+https://doi.org/10.1080/01621459.1995.10476517
 
 ## Examples
 ```julia
@@ -26,7 +59,7 @@ wlst = names(X)
 wl = parse.(Float64, wlst)
 n = nro(X)
 
-nlv = 6
+nlv = 3
 mod = model(pcaout; nlv)  
 #mod = model(pcasvd; nlv) 
 fit!(mod, X)
