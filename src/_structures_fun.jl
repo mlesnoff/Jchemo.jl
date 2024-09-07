675f758ec89732f2075778cfbae8e16746035ce5
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2024-09-07 19:05:15 +0200

    Update _structures_fun.jl

--- src/_structures_fun.jl
+++ src/_structures_fun.jl
@@ -640,7 +640,7 @@ struct Plsprobda    # plslda, plsqda, plskdeda
     lev::Vector
     ni::Vector{Int}
     par::Union{ParPlsda, ParPlsqda, ParPlskdeda, ParSplsda, ParSplsqda, ParSplskdeda,
-        ParKplsda, ParKplsqda, ParKplskdeda}
+        ParKplsda, ParKplsqda, ParKplskdeda, ParUmap}
 end
 
 ## Local
