e2988ccf155466e0767eb955fac0d4abddc3c254
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-03-26 16:31:26 +0100

    Update _util.jl

--- src/_util.jl
+++ src/_util.jl
@@ -1,3 +1,23 @@
+"""
+    aggmean(X, y)
+Compute column-wise mean by class in a dataset.
+* `X` : Data (n, p).
+* `y` : A categorical variable (n) (class membership).
+
+## Examples
+```julia
+using Jchemo
+
+n, p = 20, 5
+X = rand(n, p)
+y = rand(1:3, n)
+df = DataFrame(X, :auto) 
+res = aggmean(X, y)
+res.X
+res.lev 
+aggmean(df, y).X
+```
+""" 
 function aggmean(X, y) 
     X = ensure_mat(X)
     y = vec(y)
