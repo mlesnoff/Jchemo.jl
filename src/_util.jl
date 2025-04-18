495907ef9c8be2ab646d2dd29d8b278f8ecf9aec
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-04-18 10:41:56 +0200

    Update _util.jl

--- src/_util.jl
+++ src/_util.jl
@@ -413,8 +413,8 @@ mlev(x) = sort(unique(x))
 
 """ 
     mweight(x::Vector)
-Return an object of type `Weight` containing vector 
-`w = x / sum(x)` (if ad'hoc building, `w` must sum to 1).
+Return an object of type `Weight` containing vector `w = x / sum(x)` (if ad'hoc building, 
+    `w` must sum to 1).
 
 ## Examples
 ```julia
@@ -436,38 +436,35 @@ mweight(x::Vector) = Weight(x / sum(x))
 #mweight!(w::Union{Vector{Float32}, Vector{Float64}}) = w ./= sum(w)
 
 """ 
-    mweightcla(x::AbstractVector; prior::Union{Symbol, Vector} = :unif)
-    mweightcla(Q::DataType, x::Vector; prior::Union{Symbol, Vector} = :unif)
-Compute observation weights for a categorical variable, 
-    given specified sub-total weights for the classes.
-* `x` : A categorical variable (n) (class membership).
+    mweightcla(y::AbstractVector; prior::Union{Symbol, Vector} = :prop)
+    mweightcla(Q::DataType, y::Vector; prior::Union{Symbol, Vector} = :prop)
+Compute observation weights for a categorical variable, given specified sub-total weights 
+    for the classes.
+* `y` : A categorical variable (n) (class membership).
 * `Q` : A data type (e.g. `Float32`).
 Keyword arguments:
-* `prior` : Type of prior probabilities for class 
-    membership. Possible values are: `:unif` (uniform), 
-    `:prop` (proportional), or a vector (of length equal to 
-    the number of classes) giving the prior weight for each class 
-    (the vector must be sorted in the same order as `mlev(x)`).
+* `prior` : Type of prior probabilities for class membership. Possible values are: `:prop` (proportionnal), 
+    `:unif` (uniform), or a vector (of length equal to the number of classes) giving the prior weight for each class 
+    (in case of vector, it must be sorted in the same order as `mlev(y)`).
 
-Return an object of type `Weight` (see function `mweight`) containing
-a vector `w` (n) that sums to 1.
+Return an object of type `Weight` (see function `mweight`) containing a vector `w` (n) that sums to 1.
 
 ## Examples
 ```julia
 using Jchemo
 
-x = vcat(rand(["a" ; "c"], 900), repeat(["b"], 100))
-tab(x)
-weights = mweightcla(x)
-#weights = mweightcla(x; prior = :prop)
-#weights = mweightcla(x; prior = [.1, .7, .2])
-res = aggstat(weights.w, x; algo = sum)
+y = vcat(rand(["a" ; "c"], 900), repeat(["b"], 100))
+tab(y)
+weights = mweightcla(y)
+#weights = mweightcla(y; prior = :prop)
+#weights = mweightcla(y; prior = [.1, .7, .2])
+res = aggstat(weights.w, y; algo = sum)
 [res.lev res.X]
 ```
 """
-function mweightcla(x::AbstractVector; prior::Union{Symbol, Vector} = :unif)
-    n = length(x)
-    res = tab(x)
+function mweightcla(y::AbstractVector; prior::Union{Symbol, Vector} = :prop)
+    n = length(y)
+    res = tab(y)
     lev = res.keys
     nlev = length(lev)
     if isequal(prior, :unif)
@@ -479,14 +476,14 @@ function mweightcla(x::AbstractVector; prior::Union{Symbol, Vector} = :unif)
     end
     w = zeros(n)
     @inbounds for i in eachindex(lev)
-        s = x .== lev[i]
+        s = y .== lev[i]
         w[s] .= priors[i] / res.vals[i]
     end
     mweight(w)
 end
 
-function mweightcla(Q::DataType, x::AbstractVector; prior::Union{Symbol, Vector} = :unif)
-    mweight(convert.(Q, mweightcla(x; prior).w))
+function mweightcla(Q::DataType, y::AbstractVector; prior::Union{Symbol, Vector} = :prop)
+    mweight(convert.(Q, mweightcla(y; prior).w))
 end
 
 """
