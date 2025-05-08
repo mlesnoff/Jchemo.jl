2c19e29ce746802b936a27b2ce0a98211feab34f
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-05-08 11:37:49 +0200

    Update outod.jl

--- src/outod.jl
+++ src/outod.jl
@@ -40,6 +40,7 @@ model = pcaout(; nlv = 3)
 fit!(model, X) 
 fitm = model.fitm ;
 res = outsd(fitm) ;
+#res = outsdod(fitm, X) ;
 @names res
 f, ax = plotxy(1:n, res.d, typ, xlabel = "Obs. index", ylabel = "Outlierness")
 text!(ax, 1:n, res.d; text = string.(1:n), fontsize = 10)
