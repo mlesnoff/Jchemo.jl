a3ef880cb84758c41a3959cf49432fdbead3c4a2
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-05-11 10:35:22 +0200

    Update occlknn.jl

--- src/occlknn.jl
+++ src/occlknn.jl
@@ -15,99 +15,12 @@ Keyword arguments:
 * `scal` : Boolean. If `true`, each column of `X` is scaled by its uncorrected standard deviation.
 
 See functions:
-* `outlknn` for details on the outlierness computation,
+* `outknn` for examples,
+* `outlknn` for details on the outlierness computation method,
 * and `occsd` for details on the the cutoff computation and the outputs.
 
-When **predictions** are done, the outlierness of each new observation is compared to the outlierness 
+For **predictions** (`predict`), the outlierness of each new observation is compared to the outlierness 
 distribution estimated from the `nsamp` sampled observations. 
-
-## Examples
-```julia
-using Jchemo, JchemoData, JLD2, CairoMakie
-path_jdat = dirname(dirname(pathof(JchemoData)))
-db = joinpath(path_jdat, "data/challenge2018.jld2") 
-@load db dat
-@names dat
-X = dat.X    
-Y = dat.Y
-model = savgol(npoint = 21, deriv = 2, degree = 3)
-fit!(model, X) 
-Xp = transf(model, X) 
-s = Bool.(Y.test)
-Xtrain = rmrow(Xp, s)
-Ytrain = rmrow(Y, s)
-Xtest = Xp[s, :]
-Ytest = Y[s, :]
-
-## Build the example data
-## - cla_train is the reference class (= 'in')
-## cla_test contains the observations to be predicted (i.e. to be 'in' or 'out' of cla_train) 
-## Below, cla_train = "EEH", and two situations are considered as examples for cla_test:
-cla_train = "EHH"
-s = Ytrain.typ .== cla_train
-Xtrain_fin = Xtrain[s, :]    
-ntrain = nro(Xtrain_fin)
-## Two situations
-cla_test = "PEE"    # here test obs. should be classified 'out'
-#cla_test = "EHH"   # here test obs. should be classified 'in'
-s = Ytest.typ .== cla_test
-Xtest_fin = Xtest[s, :] 
-ntest = nro(Xtest_fin)
-## Only used to compute error rates
-ytrain_fin = repeat(["in"], ntrain)
-if cla_test == cla_train
-    ytest_fin = repeat(["in"], ntest)
-else
-    ytest_fin = repeat(["out"], ntest)
-end
-y_fin = vcat(ytrain_fin, ytest_fin)
-## 
-ntot = ntrain + ntest
-(ntot = ntot, ntrain, ntest)
-
-## Data description
-nlv = 10
-model = pcasvd(; nlv) 
-fit!(model, Xtrain_fin) 
-Ttrain = model.fitm.T
-Ttest = transf(model, Xtest_fin)
-T = vcat(Ttrain, Ttest)
-i = 1
-group = vcat(repeat(["Train"], ntrain), repeat(["Test"], ntest))
-plotxy(T[:, i], T[:, i + 1], group; leg_title = "Type of obs.", xlabel = string("PC", i), 
-    ylabel = string("PC", i + 1), title = string(cla_train, "-", cla_test)).f
-
-#### Occ
-## Training
-nsamp = 150 ; k = 5 ; cri = 2.5
-model = occlknn(; nsamp, k, cri)
-fit!(model, Xtrain_fin) 
-@names model 
-fitm = model.fitm ;
-@names fitm 
-@head dtrain = fitm.d
-fitm.cutoff
-d = dtrain.dstand
-f, ax = plotxy(1:length(d), d; size = (500, 300), xlabel = "Obs. index", 
-    ylabel = "Standardized distance")
-hlines!(ax, 1; linestyle = :dot)
-f
-## Prediction of Test
-res = predict(model, Xtest_fin) 
-@names res
-@head pred = res.pred
-@head dtest = res.d
-tab(pred)
-errp(pred, ytest_fin)
-conf(pred, ytest_fin).cnt
-##
-d = vcat(dtrain.dstand, dtest.dstand)
-group = vcat(repeat(["Train"], nsamp), repeat(["Test"], ntest))
-f, ax = plotxy(1:length(d), d, group; size = (500, 300), leg_title = "Class", xlabel = "Obs. index", 
-    ylabel = "Standardized distance")
-hlines!(ax, 1; linestyle = :dot)
-f
-```
 """ 
 occlknn(; kwargs...) = JchemoModel(occlknn, nothing, kwargs)
 
