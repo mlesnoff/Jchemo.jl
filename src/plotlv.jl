712b0be8c2f6e885f739a4cc73b594944c4d441b
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-05-14 22:29:38 +0200

    Update plotlv.jl

--- src/plotlv.jl
+++ src/plotlv.jl
@@ -1,10 +1,10 @@
-function plotlv(X; size = (700, 350), shape, start = 1, 
+function plotlv(T; size = (700, 350), shape, start = 1, 
         color = nothing, 
         #ellipse::Bool = false, prob = .95, circle::Bool = false
         zeros::Bool = false,
         xlabel = "", ylabel = "", title = "", kwargs...)
     n, p = shape
-    pmax = nco(X)
+    pmax = nco(T)
     f = Figure(; size)
     isnothing(color) ? color = (:blue, .3) : nothing
     lw = 1.5
@@ -15,7 +15,7 @@ function plotlv(X; size = (700, 350), shape, start = 1,
         for j = 1:p
             if k < pmax
                 ax[k] = Axis(f; xlabel = string(xlabel, l), ylabel = string(ylabel, l + 1))
-                scatter!(ax[k], X[:, k], X[:, k + 1]; color = color, kwargs...) 
+                scatter!(ax[k], T[:, k], T[:, k + 1]; color = color, kwargs...) 
                 if zeros
                     hlines!(0; color = :grey60, linewidth = lw)
                     vlines!(0; color = :grey60, linewidth = lw)
@@ -33,10 +33,11 @@ function plotlv(X; size = (700, 350), shape, start = 1,
     (f = f, ax = ax)
 end
 
-function plotlv(X, group; size = (600, 350), color = nothing, ellipse::Bool = false, 
-        prob = .95, circle::Bool = false, bisect::Bool = false, zeros::Bool = false,
-        xlabel = "", ylabel = "", title = "", leg::Bool = true, leg_title = "Group", 
-        kwargs...)
+function plotlv(T, group; size = (700, 350), shape, start = 1, 
+        color = nothing, 
+        #ellipse::Bool = false, prob = .95, circle::Bool = false
+        zeros::Bool = false,
+        xlabel = "", ylabel = "", title = "", kwargs...)
 
 end
 
