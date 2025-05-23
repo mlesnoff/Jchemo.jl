e575c31824849853de753d1b8ee11af4b87f73fa
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-05-23 15:56:56 +0200

    Update fda.jl

--- src/fda.jl
+++ src/fda.jl
@@ -6,38 +6,29 @@
 Factorial discriminant analysis (FDA).
 * `X` : X-data (n, p).
 * `y` : y-data (n) (class membership).
-* `weights` : Weights (n) of the observations. 
-    Must be of type `Weight` (see e.g. function `mweight`).
+* `weights` : Weights (n) of the observations. Must be of type `Weight` (see e.g. function `mweight`).
 Keyword arguments:
 * `nlv` : Nb. of discriminant components.
-* `lb` : Ridge regularization parameter "lambda".
-    Can be used when `X` has collinearities. 
-* `prior` : Type of prior probabilities for class 
-    membership. Possible values are: `:unif` (uniform), 
-    `:prop` (proportional), or a vector (of length equal to 
-    the number of classes) giving the prior weight for each class 
+* `lb` : Ridge regularization parameter "lambda". Can be used when `X` has collinearities. 
+* `prior` : Type of prior probabilities for class membership. Possible values are: `:prop` (proportionnal), 
+    `:unif` (uniform), or a vector (of length equal to the number of classes) giving the prior weight for each class 
     (in case of vector, it must be sorted in the same order as `mlev(y)`).
-* `scal` : Boolean. If `true`, each column of `X` 
-    is scaled by its uncorrected standard deviation.
+* `scal` : Boolean. If `true`, each column of `X` is scaled by its uncorrected standard deviation.
 
-FDA by eigen factorization of Inverse(W) * B, where W is 
-the "Within"-covariance matrix (pooled over the classes), 
-and B the "Between"-covariance matrix.
+FDA by eigen factorization of Inverse(W) * B, where W is the "Within"-covariance matrix (pooled over 
+the classes), and B the "Between"-covariance matrix.
 
 The function maximizes the consensus:
 * p'Bp / p'Wp 
-i.e. max p'Bp with constraint p'Wp = 1. Vectors p (columns of `V`) 
-are the linear discrimant coefficients often referred to as "LD".
+i.e. max p'Bp with constraint p'Wp = 1. Vectors p (columns of `V`) are the linear discrimant coefficients 
+often referred to as "LD".
 
 If `X` is ill-conditionned, a ridge regularization can be used:
-* If `lb` > 0, W is replaced by W + `lb` * I, 
-    where I is the Idendity matrix.
+* If `lb` > 0, W is replaced by W + `lb` * I, where I is the Idendity matrix.
 
-In the high-level version of the present functions, the observation 
-weights are automatically defined by the given priors (argument `prior`): 
-the sub-totals by class of the observation weights are set equal to the prior 
-probabilities. The low-level version (argument `weights`) allows to implement 
-other choices.
+In the high-level version of the present functions, the observation weights are automatically defined by the 
+given priors (argument `prior`): the sub-totals by class of the observation weights are set equal to the prior 
+probabilities. The low-level version (argument `weights`) allows to implement other choices.
 
 ## Examples
 ```julia
@@ -81,12 +72,11 @@ aggsumv(fitm.weights.w, ytrain)
 fitm.V
 fitm.V' * fitm.V
 
-## Explained variance computed by weighted PCA 
-## of the class centers in transformed scale
+## Explained variance computed by weighted PCA of the class centers 
+## in transformed scale
 summary(model).explvarx
 
-## Projections of the class centers 
-## to the score space
+## Projections of the class centers to the score space
 ct = fitm.Tcenters 
 f, ax = plotxy(fitm.T[:, 1], fitm.T[:, 2], ytrain; ellipse = true, title = "FDA",
     xlabel = "Score-1", ylabel = "Score-2")
