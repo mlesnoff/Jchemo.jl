8efe7b9102a40767c9b6a6cc6f615d4cec3211ba
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-07-11 11:08:02 +0200

    Update segmts.jl

--- src/segmts.jl
+++ src/segmts.jl
@@ -2,29 +2,23 @@
     segmts(n::Int, m::Int; rep = 1, seed = nothing)
     segmts(group::Vector, m::Int; rep = 1, seed = nothing)
 Build segments of observations for "test-set" validation.
-* `n` : Total nb. of observations in the dataset. 
-    The sampling  is implemented within 1:`n`.
-* `group` : A vector (n) defining blocks of observations.
-* `m` : Nb. test observations, or groups if `group` is used, returned 
-    in each segment.
+* `n` : Total nb. of observations in the dataset. The sampling is implemented within 1:`n`.
+* `group` : A vector (`n`) defining blocks of observations.
+* `m` : Nb. test observations, or groups if `group` is used, returned in each segment.
 Keyword arguments: 
 * `rep` : Nb. replications of the sampling.
-* `seed` : Eventual seed for the `Random.MersenneTwister` 
-    generator. Must be of length = `rep`. When `nothing`, 
-    the seed is random at each replication.
+* `seed` : Eventual seed for the `Random.MersenneTwister` generator. Must be of length = `rep`. 
+    When `nothing`, the seed is random at each replication.
     
-For each replication, the function builds a test set that can 
-be used to validate a model. 
+For each replication, the function builds a test set that can be used to validate a model. 
 
-If `group` is used (must be a vector of length n), the function 
-samples entire groups (= blocks) of observations instead of observations. 
-Such a block-sampling is required when data is structured by blocks and 
-when the response to predict is correlated within blocks. 
-This prevents underestimation of the generalization error.
+If `group` is used (must be a vector of length `n`), the function samples entire groups (= blocks) 
+of observations instead of observations. Such a block-sampling is required when data is structured 
+by blocks and when the response to predict is correlated within blocks. This prevents underestimation 
+of the generalization error.
 
-The function returns a list (vector) of `rep` elements. 
-Each element of the list is a vector of the indexes (positions 
-within 1:`n`) of the sampled observations.  
+The function returns a list (vector) of `rep` elements. Each element of the list is a vector of the 
+indexes (positions within 1:`n`) of the sampled observations.  
 
 ## Examples
 ```julia
