0f79741ae567ab659f4d0eb988bffdb8c4c28e74
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-07-11 16:49:46 +0200

    Update sampcla.jl

--- src/sampcla.jl
+++ src/sampcla.jl
@@ -2,23 +2,21 @@
     sampcla(x, k::Union{Int, Vector{Int}}, y = nothing)
 Build training vs. test sets by stratified sampling.  
 * `x` : Class membership (n) of the observations.
-* `k` : Nb. test observations to sample in each class. 
-    If `k` is a single value, the nb. of sampled 
-    observations is the same for each class. Alternatively, 
-    `k` can be a vector of length equal to the nb. of 
-    classes in `x`.
+* `k` : Nb. test observations to sample in each class. If `k` is a single value, the nb. of sampled 
+    observations is the same for each class. Alternatively, `k` can be a vector of length equal to 
+    the nb. of classes in `x`.
 * `y` : Quantitative variable (n) used if systematic sampling.
 
 Two outputs are returned (= row indexes of the data): 
 * `train` (n - `k`),
 * `test` (`k`). 
 
-If `y` = `nothing`, the sampling is random, else it is 
-systematic over the sorted `y` (see function `sampsys`).
+If `y` = `nothing`, the sampling is random, else it is systematic (within each class) over 
+the sorted `y` (see function `sampsys`).
 
 ## References
-Naes, T., 1987. The design of calibration in near infra-red reflectance 
-analysis by clustering. Journal of Chemometrics 1, 121-134.
+Naes, T., 1987. The design of calibration in near infra-red reflectance analysis by clustering. 
+Journal of Chemometrics 1, 121-134.
 
 ## Examples
 ```julia
