258f8930a6493f96da00eb91ef60a7536cb422e7
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-07-11 14:59:28 +0200

    Update segmkf.jl

--- src/segmkf.jl
+++ src/segmkf.jl
@@ -2,26 +2,22 @@
     segmkf(n::Int, K::Int; rep = 1)
     segmkf(group::Vector, K::Int; rep = 1)
 Build segments of observations for K-fold cross-validation.  
-* `n` : Total nb. of observations in the dataset. 
-    The sampling is implemented with 1:n.
-* `group` : A vector (n) defining blocks of observations.
+* `n` : Total nb. of observations in the dataset. The sampling is implemented with 1:`n`.
+* `group` : A vector (`n`) defining blocks of observations.
 * `K` : Nb. folds (segments) splitting the `n` observations. 
 Keyword arguments:
 * `rep` : Nb. replications of the sampling.
 
-For each replication, the function splits the `n` observations 
-to `K` segments  that can be used for K-fold cross-validation. 
+For each replication, the function splits the `n` observations to `K` segments  that can be used 
+for K-fold cross-validation. 
 
-If `group` is used (must be a vector of length `n`), the function 
-samples entire groups (= blocks) of observations instead of observations. 
-Such a block-sampling is required when data is structured by blocks and 
-when the response to predict is correlated within blocks. 
-This prevents underestimation of the generalization error.
+If `group` is used (must be a vector of length `n`), the function samples entire groups (= blocks) 
+of observations instead of observations. Such a block-sampling is required when data is structured 
+by blocks and when the response to predict is correlated within blocks. This prevents underestimation 
+of the generalization error.
 
-The function returns a list (vector) of `rep` elements. 
-Each element of the list contains `K` segments (= `K` vectors).
-Each segment contains the indexes (position within 1:`n`) of 
-the sampled observations.    
+The function returns a list (vector) of `rep` elements. Each element of the list contains `K` segments 
+(= `K` vectors). Each segment contains the indexes (position within 1:`n`) of the sampled observations.    
 
 ## Examples
 ```julia
