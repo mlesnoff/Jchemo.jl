7de8a994ae99e4d99a4752cf274afb4b7d83d8db
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-07-11 16:53:58 +0200

    Update samprand.jl

--- src/samprand.jl
+++ src/samprand.jl
@@ -4,8 +4,7 @@ Build training vs. test sets by random sampling.
 * `n` : Total nb. of observations.
 * `k` : Nb. test observations to sample.
 Keyword arguments:
-* `replace` : Boolean. If `false`, the sampling is 
-    without replacement.
+* `replace` : Boolean. If `false`, the sampling is without replacement.
 
 Two outputs are returned (= row indexes of the data): 
 * `train` (`n` - `k`),
