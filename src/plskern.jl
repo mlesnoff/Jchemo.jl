2aec4d04f3429ec245d0aba7e42beba8bf8d3dd2
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-07-12 09:35:27 +0200

    Update plskern.jl

--- src/plskern.jl
+++ src/plskern.jl
@@ -7,38 +7,32 @@ Partial least squares regression (PLSR) with the "improved kernel algorithm #1"
     (Dayal & McGegor, 1997).
 * `X` : X-data (n, p).
 * `Y` : Y-data (n, q).
-* `weights` : Weights (n) of the observations. 
-    Must be of type `Weight` (see e.g. function `mweight`).
+* `weights` : Weights (n) of the observations. Must be of type `Weight` (see e.g. 
+    function `mweight`).
 Keyword arguments:
 * `nlv` : Nb. latent variables (LVs) to compute.
-* `scal` : Boolean. If `true`, each column of `X` and `Y` 
-    is scaled by its uncorrected standard deviation.
+* `scal` : Boolean. If `true`, each column of `X` and `Y` is scaled by its uncorrected 
+    standard deviation.
     
-About the row-weighting in PLS algorithms (`weights`):
-See in particular Schaal et al. 2002, Siccard & Sabatier 2006, 
-Kim et al. 2011, and Lesnoff et al. 2020. 
+About the row-weighting in PLS algorithms (`weights`): see in particular Schaal et al. 2002, 
+Siccard & Sabatier 2006, Kim et al. 2011, and Lesnoff et al. 2020. 
 
 ## References
-Dayal, B.S., MacGregor, J.F., 1997. Improved PLS algorithms. 
-Journal of Chemometrics 11, 73-85.
+Dayal, B.S., MacGregor, J.F., 1997. Improved PLS algorithms. Journal of Chemometrics 11, 73-85.
 
-Kim, S., Kano, M., Nakagawa, H., Hasebe, S., 2011. Estimation 
-of active pharmaceutical ingredients content using locally 
-weighted partial least squares and statistical wavelength 
-selection. Int. J. Pharm., 421, 269-274.
+Kim, S., Kano, M., Nakagawa, H., Hasebe, S., 2011. Estimation of active pharmaceutical ingredients 
+content using locally weighted partial least squares and statistical wavelength selection. Int. 
+J. Pharm., 421, 269-274.
 
-Lesnoff, M., Metz, M., Roger, J.M., 2020. Comparison of locally 
-weighted PLS strategies for regression and discrimination on 
-agronomic NIR Data. Journal of Chemometrics. e3209. 
+Lesnoff, M., Metz, M., Roger, J.M., 2020. Comparison of locally weighted PLS strategies for regression
+and discrimination on agronomic NIR Data. Journal of Chemometrics. e3209. 
 https://onlinelibrary.wiley.com/doi/abs/10.1002/cem.3209
 
-Schaal, S., Atkeson, C., Vijayamakumar, S. 2002. Scalable 
-techniques from nonparametric statistics for the real time 
-robot learning. Applied Intell., 17, 49-60.
+Schaal, S., Atkeson, C., Vijayamakumar, S. 2002. Scalable techniques from nonparametric statistics 
+for the real time robot learning. Applied Intell., 17, 49-60.
 
-Sicard, E. Sabatier, R., 2006. Theoretical framework for local 
-PLS1 regression and application to a rainfall dataset. Comput. Stat. 
-Data Anal., 51, 1393-1410.
+Sicard, E. Sabatier, R., 2006. Theoretical framework for local PLS1 regression and application to a 
+rainfall dataset. Comput. Stat. Data Anal., 51, 1393-1410.
 
 ## Examples
 ```julia
@@ -87,8 +81,7 @@ res = predict(model, Xtest; nlv = 1:2)
 res = summary(model, Xtrain) ;
 @names res
 z = res.explvarx
-plotgrid(z.nlv, z.cumpvar; step = 2, xlabel = "Nb. LVs", 
-    ylabel = "Prop. Explained X-Variance").f
+plotgrid(z.nlv, z.cumpvar; step = 2, xlabel = "Nb. LVs", ylabel = "Prop. Explained X-Variance").f
 ```
 """ 
 plskern(; kwargs...) = JchemoModel(plskern, nothing, kwargs)
@@ -187,8 +180,7 @@ function transf(object::Union{Plsr, Splsr}, X; nlv = nothing)
     X = ensure_mat(X)
     a = nco(object.T)
     isnothing(nlv) ? nlv = a : nlv = min(nlv, a)
-    # Could be fcscale! but changes X
-    # If too heavy ==> Makes summary!
+    ## Could be fcscale! but changes X. If too heavy ==> Makes summary!
     fcscale(X, object.xmeans, object.xscales) * vcol(object.R, 1:nlv)
 end
 
@@ -209,8 +201,7 @@ function coef(object::Union{Plsr, Splsr}; nlv = nothing)
     Dy = Diagonal(object.yscales)
     ## To not use for Spcr (R not computed; while for Pcr, R = V)
     B =  fweight(vcol(object.R, 1:nlv), 1 ./ object.xscales) * theta * Dy
-    ## In 'int': No correction is needed, since 
-    ## ymeans, xmeans and B are in the original scale 
+    ## In 'int': No correction is needed, since ymeans, xmeans and B are in the original scale 
     int = object.ymeans' .- object.xmeans' * B
     ## End
     (B = B, int = int)
@@ -248,8 +239,7 @@ function Base.summary(object::Union{Plsr, Splsr}, X)
     X = ensure_mat(X)
     n, nlv = size(object.T)
     X = fcscale(X, object.xmeans, object.xscales)
-    ## Could be fcscale! but changes X
-    ## If too heavy ==> Makes summary!
+    ## Could be fcscale! but changes X. If too heavy ==> Makes summary!
     sstot = frob2(X, object.weights)       # = sum(object.weights.w' * X.^2)
     tt = object.TT
     tt_adj = (colnorm(object.V).^2) .* tt  # tt_adj[a] = p[a]'p[a] * tt[a]
