967db8dd523f747daa2ce887ffeda49fc722815f
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-09-03 17:44:11 +0200

    Update xresid.jl

--- src/xresid.jl
+++ src/xresid.jl
@@ -3,18 +3,14 @@
     xresid!(object, X::Matrix; nlv = nothing)
 Residual matrix from a bilinear model (e.g. PCA).
 * `object` : The fitted model.
-* `X` : New X-data to be approximated from the model.
-    Must be in the same scale as the X-data used to fit
-    the model `object`, i.e. before centering 
-    and eventual scaling.
+* `X` : New X-data to be approximated from the model.Must be in the same scale as the X-data used to fit
+    the model `object`, i.e. before centering and eventual scaling.
 Keyword arguments:
-* `nlv` : Nb. components (PCs or LVs) to consider. 
-    If `nothing`, it is the maximum nb. of components.
+* `nlv` : Nb. components (PCs or LVs) to consider. If `nothing`, it is the maximum nb. of components.
 
 Compute the residual matrix:
 * E = `X` - X_fit
-where X_fit is the fitted X returned by function 
-`xfit`. See `xfit` for examples. 
+where X_fit is the fitted X returned by function `xfit`. See `xfit` for examples. 
 ```
 """ 
 function xresid(object, X; nlv = nothing)
