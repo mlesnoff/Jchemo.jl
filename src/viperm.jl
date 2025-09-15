4f59f0f986b9ccb3c0ed5eb0eecfdfc453f874cc
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-09-15 22:07:33 +0200

    Update viperm.jl

--- src/viperm.jl
+++ src/viperm.jl
@@ -7,22 +7,29 @@ Variable importance by direct permutations.
 Keyword arguments:
 * `rep` : Number of replications of the splitting training/test. 
 * `psamp` : Proportion of data used as test set to compute the `score`.
-* `score` : Function computing the prediction score.
+* `score` : Function computing the prediction score (an error rate).
 
 The principle is as follows:
-* Data (X, Y) are splitted randomly to a training and a test set.
-* The model is fitted on Xtrain, and the score (error rate) is computed on Xtest. 
-    This gives the reference error rate.
-* Rows of a given variable (feature) j in Xtest are randomly permutated (the rest of Xtest is unchanged). 
-    The score is computed on the Xtest_perm_j (i.e. Xtest after thta the rows of variable j were permuted). 
-    The importance of variable j is computed by the difference between this score and the reference score.
-* This process is run for each variable j separately and replicated `rep` times. Average results are provided 
-    in the outputs, as well as the results per replication. 
+* Data (X, Y) are splitted randomly to a calibration set (Xcal, Ycal) and a validation set (Xval, Yval).
+* The model is fitted on (Xcal, Ycal) and used to compute the predictions from Xval. The error rate (the `score`) 
+    is computed by comparing these predictions to Yval, giving the reference error rate.
+* Consider Xval and a given variable (feature = column) j.
+    * a) The rows of variable j are permutated randomly (the other columns of Xval are unchanged), generating a new
+        matrix "Xval.perm.j".
+    * b) The model is used to compute the predictions from Xval.perm.j, and the error rate is computed by comparing 
+        these predictions to Yval. The variable importance (for variable j) is the difference between this error rate and 
+        the reference error rate.
+* This process is run for each variable j, separately.
 
-In general, this method returns similar results as the out-of-bag permutation method used in random forests 
-(Breiman, 2001).
+The overall process above is replicated `rep` times. The output provided by the function contains the average 
+results (i.e. over the `rep` replications) and the results per replication.
+
+In general, this method returns similar results as the out-of-bag permutation method (such as the one used in random 
+forests; Breiman, 2001).
 
 ## References
+Breiman, L., 2001. Random Forests. Machine Learning 45, 5–32. https://doi.org/10.1023/A:1010933404324
+
 Nørgaard, L., Saudland, A., Wagner, J., Nielsen, J.V., Munck, L., Engelsen, S.B., 2000. Interval Partial 
 Least-Squares Regression (iPLS): A Comparative Chemometric Study with an Example from Near-Infrared 
 Spectroscopy. Appl Spectrosc 54, 413–419. https://doi.org/10.1366/0003702001949500
@@ -72,9 +79,7 @@ model = rfr(n_trees = 10, max_depth = 2000, min_samples_leaf = 5)
 res = viperm(model, Xtrain, ytrain; rep = 50)
 z = vec(res.imp)
 f = Figure(size = (500, 400))
-ax = Axis(f[1, 1];
-    xlabel = "Wavelength (nm)", 
-    ylabel = "Importance")
+ax = Axis(f[1, 1]; xlabel = "Wavelength (nm)", ylabel = "Importance")
 scatter!(ax, wl, vec(z); color = (:red, .5))
 u = [910; 950]
 vlines!(ax, u; color = :grey, linewidth = 1)
