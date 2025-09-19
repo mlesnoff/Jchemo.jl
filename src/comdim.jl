77424babe57fdebdbecc14b4ac08e2f9a4a1bd9e
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-09-19 08:41:27 +0200

    Update comdim.jl

--- src/comdim.jl
+++ src/comdim.jl
@@ -4,20 +4,17 @@
     comdim(Xbl, weights::Weight; kwargs...)
     comdim!(Xbl::Matrix, weights::Weight; kwargs...)
 Common components and specific weights analysis (CCSWA, a.k.a ComDim).
-* `Xbl` : List of blocks (vector of matrices) of X-data. 
-    Typically, output of function `mblock`.  
-* `weights` : Weights (n) of the observations. 
-    Must be of type `Weight` (see e.g. function `mweight`).
+* `Xbl` : List of blocks (vector of matrices) of X-data. Typically, output of function `mblock`.  
+* `weights` : Weights (n) of the observations. Must be of type `Weight` (see e.g. function `mweight`).
 Keyword arguments:
 * `nlv` : Nb. global latent variables (LVs = scores) to compute.
 * `bscal` : Type of block scaling. See function `blockscal` for possible values.
 * `tol` : Tolerance value for convergence (Nipals).
 * `maxit` : Maximum number of iterations (Nipals).
-* `scal` : Boolean. If `true`, each column of blocks in `Xbl` 
-    is scaled by its uncorrected standard deviation 
+* `scal` : Boolean. If `true`, each column of blocks in `Xbl` is scaled by its uncorrected standard deviation 
     (before the block scaling).
 
-"SVD" algorithm of Hannafi & Qannari 2008 p.84.
+ComDim "SVD" algorithm of Hannafi & Qannari 2008 p.84.
 
 The function returns several objects, in particular:
 * `T` : The global LVs (not-normed).
@@ -43,31 +40,24 @@ Function `summary` returns:
 * `corx2t` : Correlation between the X-variables and the global LVs.  
 
 ## References
-Cariou, V., Qannari, E.M., Rutledge, D.N., Vigneau, E., 2018. 
-ComDim: From multiblock data analysis to path modeling. Food 
-Quality and Preference, Sensometrics 2016: Sensometrics-by-the-Sea 
-67, 27–34. https://doi.org/10.1016/j.foodqual.2017.02.012
+Cariou, V., Qannari, E.M., Rutledge, D.N., Vigneau, E., 2018. ComDim: From multiblock data analysis to 
+path modeling. Food Quality and Preference, Sensometrics 2016: Sensometrics-by-the-Sea 67, 27–34. 
+https://doi.org/10.1016/j.foodqual.2017.02.012
 
-Cariou, V., Jouan-Rimbaud Bouveresse, D., Qannari, E.M., 
-Rutledge, D.N., 2019. Chapter 7 - ComDim Methods for the Analysis 
-of Multiblock Data in a Data Fusion Perspective, in: Cocchi, M. (Ed.), 
-Data Handling in Science and Technology, 
-Data Fusion Methodology and Applications. Elsevier, pp. 179–204. 
+Cariou, V., Jouan-Rimbaud Bouveresse, D., Qannari, E.M., Rutledge, D.N., 2019. Chapter 7 - ComDim Methods 
+for the Analysis of Multiblock Data in a Data Fusion Perspective, in: Cocchi, M. (Ed.), 
+Data Handling in Science and Technology, Data Fusion Methodology and Applications. Elsevier, pp. 179–204. 
 https://doi.org/10.1016/B978-0-444-63984-4.00007-7
 
-Ghaziri, A.E., Cariou, V., Rutledge, D.N., Qannari, E.M., 2016. 
-Analysis of multiblock datasets using ComDim: Overview and extension 
-to the analysis of (K + 1) datasets. Journal of Chemometrics 30, 
-420–429. https://doi.org/10.1002/cem.2810
+Ghaziri, A.E., Cariou, V., Rutledge, D.N., Qannari, E.M., 2016. Analysis of multiblock datasets using ComDim: 
+Overview and extension to the analysis of (K + 1) datasets. Journal of Chemometrics 30, 420–429. 
+https://doi.org/10.1002/cem.2810
 
-Hanafi, M., 2008. Nouvelles propriétés de l’analyse en composantes 
-communes et poids spécifiques. Journal de la société française 
-de statistique 149, 75–97.
+Hanafi, M., 2008. Nouvelles propriétés de l’analyse en composantes communes et poids spécifiques. Journal de la 
+société française de statistique 149, 75–97.
 
-Qannari, E.M., Wakeling, I., Courcoux, P., MacFie, H.J.H., 2000. 
-Defining the underlying sensory dimensions. Food Quality and 
-Preference 11, 151–154. 
-https://doi.org/10.1016/S0950-3293(99)00069-5
+Qannari, E.M., Wakeling, I., Courcoux, P., MacFie, H.J.H., 2000. Defining the underlying sensory dimensions. 
+Food Quality and Preference 11, 151–154. https://doi.org/10.1016/S0950-3293(99)00069-5
 
 ## Examples
 ```julia
