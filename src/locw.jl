845c0e239fc040f214edf79cde2d481467b9c2ca
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-09-23 21:16:02 +0200

    Update locw.jl

--- src/locw.jl
+++ src/locw.jl
@@ -7,15 +7,12 @@ Compute predictions for a given kNN model.
 Keyword arguments:
 * `listnn` : List (vector) of m vectors of indexes.
 * `listw` : List (vector) of m vectors of weights.
-* `algo` : Function computing the model on 
-    the m neighborhoods.
+* `algo` : Function computing the model on the m neighborhoods.
 * `verbose` : Boolean. If `true`, predicting information are printed.
-* `kwargs` : Keywords arguments to pass in function `algo`.
-    Each argument must have length = 1 (not be a collection).
+* `kwargs` : Keywords arguments to pass in function `algo`. Each argument must have length = 1 (not be a collection).
 
-Each component i of `listnn` and `listw` contains the indexes 
-and weights, respectively, of the nearest neighbors of x_i in Xtrain. 
-The sizes of the neighborhood for i = 1,...,m can be different.
+Each component i of `listnn` and `listw` contains the indexes and weights, respectively, of the nearest neighbors 
+of x_i in Xtrain. The sizes of the neighborhood for i = 1,...,m can be different.
 """
 function locw(Xtrain, Ytrain, X; listnn, listw = nothing, algo, verbose = false, kwargs...)
     m = nro(X)
