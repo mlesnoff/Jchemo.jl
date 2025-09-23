8ecae683ad1dab5d278fae9a24788c85aef73d26
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-09-23 21:17:07 +0200

    Update locwlv.jl

--- src/locwlv.jl
+++ src/locwlv.jl
@@ -7,16 +7,13 @@ Compute predictions for a given kNN model.
 Keyword arguments:
 * `listnn` : List (vector) of m vectors of indexes.
 * `listw` : List (vector) of m vectors of weights.
-* `algo` : Function computing the model on 
-    the m neighborhoods.
+* `algo` : Function computing the model on the m neighborhoods.
 * `nlv` : Nb. or collection of nb. of latent variables (LVs).
-* `verbose` : Boolean. If `true`, predicting information 
-    are printed.
-* `kwargs` : Keywords arguments to pass in function `algo`.
-    Each argument must have length = 1 (not be a collection).
+* `verbose` : Boolean. If `true`, predicting information are printed.
+* `kwargs` : Keywords arguments to pass in function `algo`. Each argument must have length = 1 
+    (not be a collection).
 
-Same as [`locw`](@ref) but specific and much faster 
-for LV-based models (e.g. PLSR).
+Same as [`locw`](@ref) but specific and much faster for LV-based models (e.g. PLSR).
 """
 function locwlv(Xtrain, Ytrain, X; listnn, listw = nothing, algo, nlv, verbose = true, kwargs...)
     p = nco(Xtrain)
