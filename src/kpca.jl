d034425b78da86212c9b9bd849d34e2981913731
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-09-24 10:57:16 +0200

    Update kpca.jl

--- src/kpca.jl
+++ src/kpca.jl
@@ -4,35 +4,26 @@
     kpca(X, weights::Weight; kwargs...)
 Kernel PCA  (Scholkopf et al. 1997, Scholkopf & Smola 2002, Tipping 2001).
 * `X` : X-data (n, p).
-* `weights` : Weights (n) of the observations. 
-    Must be of type `Weight` (see e.g. function `mweight`).
+* `weights` : Weights (n) of the observations. Must be of type `Weight` (see e.g. function `mweight`).
 Keyword arguments:
 * `nlv` : Nb. principal components (PCs) to consider. 
-* `kern` : Type of kernel used to compute the Gram matrices.
-    Possible values are: `:krbf`, `:kpol`. See respective 
-    functions `krbf` and `kpol` for their keyword arguments.
-* `scal` : Boolean. If `true`, each column of `X` is scaled
-    by its uncorrected standard deviation.
+* `kern` : Type of kernel used to compute the Gram matrices.Possible values are: `:krbf`, `:kpol`. See respective functions `krbf` 
+    and `kpol` for their keyword arguments.
+* `scal` : Boolean. If `true`, each column of `X` is scaled by its uncorrected standard deviation.
 
-The method is implemented by SVD factorization of the weighted 
-Gram matrix: 
+The method is implemented by SVD factorization of the weighted Gram matrix: 
 * D^(1/2) * Phi(X) * Phi(X)' * D^(1/2)
-where X is the cenetred matrix and D is a diagonal matrix 
-of weights (`weights.w`) of the observations (rows of X).
+where X is the cenetred matrix and D is a diagonal matrix of weights (`weights.w`) of the observations (rows of X).
 
 ## References 
-Scholkopf, B., Smola, A., MÃ¼ller, K.-R., 1997. Kernel principal 
-component analysis, in: Gerstner, W., Germond, A., Hasler, 
-M., Nicoud, J.-D. (Eds.), Artificial Neural Networks, 
-ICANN 97, Lecture Notes in Computer Science. Springer, Berlin, 
+Scholkopf, B., Smola, A., MÃ¼ller, K.-R., 1997. Kernel principal component analysis, in: Gerstner, W., Germond, A., Hasler, 
+M., Nicoud, J.-D. (Eds.), Artificial Neural Networks, ICANN 97, Lecture Notes in Computer Science. Springer, Berlin, 
 Heidelberg, pp. 583-588. https://doi.org/10.1007/BFb0020217
 
-Scholkopf, B., Smola, A.J., 2002. Learning with kernels: support 
-vector machines, regularization, optimization, and beyond, Adaptive 
+Scholkopf, B., Smola, A.J., 2002. Learning with kernels: support vector machines, regularization, optimization, and beyond, Adaptive 
 computation and machine learning. MIT Press, Cambridge, Mass.
 
-Tipping, M.E., 2001. Sparse kernel principal component analysis. 
-Advances in neural information processing systems, MIT Press. 
+Tipping, M.E., 2001. Sparse kernel principal component analysis. Advances in neural information processing systems, MIT Press. 
 http://papers.nips.cc/paper/1791-sparse-kernel-principal-component-analysis.pdf
 
 ## Examples
@@ -52,14 +43,15 @@ Xtest = X[s.test, :]
 
 nlv = 3
 kern = :krbf ; gamma = 1e-4
-model = kpca(; nlv, kern, gamma) ;
+model = kpca(; nlv, kern, gamma) 
 fit!(model, Xtrain)
-@names model.fitm
-@head T = model.fitm.T
-T' * T
-model.fitm.V' * model.fitm.V
+fitm = model.fitm ;
+@names fitm
 
-@head Ttest = transf(model, Xtest)
+@head transf(model, Xtrain)
+@head fitm.T
+
+@head transf(model, Xtest) 
 
 res = summary(model) ;
 @names res
