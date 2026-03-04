a12e16b85aead7cf51137bea9f45cdbb310f879d
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2026-03-04 13:06:25 +0100

    protoplsr

--- src/protoplsr.jl
+++ src/protoplsr.jl
@@ -1,15 +1,3 @@
-## Approach KNN-prototypes:
-## - Some observations are sampled within Train, and represent "prototypes".
-## - A neighborhood is selected around each prototype. Note: A same training observation 
-##   can belong to several neighborhoods.
-## - A PLSR (prototype) model is optimized on each neighborhood, using a K-fold cross-validation,
-##   and stored.
-## - Each new observation to predict is assigned to its k nearest prototypes, and the final 
-##   prediction is obtained by a weighted average of the k predictions of the corresponding 
-##   prototype models.
-
-## The present function implements this approach only for univariate Y.
-
 ## --- Parameters to define the prototype models
 ## * nproto : Total nb. prototypes
 ## * nlvdis : Nb. latent variables for the dissimilarity model (0: Euclidean) 
@@ -23,7 +11,84 @@
 ## * h : Sharpeness of the weighting function used when averaging
 ## * criw : See function `winvs`
 ## * squared : Whether squared distances are used or not to compute the weights
-## * tolw : Tolerance (stabilizes predictions when many weights are close to zero; same as in lwplsr)
+## * tolw : Tolerance (stabilizes predictions when many weights are close to zero; same as in protoplsr)
+"""
+    protoplsr(; kwargs...)
+    protoplsr(X, Y; kwargs...)
+k-Nearest-Neighbours locally weighted partial least squares regression (kNN-protoplsr).
+* `X` : X-data (n, p).
+* `Y` : Y-data (n, q).
+Keyword arguments:
+* `nlvdis` : Number of latent variables (LVs) to consider in the global PLS used for the dimension 
+    reduction before computing the dissimilarities. If `nlvdis = 0`, there is no dimension reduction.
+* `metric` : Type of dissimilarity used to select the neighbors and to compute the weights 
+    (see function `getknn`). Possible values are: `:eucl` (Euclidean), `:mah` (Mahalanobis), 
+    `:sam` (spectral angular distance), `:cos` (cosine distance), `:cor` (correlation distance).
+* `nproto`: Nb. prototype models.
+* `k`: Nb. observations considered in each 
+* `centroid`:
+* `samp`:
+* `nlv` : Nb. latent variables (LVs) for the local (i.e. inside each neighborhood) models.
+* `kavg` : The number of nearest neighbors to select for each observation to predict.
+* `h` : A scalar defining the shape of the weight function computed by function `winvs`. Lower is h, 
+    sharper is the function. See function `winvs` for details (keyword arguments `criw` and `squared` of 
+    `winvs` can also be specified here).
+* `tolw` : For stabilization when very close neighbors.
+* `scal` : Boolean. If `true`, (a) each column of the global `X` (and of the global `Y` if there 
+    is a preliminary PLS reduction dimension) is scaled by its uncorrected standard deviation before to compute 
+    the distances and the weights, and (b) the X and Y scaling is also done within each neighborhood (local level) 
+    for the weighted PLSR.
+
+Function `protoplsr` fits a KNN prototype model approach:
+* Some observations are sampled within Train, and represent "prototypes".
+* A neighborhood is selected around each prototype. Note: A same training observation 
+  can belong to several neighborhoods.
+* A PLSR (prototype) model is optimized on each neighborhood, using a K-fold cross-validation,
+   and stored.
+* Each new observation to predict is assigned to its k nearest prototypes, and the final 
+  prediction is obtained by a weighted average of the k predictions of the corresponding 
+  prototype models.
+
+The present function implements this approach only for univariate Y.
+
+
+
+## Examples
+```julia
+using Jchemo, JchemoData, JLD2, CairoMakie
+path_jdat = dirname(dirname(pathof(JchemoData)))
+db = joinpath(path_jdat, "data/cassav.jld2") 
+@load db dat
+@names dat
+X = dat.X 
+y = dat.Y.tbc
+year = dat.Y.year
+tab(year)
+s = year .<= 2012
+Xtrain = X[s, :]
+ytrain = y[s]
+Xtest = rmrow(X, s)
+ytest = rmrow(y, s)
+
+nlvdis = 15 ; metric = :mah 
+k = 500 ; h = 1 ; nlv = 10
+model = protoplsr(; nlvdis, metric, h, k, nlv) 
+fit!(model, Xtrain, ytrain)
+@names model
+@names model.fitm
+
+res = predict(model, Xtest) ; 
+@names res 
+res.listnn
+res.listd
+res.listw
+@head res.pred
+@show rmsep(res.pred, ytest)
+plotxy(res.pred, ytest; color = (:red, .5), bisect = true, xlabel = "Prediction",  
+    ylabel = "Observed").f    
+```
+""" 
+protoplsr(; kwargs...) = JchemoModel(protoplsr, nothing, kwargs)
 
 Base.@kwdef mutable struct ParProtoPlsr
     nlvdis::Int = 0                         
@@ -123,7 +188,7 @@ function predict(object::ProtoPlsr, X)
     Threads.@threads for i = 1:m   # seems to not give better performance
         s = listnn[i]
         w = winvs(res.d[i]; h, criw, squared)
-        w[w .< tolw] .= tolw  # same as in lwplsr
+        w[w .< tolw] .= tolw  # same as in protoplsr
         w ./= sum(w)
         @inbounds for j in eachindex(s)
             coefs = object.coefs[s[j]]
