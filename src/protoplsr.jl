e321c471b41a78cf4e2ddf43bec70381b2aaeae1
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2026-03-04 11:16:22 +0100

    protoplsr

--- src/protoplsr.jl
+++ src/protoplsr.jl
@@ -11,12 +11,12 @@
 ## The present function implements this approach only for univariate Y.
 
 ## --- Parameters to define the prototype models
-## * n_proto : Total nb. prototypes
+## * nproto : Total nb. prototypes
 ## * nlvdis : Nb. latent variables for the dissimilarity model (0: Euclidean) 
 ## * metric :  Metric for the dissimilarity (e.g. :eucl, :mah, :cor)
-## * k_proto : Nb. neighbors for each prototype
-## * nlv_proto : Max. nb. latent variables for each prototype model  
-## * scal_proto : Scale the (local) data before fitting each prototype model
+## * k : Nb. neighbors for each prototype
+## * nlv : Max. nb. latent variables for each prototype model  
+## * scal : Scale the (local) data before fitting each prototype model
 ## --- Parameters for final prediction (weighted average of the prototype predictions)
 ## * [The metric is the same as the one used to select the prototype samples]
 ## * k : Nb. prototype models whose prédictions are averaged
@@ -25,29 +25,30 @@
 ## * squared : Whether squared distances are used or not to compute the weights
 ## * tolw : Tolerance (stabilizes predictions when many weights are close to zero; same as in lwplsr)
 
-struct ProtoPlsr
-    Xproto::Matrix
-    Yproto::Matrix
-    fitm_emb::Union{Nothing, Plsr}
-    resnn::Vector
-    fitm
-    coefs::Vector
-    par::ParProtoPlsr
-end
-
-struct ParProtoPlsr
-    n_proto::Int = 1
+Base.@kwdef mutable struct ParProtoPlsr
     nlvdis::Int = 0                         
     metric::Symbol = :eucl  
-    k_proto::Int = 1 
-    centroid_proto::Int = true
-    nlv_proto::Union{Int, Vector{Int}, UnitRange} = 1   
-    scal_proto::Bool = false    
-    k::Int = 1                              
+    nproto::Int = 1
+    k::Int = 1 
+    centroid::Bool = true
+    samp::Symbol = :rand
+    nlv::Union{Int, Vector{Int}, UnitRange} = 1   
+    kavg::Int = 1                              
     h::Float64 = Inf                        
     criw::Float64 = 4                       
     squared::Bool = false                   
     tolw::Float64 = 1e-4                    
+    scal::Bool = false    
+end
+
+struct ProtoPlsr
+    Xproto::Matrix
+    Yproto::Matrix
+    fitm_emb::Union{Nothing, Plsr}
+    resnn::NamedTuple
+    fitm
+    coefs::Vector
+    par::ParProtoPlsr
 end
 
 function protoplsr(X, Y; kwargs...)
@@ -56,33 +57,36 @@ function protoplsr(X, Y; kwargs...)
     Y = ensure_mat(Y)
     n = nro(X)
     ## Selection of the prototypes
-    #s_proto = sort(sampks(X; k = n_proto, metric = "eucl").train)
-    s_proto = samprand(n, par.n_proto; seed = 1234).test
+    if par.samp == :rand
+        s_proto = samprand(n, par.nproto; seed = 1234).test
+    elseif par.samp == :ks
+        s_proto = sampks(X, par.nproto; metric).test
+    end
     ## Compute the neighborhood of each prototype
-    if nlvdis == 0
+    if par.nlvdis == 0
         fitm_emb = nothing
-        resnn = getknn(X, vrow(X, s_proto); k = par.k_proto, par.metric)
+        resnn = getknn(X, vrow(X, s_proto); k = par.k, par.metric)
     else
         fitm_emb = plskern(X, Y; nlv = par.nlvdis)
-        resnn = getknn(fitm_emb.T, transform(fitm_emb, vrow(X, s_proto)); k = par.k_proto, par.metric)
+        resnn = getknn(fitm_emb.T, transform(fitm_emb, vrow(X, s_proto)); k = par.k, par.metric)
     end
     ## Optimize/fit the prototype models 
-    fitm = list(Jchemo.Plsr, par.n_proto)
-    coefs = list(NamedTuple, par.n_proto)
-    segm = segmkf(k_proto, 3; rep = 1, seed = 1234)
-    #@inbounds for i in eachindex(s_proto) 
-    Xproto = similar(X, par.n_proto, nco(X))
-    Yproto = similar(Y, par.n_proto, nco(Y))
-    Threads.@threads for i in eachindex(s_proto)
+    fitm = list(Plsr, par.nproto)
+    coefs = list(NamedTuple, par.nproto)
+    segm = segmkf(par.k, 3; rep = 1, seed = 1234)
+    Xproto = similar(X, par.nproto, nco(X))
+    Yproto = similar(Y, par.nproto, nco(Y))
+    @inbounds for i in eachindex(s_proto) 
+    #Threads.@threads for i in eachindex(s_proto)
         vX = vrow(X, resnn.ind[i])
         vY = vrow(Y, resnn.ind[i])
-        pars = mpar(scal = par.scal_proto)
+        pars = mpar(scal = par.scal)
         model = plskern()
-        rescv = gridcv(model, vX, vY; segm, score = rmsep, pars, nlv = 0:par.nlv_proto).res
+        rescv = gridcv(model, vX, vY; segm, score = rmsep, pars, nlv = 0:par.nlv).res
         u = findall(rescv.y1 .== minimum(rescv.y1))[1]
         fitm[i] = plskern(vX, vY; nlv = rescv.nlv[u], scal = rescv.scal[u])
         coefs[i] = coef(fitm[i])
-        if par.centroid_proto
+        if par.centroid          
             Xproto[i, :] .= colmean(vX)
             Yproto[i, :] .= colmean(vY)    
         else
@@ -99,24 +103,24 @@ function predict(object::ProtoPlsr, X)
     Q = eltype(X)
     m = nro(X)
     q = nco(object.Yproto)
-    n_proto = nro(object.Xproto)
-    metric = object.pars.metric
-    k = min(object.pars.k, n_proto)  # nb prototype models averaged
-    h = object.pars.h
-    criw = object.pars.criw
-    squared = object.pars.squared
-    tolw = object.pars.tolw
+    nproto = object.par.nproto
+    metric = object.par.metric
+    kavg = min(object.par.kavg, nproto)  # nb prototype models averaged
+    h = object.par.h
+    criw = object.par.criw
+    squared = object.par.squared
+    tolw = object.par.tolw
     # Compute the neighborhood (within prototypes) of each new observation
     if isnothing(object.fitm_emb)
-        res = getknn(object.Xproto, X; k, metric)
+        res = getknn(object.Xproto, X; k = kavg, metric)
     else
-        res = getknn(object.fitm_emb.T, transform(object.fitm_emb, X); k, metric) 
+        res = getknn(object.fitm_emb.T, transform(object.fitm_emb, X); k = kavg, metric) 
     end
     listnn = res.ind
     listw = list(Vector{Q}, m)
     pred = zeros(m, q)
-    @inbounds for i = 1:m
-    #Threads.@threads for i = 1:m   # seems to not give better performance
+    #@inbounds for i = 1:m
+    Threads.@threads for i = 1:m   # seems to not give better performance
         s = listnn[i]
         w = winvs(res.d[i]; h, criw, squared)
         w[w .< tolw] .= tolw  # same as in lwplsr
