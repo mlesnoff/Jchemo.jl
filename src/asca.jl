e8d2d29bd4096c5b6d36343c69731f16b5dafe14
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2026-03-29 20:28:32 +0200

    asca

---
+++ src/asca.jl
@@ -0,0 +1,31 @@
+"""
+    asca(X, f::StatsModels.FormulaTerm, dat::DataFrame)
+ANOVA Simultaneous Component Analysis (ASCA).
+* `X` :  X-data (n, p) to decompose.
+* `f` : A formula that defines the factor(s) on which is(are) done the decomposition.
+    See the syntax in the examples below.
+* `dat` (n, q) : Dataframe containing the factor(s) specified in `f`. 
+
+## References
+Bertinetto, C., Engel, J., Jansen, J., 2020. ANOVA simultaneous component analysis: A tutorial review. 
+Analytica Chimica Acta: X 6, 100061. https://doi.org/10.1016/j.acax.2020.100061
+
+Doledec, S., Chessel, D., 1987. Rythmes saisonniers et composantes stationnelles en milieu aquatique. 
+I: Description d’un plan d’observation complet par projection de variables. 
+Acta oecol., Oecol. gen 8, 403–426.
+
+Smilde, A.K., Jansen, J.J., Hoefsloot, H.C.J., Lamers, R.-J.A.N., van der Greef, J., Timmerman, M.E., 2005. 
+ANOVA-simultaneous component analysis (ASCA): a new tool for analyzing designed metabolomics data. 
+Bioinformatics 21, 3043–3048. https://doi.org/10.1093/bioinformatics/bti476
+
+Smilde, A.K., Marini, F., Westerhuis, J.A., Liland, K.H. (Eds.), 2025. Analysis of variance 
+for high-dimensional data: applications in life, food and chemical sciences. Wiley, Hoboken, NJ.
+
+## Examples 
+```julia
+```
+"""
+function asca(X, f::StatsModels.FormulaTerm, dat::DataFrame)
+
+end
+
