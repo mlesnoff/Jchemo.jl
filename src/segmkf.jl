aaa55c879333fcb3ca2e267181eab57a6eda749b
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2026-04-16 21:12:59 +0200

    segmkf

--- src/segmkf.jl
+++ src/segmkf.jl
@@ -2,14 +2,15 @@
     segmkf(n::Int, K::Int; rep = 1, seed::Union{Nothing, Int} = nothing)
     segmkf(group::Vector, K::Int; rep = 1, seed::Union{Nothing, Int} = nothing)
 Build segments of observations for K-fold cross-validation.  
-* `n` : Total nb. of observations in the dataset. The sampling is implemented with 1:`n`.
+* `n` : Total nb. of observations in the dataset. The sampling is implemented within 1:`n`.
 * `group` : A vector (`n`) defining blocks of observations.
 * `K` : Nb. folds (segments) splitting the `n` observations. 
 Keyword arguments:
-* `rep` : Nb. replications of the sampling.
-* `seed` : Eventual seed for the `Random.MersenneTwister` generator. Must be of length = `rep`. 
+* `rep` : Nb. replications of the K-fold sampling.
+* `seed` : Eventual seed for the `Random.MersenneTwister` generator. 
 
-For each replication, the function splits the `n` observations to `K` segments  that can be used for K-fold cross-validation. 
+For each replication, the function splits the `n` observations to `K` segments  that can be used for K-fold 
+cross-validation. 
 
 If `group` is used (must be a vector of length `n`), the function samples entire groups (= blocks) of observations 
 instead of observations. Such a block-sampling is required when data is structured by blocks and when the response to 
@@ -25,12 +26,10 @@ using Jchemo
 n = 10 ; K = 3
 rep = 4 
 segm = segmkf(n, K; rep)
+#segm = segmkf(n, K; rep, seed = 1234)
 i = 1 
 segm[i]
 segm[i][1]
-
-segmkf(n, K; seed = 123)
-segmkf(n, K; rep, seed = collect(1:rep))
 
 n = 10 
 group = ["A", "B", "C", "D", "E", "A", "B", "C", "D", "E"]    # blocks of the observations
