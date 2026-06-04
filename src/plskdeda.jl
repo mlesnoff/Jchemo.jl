4e1ea62cc0a241b8ddbe58b1900e03fbf0c8bde8
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2026-06-04 20:47:51 +0200

    plskdeda

--- src/plskdeda.jl
+++ src/plskdeda.jl
@@ -90,8 +90,10 @@ function plskdeda(X, y, weights::ProbabilityWeights; kwargs...)
     ni = tab(y).vals
     priors = aggsumv(weights.values, vec(y)).val  # output not used, only for information
     fitm_emb = plskern(X, res.Y, weights; kwargs...)
-    fitm_da = list(Kdeda, par.nlv)
-    @inbounds for i = 1:par.nlv
+    nlv = fitm_emb.par.nlv
+    par.nlv = nlv
+    fitm_da = list(Kdeda, nlv)
+    @inbounds for i in eachindex(fitm_da)
         fitm_da[i] = kdeda(vcol(fitm_emb.T, 1:i), y, weights; kwargs...)
     end
     Plsprobda(fitm_emb, fitm_da, ni, priors, res.lev, par) 
