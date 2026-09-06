4fc6d71958849319589608091d009eb5655e40de
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2026-09-06 10:05:26 +0200

    ensure_mat

--- src/_util.jl
+++ src/_util.jl
@@ -183,9 +183,15 @@ Reshape `X` to a matrix if necessary.
 ensure_mat(X::Matrix) = X
 ensure_mat(X::AbstractMatrix) = Matrix(X)
 ensure_mat(X::Vector) = reshape(X, :, 1)
+
+ensure_mat(X::Matrix{Union{Missing, Q}}) where Q <: Float = Q.(X)
+ensure_mat(X::AbstractMatrix{Union{Missing, Q}}) where Q <: Float = Matrix(Q.(X))
 ensure_mat(X::Vector{Union{Missing, Q}}) where Q <: Float = reshape(Q.(X), :, 1)
+
 ensure_mat(X::Number) = reshape([X], 1, 1)
+
 ensure_mat(X::DataFrame) = Matrix(X)
+
 function ensure_mat(X::SubArray)  # for views
     if isa(X, AbstractVector)
         return Matrix(reshape(X, length(X), 1))
